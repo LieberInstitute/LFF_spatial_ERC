5f535bbdc857f6bf08f4cf5849c0a522e1728c9b
heenadivecha <heenadivecha@gmail.com>
2024-04-02 11:20:07 -0400

    Fixed the path to correct parameter file

--- code/01_spaceranger/spaceranger_SLURM_17v.sh
+++ code/01_spaceranger/spaceranger_SLURM_17v.sh
@@ -24,7 +24,7 @@ module load spaceranger/2.1.0
 module list
 
 ## Read inputs from spaceranger_parameters.txt file
-FILE=$(awk "NR==${SLURM_ARRAY_TASK_ID}" spaceranger_parameters.txt)
+FILE=$(awk "NR==${SLURM_ARRAY_TASK_ID}" spaceranger_parameter_17v.txt)
 SAMPLE=$(echo ${FILE} | cut -d "," -f 1)
 SLIDE=$(echo ${FILE} | cut -d "," -f 2)
 CAPTUREAREA=$(echo ${FILE} | cut -d "," -f 3)
