d8c75e3a6676dc0897788ce65581fee82845f241
Louise Huuki <lahuuki@gmail.com>
2024-06-12 14:49:53 -0400

    Update QC plotting details, title, sample order. Use ggridge to compare QC metric distibutions in samples. Add qc metric notes. Update grid plots.

--- code/02_build_spe/03_QC_spe.R
+++ code/02_build_spe/03_QC_spe.R
@@ -9,40 +9,36 @@ library("sessioninfo")
 plot_dir <- here("plots", "02_build_spe", "03_QC_spe")
 if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
+data_dir <- here("processed-data", "02_build_spe", "03_QC_spe")
+if(!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
+
 ## load spe
 spe <- readRDS(here("processed-data", "02_build_spe", "spe_raw.rds"))
 
-spe$trimmed <- ifelse(grepl("untrimmed", spe$base_path), "untrimmed", "trimmed")
 
-#### Tissue Overlap ####
-
-(sample_in_tissue <- table(spe$sample_id[spe$in_tissue]))
-# V13B23-363_A1 V13B23-363_B1 V13B23-363_C1 V13B23-363_D1 V13B23-364_A1 V13B23-364_B1 V13B23-364_C1 V13B23-364_D1 
-# 4872          4820          3560          4843          4031          4880          3580          3663 
-# V13B23-365_A1 V13B23-365_B1 V13B23-365_C1 V13B23-365_D1 V13B23-366_B1 V13B23-366_C1 V13B23-366_D1 V13Y24-340_A1 
-# 4335          4601          4700          4583          3647          4214          3231          3851 
-# V13Y24-340_B1 V13Y24-340_C1 V13Y24-340_D1 V13Y24-342_A1 V13Y24-342_B1 V13Y24-342_C1 V13Y24-342_D1 V13Y24-343_A1 
-# 3082          3406          2843          3989          4840          3754          4059          4677 
-# V13Y24-343_B1 V13Y24-343_C1 V13Y24-343_D1 V13Y24-344_A1 V13Y24-344_B1 V13Y24-344_C1 V13Y24-344_D1 
-# 3919          3534          4057          2396          3865          2728          4186 
+#### In Tissue ####
+sample_in_tissue <- table(spe$sample_id[spe$in_tissue])
+sort(sample_in_tissue)
+# Br2582 Br3974 Br5367 Br2305 Br6263 Br5712 Br1556 Br5832 Br5854 Br6321 Br1289 Br1706 Br5517 Br5415 Br1039 Br6476 Br6085 Br5276 Br5460 
+# 2396   2728   2843   3082   3231   3406   3534   3560   3580   3647   3663   3754   3851   3865   3919   3989   4031   4057   4059 
+# Br1691 Br6538 Br6423 Br5529 Br6098 Br5212 Br5161 Br5599 Br5426 Br5634 Br5941 Br6161 
+# 4186   4214   4335   4583   4601   4677   4700   4820   4840   4843   4872   4880
 
 summary(as.integer(sample_in_tissue))
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 # 2396    3570    3989    3960    4592    4880
 
+sample_order = sort(unique(spe$sample_id))
+
 vis_grid_clus(
     spe = spe,
     clustervar = "overlaps_tissue",
-    pdf = here::here(plot_dir, "in_tissue_grid.pdf"),
+    pdf = here::here(plot_dir, "spe_erc_grid-in_tissue.pdf"),
     sort_clust = FALSE,
     point_size = 1,
-    colors = c("in" = "grey90", "out" = "orange")
+    colors = c("in" = "grey75", "out" = "orange"),
+    sample_order = sample_order
 )
-
-summary(spe$sum_umi[!inTissue(spe_raw)])
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
-# 2.0   101.0   162.0   231.3   287.8  1553.0
-
 
 
 #### UMI ####
@@ -58,29 +54,29 @@ summary(spe$sum_umi[!spe$in_tissue])
 vis_grid_gene(
     spe = spe,
     geneid = "sum_umi",
-    pdf = here::here(plot_dir, "sum_umi_all.pdf"),
-    # assayname = "counts",
-    point_size = 1
+    pdf = here::here(plot_dir, "spe_erc_grid-sum_umi_all.pdf"),
+    assayname = "counts",
+    point_size = 1,
+    sample_order = sample_order
 )
 
 vis_grid_gene(
     spe = spe[,spe$in_tissue],
     geneid = "sum_umi",
-    pdf = here::here(plot_dir, "sum_umi_in_tissue.pdf"),
-    # assayname = "counts",
-    point_size = 1
+    pdf = here::here(plot_dir, "spe_erc_grid-sum_umi_in_tissue.pdf"),
+    assayname = "counts",
+    point_size = 1,
+    sample_order = sample_order
 )
 
 vis_grid_gene(
     spe = spe[,!spe$in_tissue],
     geneid = "sum_umi",
-    pdf = here::here(plot_dir, "sum_umi_out_tissue.pdf"),
+    pdf = here::here(plot_dir, "spe_erc_grid-sum_umi_out_tissue.pdf"),
     assayname = "counts",
-    point_size = 1
+    point_size = 1,
+    sample_order = sample_order
 )
-
-## Violin plots 
-
 
 #### Mito Rate ####
 summary(spe$expr_chrM_ratio[spe$in_tissue])
@@ -94,17 +90,19 @@ summary(spe$expr_chrM_ratio[!spe$in_tissue])
 vis_grid_gene(
     spe = spe[,spe$in_tissue],
     geneid = "expr_chrM_ratio",
-    pdf = here::here(plot_dir, "expr_chrM_ratio_in_tissue.pdf"),
+    pdf = here::here(plot_dir, "spe_erc_grid-expr_chrM_ratio_in_tissue.pdf"),
     # assayname = "counts",
-    point_size = 1
+    point_size = 1,
+    sample_order = sample_order
 )
 
 vis_grid_gene(
     spe = spe[,!spe$in_tissue],
     geneid = "expr_chrM_ratio",
-    pdf = here::here(plot_dir, "expr_chrM_ratio_out_tissue.pdf"),
+    pdf = here::here(plot_dir, "spe_erc_grid-expr_chrM_ratio_out_tissue.pdf"),
     assayname = "counts",
-    point_size = 1
+    point_size = 1,
+    sample_order = sample_order
 )
 
 
@@ -120,53 +118,30 @@ summary(spe$sum_gene[!spe$in_tissue])
 vis_grid_gene(
     spe = spe[,spe$in_tissue],
     geneid = "sum_gene",
-    pdf = here::here(plot_dir, "sum_gene_in_tissue.pdf"),
+    pdf = here::here(plot_dir, "spe_erc_grid-sum_gene_in_tissue.pdf"),
     point_size = 1
 )
 
 vis_grid_gene(
     spe = spe[,!spe$in_tissue],
     geneid = "sum_gene",
-    pdf = here::here(plot_dir, "sum_gene_out_tissue.pdf"),
+    pdf = here::here(plot_dir, "spe_erc_grid-sum_gene_out_tissue.pdf"),
     point_size = 1
 )
 
 
 #### Outliers ####
+pdf(here(plot_dir, "spe_erc_QC_outliers_violin_plots.pdf"), width = 21)
 
-spe <- spe[, spe$in_tissue]
-
-spe$low_lib_size = isOutlier(
-    spe$sum_umi,
-    type = "lower",
-    log = TRUE,
-    batch = spe$sample_id
-)
-
-spe$low_n_features = isOutlier(
-    spe$sum_gene,
-    type = "lower",
-    log = TRUE,
-    batch = spe$sample_id
-)
-
-spe$high_Mito_percent = isOutlier(
-    spe$expr_chrM_ratio,
-    type = "higher",
-    batch = spe$sample_id
-)
-
-pdf(here(plot_dir, "QC_outliers.pdf"), width = 21)
-
-plotColData(spe, x = "sample_id", y = "sum_umi", colour_by = "low_lib_size") +
+plotColData(spe, x = "sample_id", y = "sum_umi", colour_by = "scran_low_lib_size") +
     ggtitle("sum_umi") +
     facet_wrap(~ spe$trimmed[spe$in_tissue], scales = "free_x", nrow = 2)
 
-plotColData(spe, x = "sample_id", y = "sum_gene", colour_by = "low_n_features") +
+plotColData(spe, x = "sample_id", y = "sum_gene", colour_by = "scran_low_n_features") +
     ggtitle("sum_gene") +
     facet_wrap(~ spe$trimmed[spe$in_tissue], scales = "free_x", nrow = 2)
 
-plotColData(spe, x = "sample_id", y = "expr_chrM_ratio", colour_by = "high_Mito_percent") +
+plotColData(spe, x = "sample_id", y = "expr_chrM_ratio", colour_by = "scran_high_Mito_percent") +
     ggtitle("expr_chrM_ratio") +
     facet_wrap(~ spe$trimmed[spe$in_tissue], scales = "free_x", nrow = 2)
 
@@ -174,8 +149,8 @@ dev.off()
 
 vis_grid_clus(
     spe = spe,
-    clustervar = "low_lib_size",
-    pdf = here::here(plot_dir, "QC_grid_low_lib_size.pdf"),
+    clustervar = "scran_low_lib_size",
+    pdf = here::here(plot_dir, "spe_erc_grid_qc-low_lib_size.pdf"),
     sort_clust = FALSE,
     point_size = 1,
     colors = c(`TRUE` = "orange", `FALSE` = "lightskyblue3")
@@ -183,8 +158,8 @@ vis_grid_clus(
 
 vis_grid_clus(
     spe = spe,
-    clustervar = "low_n_features",
-    pdf = here::here(plot_dir, "QC_grid_low_n_features.pdf"),
+    clustervar = "scran_low_n_features",
+    pdf = here::here(plot_dir, "spe_erc_grid_qc-low_n_features.pdf"),
     sort_clust = FALSE,
     point_size = 1,
     colors = c(`TRUE` = "orange", `FALSE` = "lightskyblue3")
@@ -192,54 +167,79 @@ vis_grid_clus(
 
 vis_grid_clus(
     spe = spe,
-    clustervar = "high_Mito_percent",
-    pdf = here::here(plot_dir, "QC_grid_high_Mito_percent.pdf"),
+    clustervar = "scran_high_Mito_percent",
+    pdf = here::here(plot_dir, "spe_erc_grid_qc-high_Mito_percent.pdf"),
     sort_clust = FALSE,
     point_size = 1,
     colors = c(`TRUE` = "orange", `FALSE` = "lightskyblue3")
 )
 
+## temp 
 
-
-## Scran QC
-qcstats <- perCellQCMetrics(spe, subsets = list(
-    Mito = which(seqnames(spe) == "chrM")
-))
-qcfilter <- quickPerCellQC(qcstats, sub.fields = "subsets_Mito_percent")
-colSums(as.matrix(qcfilter))
-
-
-
-## Find edge spots
-spot_coords <- colData(spe) |>
-    as.data.frame() |>
-    select(sample_id, array_row, array_col)
-
-edge_spots <- spot_coords |>
-    group_by(sample_id, array_row) |>
-    mutate(edge_col = array_col == min(array_col) | array_col == max(array_col)) |>
-    group_by(sample_id, array_col) |>
-    mutate(edge_row = array_row == min(array_row) | array_row == max(array_row)) |>
-    group_by(sample_id) |>
-    mutate(edge = edge_row | edge_col)
-
-edge_spots |> count(edge)
-
-spe$edge_spot <- edge_spots$edge
-
-vis_grid_clus(
-    spe = spe,
-    clustervar = "edge_spot",
-    pdf = here::here(plot_dir, "QC_edge.pdf"),
-    sort_clust = FALSE,
-    point_size = 1,
-    colors = c(`TRUE` = "orange", `FALSE` = "lightskyblue3")
+pdf(here(plot_dir, "spe_erc_Br5276-sum_umi.pdf"))
+vis_gene(
+    spe = spe[,spe$in_tissue],
+    sampleid = "Br5276",
+    geneid = "sum_umi",
+    assayname = "counts"
+    # colors = c("in" = "grey90", "out" = "orange")
 )
+dev.off()
+
+pd <- as.data.frame(colData(spe))
+
+## ggplots for qc
+
+# qc_density_sum_umi <- pd |>
+#     ggplot(aes(x = sum_umi, color = in_tissue)) +
+#     geom_density() +
+#     # facet_grid(sample_id~.)
+#     facet_wrap(~sample_id, ncol = 2)
+# 
+# ggsave(qc_density_sum_umi, filename = here(plot_dir, "spe_erc_density-sum_umi.png"), height = 12)
+
+library(ggridges)
+
+qc_ggridge_sum_umi <- pd |>
+    ggplot(aes(x = sum_umi, y= sample_id, fill = in_tissue, color = in_tissue)) +
+    geom_density_ridges(quantile_lines = TRUE,
+                        alpha = .5)  +
+    scale_fill_manual(values = c(`TRUE` = "grey20", `FALSE` = "darkorange")) +
+    scale_color_manual(values = c(`TRUE` = "grey20", `FALSE` = "darkorange"))+
+    theme(legend.position = "bottom")
+
+ggsave(qc_ggridge_sum_umi, filename = here(plot_dir, "spe_erc_ggridge-sum_umi.png"), height = 12)
+
+qc_ggridge_sum_gene <- pd |>
+    ggplot(aes(x = sum_gene, y= sample_id, fill = in_tissue, color = in_tissue)) +
+    geom_density_ridges(quantile_lines = TRUE,
+                        alpha = .5)  +
+    scale_fill_manual(values = c(`TRUE` = "grey20", `FALSE` = "darkorange")) +
+    scale_color_manual(values = c(`TRUE` = "grey20", `FALSE` = "darkorange"))+
+    theme(legend.position = "bottom")
+
+ggsave(qc_ggridge_sum_gene, filename = here(plot_dir, "spe_erc_ggridge-sum_gene.png"), height = 12)
+
+qc_ggridge_expr_chrM_ratio <- pd |>
+    ggplot(aes(x = expr_chrM_ratio, y= sample_id, fill = in_tissue, color = in_tissue)) +
+    geom_density_ridges(quantile_lines = TRUE,
+                        alpha = .5)  +
+    scale_fill_manual(values = c(`TRUE` = "grey20", `FALSE` = "darkorange")) +
+    scale_color_manual(values = c(`TRUE` = "grey20", `FALSE` = "darkorange")) +
+    theme(legend.position = "bottom")
+
+ggsave(qc_ggridge_expr_chrM_ratio, filename = here(plot_dir, "spe_erc_ggridge-expr_chrM_ratio.png"), height = 12)
+
+library(patchwork)
+
+ggsave(qc_ggridge_sum_umi + theme(legend.position = "None")+ 
+           qc_ggridge_sum_gene + theme(axis.title.y=element_blank(),axis.text.y=element_blank(), legend.position = "bottom") +
+           qc_ggridge_expr_chrM_ratio  + theme(axis.title.y=element_blank(),axis.text.y=element_blank(), legend.position = "None"), 
+       filename = here(plot_dir, "spe_erc_ggridge.png"), 
+       height = 12, width = 12)
+
 
 #### Read in Annotations ####
-
-pd <- as.data.frame(colData(spe)) |>
-    select(sample_id, spot_name = key, in_tissue, scran_low_lib_size_edge, scran_discard)
 
 qc_anno <- map_dfr(list.files(here("processed-data", "02_build_spe", "03_qc_app"), full.names = TRUE), 
                    ~read.csv(.x) |> mutate(file = gsub("spatialLIBD_ManualAnnotation_|.csv", "",basename(.x)))) |>
@@ -267,7 +267,8 @@ qc_anno |> dplyr::count(ManualAnnotation, qc_anno)
 qc_anno_clean <- qc_anno |>
     select(sample_id, sample_id, spot_name, qc_anno)  |>
     unique() |>
-    left_join(pd) |>
+    left_join(pd |>
+                  select(sample_id, spot_name = key, in_tissue, scran_low_lib_size_edge, scran_discard)) |>
     filter(in_tissue) |>
     as.data.frame()
 
