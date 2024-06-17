93a1dfdbb6cc311f9a9e7ad102c5bdb7d3d612ef
Louise Huuki <lahuuki@gmail.com>
2024-06-17 17:16:45 -0400

    Plot all spot sweep outout, save as csv

--- code/02_build_spe/04_SpotSweeper.R
+++ code/02_build_spe/04_SpotSweeper.R
@@ -4,6 +4,8 @@ library("SpotSweeper")
 library("spatialLIBD")
 library("here")
 library("sessioninfo")
+library("ggpubr")
+
 
 plot_dir <- here("plots", "02_build_spe", "04_SpotSweeper")
 if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
@@ -84,73 +86,23 @@ spe$local_outliers <- as.logical(spe$sum_umi_outliers) |
 
 table(spe$local_outliers)
 
-library(ggpubr)
 
-# library size
-p1 <- plotQC(spe,
-             metric = "sum_umi_log",
-             outliers = "sum_umi_outliers", point_size = 1.1
-) +
-    ggtitle("Sum UMI")
-
-# unique genes
-p2 <- plotQC(spe,
-             metric = "sum_gene_log",
-             outliers = "sum_gene_outliers", point_size = 1.1
-) +
-    ggtitle("Sum Genes")
-
-# mitochondrial percent
-p3 <- plotQC(spe,
-             metric = "expr_chrM_ratio",
-             outliers = "expr_chrM_ratio_outliers", point_size = 1.1
-) +
-    ggtitle("ChrM Ratio")
-
-# all local outliers
-p4 <- plotQC(spe,
-             metric = "sum_umi_log",
-             outliers = "local_outliers", point_size = 1.1, stroke = 0.75
-) +
-    ggtitle("All Local Outliers")
-
-# plot
-plot_list <- list(p1, p2, p3, p4)
-ggarrange(
-    plotlist = plot_list,
-    ncol = 2, nrow = 2,
-    common.legend = FALSE
-)
-
-# find artifacts using SpotSweeper
-
-spe <- findArtifacts(spe,
-                     mito_percent = "expr_chrM_ratio",
-                     mito_sum = "expr_chrM",
-                     n_rings = 5,
-                     name = "artifact"
-)
+#### find artifacts using SpotSweeper ####
+## Only works one sample at a time
+# spe <- findArtifacts(spe,
+#                      mito_percent = "expr_chrM_ratio",
+#                      mito_sum = "expr_chrM",
+#                      n_rings = 5,
+#                      name = "artifact"
+# )
 
 # Error in .set_internal_all(x, value, getfun = int_colData, setfun = `int_colData<-`,  : 
 #                                invalid 'value' in 'reducedDims(<SpatialExperiment>) <- value'
 #                            each element of 'value' should have number of rows equal to 'ncol(x)'
-# 4: In .check_reddim_names(x, value[[v]], withDimnames = TRUE, vname = sprintf("value[[%s]]",  :
-#     non-NULL 'rownames(value[[4]])' should be the same as 'colnames(x)' for 'reducedDims<-'. This will
-#   be an error in the next release of Bioconductor.
-#  > traceback()
-# 5: stop("invalid 'value' in '", funstr, "(<", class(x), ">) <- value'\n", 
-#         "each element of 'value' should have number of ", vdimstr, 
-#         " equal to '", xdimstr, "(x)'")
-# 4: .set_internal_all(x, value, getfun = int_colData, setfun = `int_colData<-`, 
-#                      key = .red_key, convertfun = NULL, xdimfun = ncol, vdimfun = nrow, 
-#                      funstr = "reducedDims", xdimstr = "ncol", vdimstr = "rows")
-# 3: `reducedDims<-`(`*tmp*`, value = reducedDims(spe.temp))
-# 2: `reducedDims<-`(`*tmp*`, value = reducedDims(spe.temp))
-# 1: findArtifacts(spe, mito_percent = "expr_chrM_ratio", mito_sum = "expr_chrM", 
-#                 n_rings = 5, name = "artifact")                           
-#                           
-# 
-artifact_list <- purrr::map(unique(spe$sample_id), function(samp){
+
+set.seed(20240617)
+
+artifact_df <- purrr::map_dfr(unique(spe$sample_id), function(samp){
     message(Sys.time(), " - ", samp)
     spe_temp <- findArtifacts(spe[,spe$sample_id == samp],
                               mito_percent = "expr_chrM_ratio",
@@ -159,13 +111,102 @@ artifact_list <- purrr::map(unique(spe$sample_id), function(samp){
                               name = "artifact"
     )
     
-    print(plotQC(spe_temp,
-           metric = "expr_chrM_ratio",
-           outliers = "artifact", 
-           point_size = 1.1, 
-           stroke = 0.75
-    ))
+    # print(plotQC(spe_temp,
+    #        metric = "expr_chrM_ratio",
+    #        outliers = "artifact", 
+    #        point_size = 1.1, 
+    #        stroke = 0.75
+    # ))
     
-    return(spe$artifact)
+    return(as.data.frame(colData(spe_temp)[,c("sample_id", "key", "artifact")]))
 })
+
+head(artifact_df)
+
+# colData(spe) <- merge(colData(spe), artifact_df)
+identical(spe$key, artifact_df$key)
+spe$artifact <- artifact_df$artifact
+
+
+#### save spot sweeper data ####
+spotsweeper_data <- as.data.frame(colData(spe)[,c("sample_id", "key", "array_row", "array_col", "sum_umi_outliers", "sum_gene_outliers", "expr_chrM_ratio_outliers", "local_outliers", "artifact")])
+head(spotsweeper_data)
+write.csv(spotsweeper_data, file = here(data_dir, "SpotSweeper_data.csv"))
+
+# dryspots <- c("V13B23-364_A1", "V13B23-365_B1", "V13B23-363_B1")
+# Br5599 - artifact is not in corner (not sure this is a dry corner)
+# Br6085 - finds vascular tissue 
+# Br6098 - flagged corner + top edge, but lots of other tissue
+table(spe$BrNum[which(spe$Visium_slide %in% dryspots)])
+
+#### plotting ####
+plot_all_spot_sweep <- function(spe, sample = unique(spe$sample_id)[1]){
+    
+    spe <- spe[,spe$sample_id == sample]
+    
+    # library size
+    p1 <- plotQC(spe,
+                 metric = "sum_umi_log",
+                 outliers = "sum_umi_outliers", point_size = 1.1
+    ) +
+        ggtitle(paste(sample, "Sum UMI"))
+    
+    # unique genes
+    p2 <- plotQC(spe,
+                 metric = "sum_gene_log",
+                 outliers = "sum_gene_outliers", point_size = 1.1
+    ) +
+        ggtitle("Sum Genes")
+    
+    # mitochondrial percent
+    p3 <- plotQC(spe,
+                 metric = "expr_chrM_ratio",
+                 outliers = "expr_chrM_ratio_outliers", point_size = 1.1
+    ) +
+        ggtitle("ChrM Ratio")
+    
+    # all local outliers
+    p4 <- plotQC(spe,
+                 metric = "sum_umi_log",
+                 outliers = "local_outliers", point_size = 1.1, stroke = 0.75
+    ) +
+        ggtitle("All Local Outliers")
+    
+    ## artifact
+    p5 <- plotQC(spe,
+                 metric = "sum_umi_log",
+                 outliers = "artifact", point_size = 1.1, stroke = 0.75
+    ) +
+        ggtitle("Artifact")
+    
+    # plot
+    plot_list <- list(p1, p2, p3, p4, p5)
+    ggarrange(
+        plotlist = plot_list,
+        ncol = 3, nrow = 2,
+        common.legend = FALSE
+    )
+} 
+
+pdf(here(plot_dir, "SpotSweeper_ERC.pdf"), width = 12, height = 10)
+purrr::map(sort(unique(spe$sample_id)), ~print(plot_all_spot_sweep(spe = spe, sample = .x)))
+dev.off()
+
+## Consistent Outliers
+spotsweeper_data |>
+    as.tibble() |>
+    filter(local_outliers) |>
+    # count(array_row, array_col, sum_umi_outliers, sum_gene_outliers, expr_chrM_ratio_outliers) |>
+    count(array_row, array_col) |>
+    arrange(-n)
+    
+#   array_row array_col     n
+# 1        47        77    28
+# 2        38        88    26
+# 3        57        99    24
+# 4        19        91    18
+# 5        49       103    15
+# 6        28        32    11
+# 7        53       117    10
+
 
