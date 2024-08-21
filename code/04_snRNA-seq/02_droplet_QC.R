247a8dbeaee75994a9430152e47835bc9f5f4222
Louise Huuki <lahuuki@gmail.com>
2024-08-21 17:43:06 -0400

    Plot post qc n nuc, add droplet detection, add repo info & slurmjob

--- code/04_snRNA-seq/02_droplet_QC.R
+++ code/04_snRNA-seq/02_droplet_QC.R
@@ -27,7 +27,7 @@ length(sample_list)
 sample_info <- read_csv(here("processed-data", "04_snRNA-seq", "erc_sn_sample_info.csv"))
 
 #### get droplet scores ####
-droplet_counts <- tibble(chromium_id = character(), pre_drop = numeric(), post_drop = numeric())
+droplet_counts <- tibble(chromium_id = character(), n_pre_drop = numeric(), n_post_drop = numeric())
 # droplet_counts <- data.frame(colnames = c("Sample", "pre_drop", "post_drop"))
 
 sce_list <- map(sample_list, function(sample){
@@ -45,7 +45,7 @@ sce_list <- map(sample_list, function(sample){
     
     message(Sys.time(), " - ", sample, ": Pre-Drop = ", ncol_preDrop, " and Post-Drop = ", ncol_postDrop)
     ## save pre-post drops to table
-    droplet_counts <<- droplet_counts |> add_row(chromium_id = sample, pre_drop = ncol_preDrop, post_drop = ncol_postDrop)
+    droplet_counts <<- droplet_counts |> add_row(chromium_id = sample, n_pre_drop = ncol_preDrop, n_post_drop = ncol_postDrop)
     
     return(sce)
 })
@@ -60,29 +60,30 @@ summary(droplet_counts)
 #                    3rd Qu.:1411650   3rd Qu.:5720  
 #                    Max.   :1732806   Max.   :7847 
 
-#### Compile sample qualit info ####
+#### Compile sample quality info ####
 ## retrieve median reads per cell
 cell_ranger_out_paths <- list.files(here("processed-data", "03_cellranger"))
 cell_ranger_metrics <- map_dfr(cell_ranger_out_paths, 
                                ~read_csv(here("processed-data", "03_cellranger", .x, "outs", "metrics_summary.csv")) |>
                                    mutate(chromium_id = .x, .before = 1))
 
-cell_ranger_metrics <- sample_info |> 
-    select(BrNum, chromium_id, exp_round, seq_round, Ancestry, APOE) |>
-    left_join(cell_ranger_metrics) |> 
+write_csv(cell_ranger_metrics, file = here(data_dir, "cell_ranger_metrics.csv"))
+
+## create QC summary
+sample_qc_summary <- sample_info |> 
+    dplyr::select(BrNum, chromium_id, exp_round, seq_round, Ancestry, APOE, APOE_carrier) |>
+    left_join(cell_ranger_metrics |> 
+                  dplyr::select(1:6)) |> 
     left_join(droplet_counts) 
 
 summary(cell_ranger_metrics)
 
-## maybe write later...?
-write_csv(cell_ranger_metrics, file = here(data_dir, "cell_ranger_metrics.csv"))
-
-#### Plot pot drop values ####
-#barplot
-droplet_barplot <- cell_ranger_metrics |>
-    ggplot(aes(x = reorder(BrNum, post_drop), y = post_drop)) +
+#### Plot  drop values ####
+# n nuclei barplot
+droplet_barplot <- sample_qc_summary |>
+    ggplot(aes(x = reorder(BrNum, n_post_drop), y = n_post_drop)) +
     geom_col() +
-    geom_text(aes(label = post_drop), nudge_y = -275, color = "white") +
+    geom_text(aes(label = n_post_drop), nudge_y = -275, color = "white") +
     theme_bw() +
     coord_flip() +
     labs(x = "Sample", y = "Non-empty Droplets")
@@ -90,8 +91,8 @@ droplet_barplot <- cell_ranger_metrics |>
 ggsave(droplet_barplot, filename = here(plot_dir, "erc_n_droplets_post_drop_barplot.png"))
 
 # boxplot by seq round
-droplet_boxplot_seq <- cell_ranger_metrics |>
-    ggplot(aes(x = as.factor(seq_round), y = post_drop)) +
+droplet_boxplot_seq <- sample_qc_summary |>
+    ggplot(aes(x = as.factor(seq_round), y = n_post_drop)) +
     geom_boxplot(outlier.shape = NA) +
     geom_point() +
     geom_text_repel(aes(label = BrNum), size = 2) + 
@@ -101,8 +102,8 @@ droplet_boxplot_seq <- cell_ranger_metrics |>
 ggsave(droplet_boxplot, filename = here(plot_dir, "erc_n_droplets_post_drop_boxplot_seq.png"))
 
 #boxplot by APOE
-droplet_boxplot_apoe <- cell_ranger_metrics |>
-    ggplot(aes(x = APOE, y = post_drop)) +
+droplet_boxplot_apoe <- sample_qc_summary |>
+    ggplot(aes(x = APOE, y = n_post_drop)) +
     geom_boxplot(outlier.shape = NA) +
     geom_point() +
     geom_text_repel(aes(label = BrNum), size = 2) + 
@@ -111,9 +112,19 @@ droplet_boxplot_apoe <- cell_ranger_metrics |>
 
 ggsave(droplet_boxplot_apoe, filename = here(plot_dir, "erc_n_droplets_post_drop_boxplot_apoe.png"))
 
+droplet_boxplot_apoe_carrier <- sample_qc_summary |>
+    ggplot(aes(x = APOE_carrier, y = n_post_drop)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_point() +
+    geom_text_repel(aes(label = BrNum), size = 2) + 
+    theme_bw() +
+    labs(y = "Non-empty Droplets")
+
+ggsave(droplet_boxplot_apoe_carrier, filename = here(plot_dir, "erc_n_droplets_post_drop_boxplot_apoe_carrier.png"))
+
 ## compare droplet scores to cell ranger n cell predictions
-post_drop_v_est_cells <- cell_ranger_metrics |>
-    ggplot(aes(post_drop, `Estimated Number of Cells`)) +
+post_drop_v_est_cells <- sample_qc_summary |>
+    ggplot(aes(n_post_drop, `Estimated Number of Cells`)) +
     geom_point() +
     geom_text_repel(aes(label = chromium_id), size = 2) +
     geom_abline(color = "red") +
@@ -122,8 +133,8 @@ post_drop_v_est_cells <- cell_ranger_metrics |>
 ggsave(post_drop_v_est_cells, filename = here(plot_dir, "erc_post_drop_v_est_cells.png"))
 
 ## compare cells vs. mean reads
-post_drop_v_mean_reads <- cell_ranger_metrics |>
-    ggplot(aes(post_drop, `Mean Reads per Cell`)) +
+post_drop_v_mean_reads <- sample_qc_summary |>
+    ggplot(aes(n_post_drop, `Mean Reads per Cell`)) +
     geom_point() +
     geom_text_repel(aes(label = chromium_id), size = 2) +
     labs(x = "n non-empty droplets (DropletUtils)") +
@@ -147,7 +158,7 @@ dim(sce)
 colData(sce)
 
 sce$path <- sce$Sample
-sce$chromiums_id <- gsub(".*?([0-9]+c_ERC_SVB)/outs.*?$", "\\1", sce$Sample)
+sce$chromium_id <- gsub(".*?([0-9]+c_ERC_SVB)/outs.*?$", "\\1", sce$Sample)
 
 pd <- as.data.frame(colData(sce)) |>
     left_join(sample_info) |>
@@ -156,9 +167,10 @@ pd <- as.data.frame(colData(sce)) |>
            seq_round = factor(seq_round),
            Ancestry = factor(Ancestry),
            APOE = factor(APOE),
+           APOE_carrier = factor(APOE_carrier),
            Sex = factor(Sex)
            ) |>
-    select(Barcode, sample_id, chromium_id, BrNum, APOE:seq_round, path)
+    dplyr::select(Barcode, sample_id, chromium_id, BrNum, APOE:seq_round, path)
 
 colData(sce) <- DataFrame(pd)
 
@@ -182,7 +194,7 @@ sce$high_mito <- isOutlier(sce$subsets_Mito_percent, nmads = 3, type = "higher",
 sce$low_sum <- isOutlier(sce$sum, log = TRUE, type = "lower", batch = sce$sample_id)
 sce$low_detected <- isOutlier(sce$detected, log = TRUE, type = "lower", batch = sce$sample_id)
 
-## drop with any fails
+## drop niclei that are outliers in any of the three metrics
 sce$discard_auto <- sce$high_mito | sce$low_sum | sce$low_detected
 table(sce$discard_auto)
 # FALSE   TRUE 
@@ -230,8 +242,140 @@ print(qc_detected_v_mito)
 print(qc_sum_v_detected)
 dev.off()
 
+#### Add qc counts to qc summary ####
+sample_qc_summary <- sample_qc_summary |>
+    left_join(as.data.frame(colData(sce)) |>
+                  group_by(BrNum) |>
+                  summarise(low_sum = sum(low_sum),
+                            low_detected = sum(low_detected), 
+                            high_mito = sum(high_mito),
+                            discard_auto = sum(discard_auto),
+                            # n_postQC2 = sum(!discard_auto), ## not working??
+                            n_postQC = n()-discard_auto))
 
+sample_qc_summary |>
+    summarise(total = sum(n_postQC),
+              median = median(n_postQC))
+
+sample_qc_summary |>
+    group_by(APOE_carrier) |>
+    summarise(total = sum(n_postQC),
+              median = median(n_postQC))
+
+#### plot post QC values ####
+
+n_nuc_qc_barplot <- sample_qc_summary |>
+    mutate(BrNum = fct_reorder(BrNum, n_post_drop)) |>
+    dplyr::select(BrNum, discard_auto, keep_auto = n_postQC) |>
+    pivot_longer(!BrNum, values_to = "n_nuclei", names_to = "QC_class") |>
+    group_by(BrNum) |>
+    arrange(desc(QC_class)) |>
+    mutate(text_y = cumsum(n_nuclei)) |>
+    ggplot(aes(x = BrNum, y = n_nuclei, fill = QC_class)) +
+    geom_col() +
+    geom_text(aes(label = n_nuclei, y = text_y), nudge_y = -200, color = "white", size = 2) +
+    theme_bw() +
+    coord_flip() +
+    labs(x = "Sample", y = "N Nuclei")
+
+ggsave(n_nuc_qc_barplot, filename = here(plot_dir, "erc_n_nuc_post_QC_barplot.png"))
+
+
+postQC_boxplot_seq <- sample_qc_summary |>
+    ggplot(aes(x = seq_round, y = n_postQC)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_point() +
+    geom_text_repel(aes(label = BrNum), size = 2) + 
+    theme_bw() +
+    labs(x = "Sequencing Round", y = "Nuclei Post-QC")
+
+ggsave(postQC_boxplot_seq, filename = here(plot_dir, "erc_n_post_QC_boxplot_seq.png"))
+
+postQC_boxplot_APOE_carrier <- sample_qc_summary |>
+    ggplot(aes(x = APOE_carrier, y = n_postQC)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_point() +
+    geom_text_repel(aes(label = BrNum), size = 2, color = "grey40") + 
+    theme_bw() +
+    labs(y = "Nuclei Post-QC")
+
+ggsave(postQC_boxplot_APOE_carrier, filename = here(plot_dir, "erc_n_post_QC_boxplot_APOE_carrier.png"), height = 5, width = 5)
+
+#### Doublet detection #### TODO
+## To speed up, run on sample-level top-HVGs - just take top 1000
+set.seed(821)
+
+colData(sce)$doubletScore <- NA
+
+for (i in splitit(sce$Sample)) {
+    sce_temp <- sce[, i]
+    ## To speed up, run on sample-level top-HVGs - just take top 1000
+    normd <- logNormCounts(sce_temp)
+    geneVar <- modelGeneVar(normd)
+    topHVGs <- getTopHVGs(geneVar, n = 1000)
+    
+    dbl_dens <- computeDoubletDensity(normd, subset.row = topHVGs)
+    colData(sce)$doubletScore[i] <- dbl_dens
+}
+
+summary(sce$doubletScore)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 0.0000  0.1439  0.4391  0.8215  1.0823 31.4607
+
+## Visualize doublet scores ##
+
+dbl_df <- colData(sce) %>%
+    as.data.frame() %>%
+    select(Sample, doubletScore)
+
+dbl_box_plot <- dbl_df %>%
+    ggplot(aes(x = reorder(Sample, doubletScore, FUN = median), y = doubletScore)) +
+    geom_boxplot() +
+    labs(x = "Sample") +
+    geom_hline(yintercept = 5, color = "red", linetype = "dashed") +
+    coord_flip() +
+    my_theme
+
+ggsave(dbl_box_plot, filename = here(plot_dir, "doublet_scores_boxplot.png"))
+
+dbl_density_plot <- dbl_df %>%
+    ggplot(aes(x = doubletScore)) +
+    geom_density() +
+    labs(x = "doublet score") +
+    facet_grid(Sample ~ .) +
+    theme_bw()
+
+ggsave(dbl_density_plot, filename = here(plot_dir, "doublet_scores_desnity.png"), height = 17)
+
+dbl_df %>%
+    group_by(Sample) %>%
+    summarize(
+        median = median(doubletScore),
+        q95 = quantile(doubletScore, .95),
+        drop = sum(doubletScore >= 5),
+        drop_precent = 100 * drop / n()
+    )
+
+
+#### Save Data ####
+write_csv(sample_qc_summary, file = here("processed-data", "04_snRNA-seq", "02_droplet_QC", "erc_sn_sample_QC_summary.csv"))
 
 ## Save preQC-sce
 save(sce, file = here("processed-data", "sce_objects", 
-                               "sce.Rdata"))
+                               "sce_erc_preQC.Rdata"))
+
+## save HDF5
+saveHDF5SummarizedExperiment(sce,
+                             dir = here("processed-data", "sce_objects", "hdf5_sce"), prefix = "", replace = FALSE,
+                             chunkdim = NULL, level = NULL, as.sparse = TRUE,
+                             verbose = TRUE
+)
+
+# slurmjobs::job_single('02_droplet_QC', create_shell = TRUE, memory = '25G', command = "Rscript 02_droplet_QC.R")
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
