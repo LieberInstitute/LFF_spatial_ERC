e570136ac001654a8f0fe79eff130996f0ab66e9
Louise Huuki <lahuuki@gmail.com>
2024-08-21 14:35:01 -0400

    Add colData to sce, add qc metrics, run isOutlier

--- code/04_snRNA-seq/02_droplet_QC.R
+++ code/04_snRNA-seq/02_droplet_QC.R
@@ -8,6 +8,10 @@ library("tidyverse")
 library("ggrepel")
 library("here")
 library("sessioninfo")
+library("EnsDb.Hsapiens.v86")
+library(scran)
+library(scuttle)
+library(scater)
 
 plot_dir <- here("plots", "04_snRNA-seq", "02_droplet_QC")
 if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
@@ -21,11 +25,6 @@ length(sample_list)
 
 #### get sample info ####
 sample_info <- read_csv(here("processed-data", "04_snRNA-seq", "erc_sn_sample_info.csv"))
-
-sample_info <- sample_info |> 
-    mutate(sample_id = gsub("Hs_", "", Brain))
-
-colnames(sample_info)
 
 #### get droplet scores ####
 droplet_counts <- tibble(chromium_id = character(), pre_drop = numeric(), post_drop = numeric())
@@ -44,9 +43,9 @@ sce_list <- map(sample_list, function(sample){
     ncol_postDrop = ncol(sce)
     # 3622
     
-    message(Sys.time(), "-", sample, ": Pre-Drop = ", ncol_preDrop, " and Post-Drop = ", ncol_postDrop)
+    message(Sys.time(), " - ", sample, ": Pre-Drop = ", ncol_preDrop, " and Post-Drop = ", ncol_postDrop)
     ## save pre-post drops to table
-    droplet_counts <<- droplet_counts |> add_row(Sample = sample, pre_drop = ncol_preDrop, post_drop = ncol_postDrop)
+    droplet_counts <<- droplet_counts |> add_row(chromium_id = sample, pre_drop = ncol_preDrop, post_drop = ncol_postDrop)
     
     return(sce)
 })
@@ -61,32 +60,58 @@ summary(droplet_counts)
 #                    3rd Qu.:1411650   3rd Qu.:5720  
 #                    Max.   :1732806   Max.   :7847 
 
+#### Compile sample qualit info ####
 ## retrieve median reads per cell
 cell_ranger_out_paths <- list.files(here("processed-data", "03_cellranger"))
 cell_ranger_metrics <- map_dfr(cell_ranger_out_paths, 
                                ~read_csv(here("processed-data", "03_cellranger", .x, "outs", "metrics_summary.csv")) |>
                                    mutate(chromium_id = .x, .before = 1))
 
-cell_ranger_metrics <- cell_ranger_metrics |> left_join(droplet_counts |> rename(chromium_id = Sample))
-
+cell_ranger_metrics <- sample_info |> 
+    select(BrNum, chromium_id, exp_round, seq_round, Ancestry, APOE) |>
+    left_join(cell_ranger_metrics) |> 
+    left_join(droplet_counts) 
 
 summary(cell_ranger_metrics)
 
+## maybe write later...?
 write_csv(cell_ranger_metrics, file = here(data_dir, "cell_ranger_metrics.csv"))
 
-droplet_counts |>
-    arrange(post_drop)
-
-droplet_boxplot <- droplet_counts |>
-    ggplot(aes(x = reorder(Sample, post_drop), y = post_drop)) +
+#### Plot pot drop values ####
+#barplot
+droplet_barplot <- cell_ranger_metrics |>
+    ggplot(aes(x = reorder(BrNum, post_drop), y = post_drop)) +
     geom_col() +
+    geom_text(aes(label = post_drop), nudge_y = -275, color = "white") +
     theme_bw() +
     coord_flip() +
-    # geom_hline(yintercept = 5000, color = "red", linetype = "dashed") +
     labs(x = "Sample", y = "Non-empty Droplets")
 
-ggsave(droplet_boxplot, filename = here(plot_dir, "erc_n_droplets_post_drop.png"))
+ggsave(droplet_barplot, filename = here(plot_dir, "erc_n_droplets_post_drop_barplot.png"))
 
+# boxplot by seq round
+droplet_boxplot_seq <- cell_ranger_metrics |>
+    ggplot(aes(x = as.factor(seq_round), y = post_drop)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_point() +
+    geom_text_repel(aes(label = BrNum), size = 2) + 
+    theme_bw() +
+    labs(x = "Sequencing Round", y = "Non-empty Droplets")
+
+ggsave(droplet_boxplot, filename = here(plot_dir, "erc_n_droplets_post_drop_boxplot_seq.png"))
+
+#boxplot by APOE
+droplet_boxplot_apoe <- cell_ranger_metrics |>
+    ggplot(aes(x = APOE, y = post_drop)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_point() +
+    geom_text_repel(aes(label = BrNum), size = 2) + 
+    theme_bw() +
+    labs(y = "Non-empty Droplets")
+
+ggsave(droplet_boxplot_apoe, filename = here(plot_dir, "erc_n_droplets_post_drop_boxplot_apoe.png"))
+
+## compare droplet scores to cell ranger n cell predictions
 post_drop_v_est_cells <- cell_ranger_metrics |>
     ggplot(aes(post_drop, `Estimated Number of Cells`)) +
     geom_point() +
@@ -96,7 +121,7 @@ post_drop_v_est_cells <- cell_ranger_metrics |>
 
 ggsave(post_drop_v_est_cells, filename = here(plot_dir, "erc_post_drop_v_est_cells.png"))
 
-
+## compare cells vs. mean reads
 post_drop_v_mean_reads <- cell_ranger_metrics |>
     ggplot(aes(post_drop, `Mean Reads per Cell`)) +
     geom_point() +
@@ -108,16 +133,98 @@ post_drop_v_mean_reads <- cell_ranger_metrics |>
 ggsave(post_drop_v_mean_reads, filename = here(plot_dir, "erc_post_drop_v_mean_reads.png"))
 
 #### Build SCE ####
-map_int(sce_list, nrow)
+table(map_int(sce_list, nrow))
 
 ## check row data is identical
 all(map_lgl(sce_list, ~identical(rownames(sce_list[[1]]), rownames(.x))))
 
 ## rbind to one sce
-sce_erc_preQC <- do.call("cbind", sce_list)
-dim(sce_erc_preQC)
+sce <- do.call("cbind", sce_list)
+dim(sce)
 # [1]  38606 154072
 
+#### Build colData ####
+colData(sce)
+
+sce$path <- sce$Sample
+sce$chromiums_id <- gsub(".*?([0-9]+c_ERC_SVB)/outs.*?$", "\\1", sce$Sample)
+
+pd <- as.data.frame(colData(sce)) |>
+    left_join(sample_info) |>
+    mutate(Barcode = paste0(Barcode, "_", BrNum),
+           exp_round = factor(exp_round),
+           seq_round = factor(seq_round),
+           Ancestry = factor(Ancestry),
+           APOE = factor(APOE),
+           Sex = factor(Sex)
+           ) |>
+    select(Barcode, sample_id, chromium_id, BrNum, APOE:seq_round, path)
+
+colData(sce) <- DataFrame(pd)
+
+
+#### Compute QC metrics ####
+
+location <- mapIds(EnsDb.Hsapiens.v86, keys = rowData(sce)$ID, 
+                   column = "SEQNAME", keytype = "GENEID")
+# Unable to map 4792 of 38606 requested IDs. 
+
+sce <- scuttle::addPerCellQC(
+    sce,
+    # subsets = list(Mito = list(Mito = which(location=="MT")))
+    subsets = list(Mito = which(location=="MT"))
+    # ,
+    # BPPARAM = BiocParallel::MulticoreParam(4)
+)
+
+## find outliers with Scran
+sce$high_mito <- isOutlier(sce$subsets_Mito_percent, nmads = 3, type = "higher", batch = sce$seq_round)
+sce$low_sum <- isOutlier(sce$sum, log = TRUE, type = "lower", batch = sce$seq_round)
+sce$low_detected <- isOutlier(sce$detected, log = TRUE, type = "lower", batch = sce$seq_round)
+
+## drop with any fails
+sce$discard_auto <- sce$high_mito | sce$low_sum | sce$low_detected
+table(sce$discard_auto)
+# FALSE   TRUE 
+# 125682  28390
+
+addmargins(table(sce$BrNum, sce$discard_auto))
+
+#### QC plots ####
+pdf(here(plot_dir, "erc_sn_QC_outliers.pdf"), width = 21)
+## Mito rate
+plotColData(sce, x = "BrNum", y = "subsets_Mito_percent", colour_by = "high_mito") +
+    ggtitle("Mito Precent") +
+    facet_wrap(~ sce$seq_round, scales = "free_x", nrow = 1)
+
+# ## low sum
+plotColData(sce, x = "BrNum", y = "sum", colour_by = "low_sum") +
+    scale_y_log10() +
+    ggtitle("Total count") +
+    facet_wrap(~ sce$seq_round, scales = "free_x", nrow = 1)
+
+# ## low detected
+plotColData(sce, x = "BrNum", y = "detected", colour_by = "low_detected") +
+    scale_y_log10() +
+    ggtitle("Detected features") +
+    facet_wrap(~ sce$seq_round, scales = "free_x", nrow = 1)
+
+# Mito rate vs n detected features
+plotColData(sce,
+            x = "detected", y = "subsets_Mito_percent",
+            colour_by = "discard_auto", point_size = 2.5, point_alpha = 0.5
+)
+
+# Detected features vs total count
+plotColData(sce,
+            x = "sum", y = "detected",
+            colour_by = "discard_auto", point_size = 2.5, point_alpha = 0.5
+)
+
+dev.off()
+
+
+
 ## Save preQC-sce
-save(sce_erc_preQC, file = here("processed-data", "sce_objects", 
-                               "sce_erc_preQC.Rdata"))
+save(sce, file = here("processed-data", "sce_objects", 
+                               "sce.Rdata"))
