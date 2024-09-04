78691022a43a0aa924ed8dd94eaeec6c031fa0d1
Louise Huuki <lahuuki@gmail.com>
2024-09-04 14:17:24 -0400

    Simplify object name, add slurmjob

--- code/04_snRNA-seq/05_harmony_correction.R
+++ code/04_snRNA-seq/05_harmony_correction.R
@@ -5,22 +5,23 @@ library("HDF5Array")
 library("here")
 library("sessioninfo")
 
-## Choose Correction
-correction = ""
 
-## Load empty-free sce data
+## Load "uncorrected" sce data (postQC, GLMPCA)
 message(Sys.time(), " - load data")
-sce_uncorrected <- loadHDF5SummarizedExperiment(here("processed-data", "spe_objects", "sce_uncorrected"))
+sce <- loadHDF5SummarizedExperiment(here("processed-data", "sce_objects", "sce_uncorrected"))
+sce
 
-stopifnot(correction %in% colnames(colData(sce_uncorrected)))
+## Choose Correction
+correction = "sample_id"
+stopifnot(correction %in% colnames(colData(sce)))
 message("Correcting by: ", correction)
 
 ## Run harmony
 ## needs PCA
-reducedDim(sce_uncorrected, "PCA") <- reducedDim(sce_uncorrected, "GLMPCA_approx")
+reducedDim(sce, "PCA") <- reducedDim(sce, "GLMPCA_approx")
 
 message("running Harmony - ", Sys.time())
-sce <- RunHarmony(sce_uncorrected, group.by.vars = correction, verbose = TRUE)
+sce <- RunHarmony(sce, group.by.vars = correction, verbose = TRUE)
 
 ## Remove redundant PCA
 reducedDim(sce, "PCA") <- NULL
@@ -35,13 +36,13 @@ sce <- runUMAP(sce, dimred = "HARMONY")
 
 message("Done TSNE + UMAP - Saving data...", Sys.time())
 
-save(sce, file = here("processed-data", "03_build_sce", paste0("sce_harmony_", correction, ".Rdata")))
+# save(sce, file = here("processed-data", "spe_objects", paste0("sce_harmony_", correction, ".Rdata")))
+save(sce, file = here("processed-data", "spe_objects", "sce_harmony.Rdata"))
 
-# saveHDF5SummarizedExperiment(sce_uncorrected, dir = here("processed-data", "03_build_sce", paste0("sce_harmony_", correction)))
-saveHDF5SummarizedExperiment(sce_uncorrected, dir = here("processed-data", "spe_objects", "sce_harmony"))
+# saveHDF5SummarizedExperiment(sce, dir = here("processed-data", "03_build_sce", paste0("sce_harmony_", correction)))
+saveHDF5SummarizedExperiment(sce, dir = here("processed-data", "spe_objects", "sce_harmony"))
 
-
-# slurm_jobs::job_single('05_harmony_correction', create_shell = TRUE, queue= 'bluejay', memory = '25G', command = "Rscript 05_harmony_correction.R Sample")
+# slurmjobs::job_single('05_harmony_correction', create_shell = TRUE, memory = '100G', command = "Rscript 05_harmony_correction.R")
 
 ## Reproducibility information
 print("Reproducibility information:")
