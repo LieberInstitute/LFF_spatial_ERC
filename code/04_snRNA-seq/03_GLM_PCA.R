ce26cde1c3875e1885279555a36692c1c5e07de1
Louise Huuki <lahuuki@gmail.com>
2024-09-04 14:03:30 -0400

    Fix formatting and HDF5 save

--- code/04_snRNA-seq/03_GLM_PCA.R
+++ code/04_snRNA-seq/03_GLM_PCA.R
@@ -14,14 +14,14 @@ plot_dir <- here("plots", "04_snRNA-seq", "03_GLM_PCA")
 if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
 ## Load HD5F
-message(Sys.time(), "- load post-QC sce")
-sce <- loadHDF5SummarizedExperiment(dir = here("processed-data", "sce_objects", "hdf5_sce_postQC"), prefix = "")
-dim(sce)
+message(Sys.time(), " - load post-QC sce")
+sce <- loadHDF5SummarizedExperiment(dir = here("processed-data", "sce_objects", "hdf5_sce_postQC"))
+sce
 
-#### DEviance Feat Selection ####
+#### Deviance Feat Selection ####
 set.seed(606)
 
-message("running Deviance Feat. Selection - ", Sys.time())
+message(Sys.time(), " - running Deviance Feat. Selection")
 sce <- devianceFeatureSelection(sce,
     assay = "counts", fam = "binomial", sorted = F,
     batch = as.factor(sce$seq_round)
@@ -37,33 +37,43 @@ plot(sort(rowData(sce)$binomial_deviance, decreasing = T),
 abline(v = 2000, lty = 2, col = "red")
 dev.off()
 
-message("running nullResiduals - ", Sys.time())
+message(Sys.time(), " - running nullResiduals - ")
 sce <- nullResiduals(sce,
-    assay = "counts", fam = "binomial", # default params
+    assay = "counts", 
+    fam = "binomial", # default params
     type = "deviance"
 )
 
 
 hdgs.hb <- rownames(sce)[order(rowData(sce)$binomial_deviance, decreasing = T)][1:2000]
 
-message("running PCA - ", Sys.time())
-sce_uncorrected <- runPCA(sce,
+message(Sys.time(), " - running PCA - ")
+sce <- runPCA(sce,
     exprs_values = "binomial_deviance_residuals",
-    subset_row = hdgs.hb, ncomponents = 100,
+    subset_row = hdgs.hb,
+    ncomponents = 100,
     name = "GLMPCA_approx",
     BSPARAM = BiocSingular::IrlbaParam()
 )
 
 #### Reduce Dims####
 
-message("running TSNE - ", Sys.time())
-sce_uncorrected <- runTSNE(sce_uncorrected, dimred = "GLMPCA_approx")
+message(Sys.time(), " - running TSNE")
+sce <- runTSNE(sce, dimred = "GLMPCA_approx")
 
-message("running UMAP - ", Sys.time())
-sce_uncorrected <- runUMAP(sce_uncorrected, dimred = "GLMPCA_approx")
+message(Sys.time(), " - running UMAP")
+sce <- runUMAP(sce, dimred = "GLMPCA_approx")
 
-message("Saving Data - ", Sys.time())
-saveHDF5SummarizedExperiment(sce_uncorrected, dir = here("processed-data", "sce_objects", "sce_uncorrected"))
+
+message("SCE contents:")
+sce
+
+message("Reduced Dim Names:")
+reducedDimNames(sce)
+
+data_dir <- here("processed-data", "sce_objects", "sce_uncorrected")
+message(Sys.time(), " - Saving Data to: ", data_dir)
+saveHDF5SummarizedExperiment(sce, dir = data_dir, replace = TRUE)
 
 # slurmjobs::job_single('03_GLM_PCA', create_shell = TRUE, memory = '100G', command = "Rscript 03_GLM_PCA.R")
 
