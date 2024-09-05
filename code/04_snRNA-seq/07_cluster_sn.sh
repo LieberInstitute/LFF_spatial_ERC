7c90362a39f56d922a47e93c8df296752986703a
lahuuki <lahuuki@gmail.com>
2024-09-05 13:26:21 -0400

    Add cluster sh files

---
+++ code/04_snRNA-seq/07_cluster_sn.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -p katun
+#SBATCH --mem=200G
+#SBATCH --job-name=07_cluster_sn
+#SBATCH -c 1
+#SBATCH -o logs/07_cluster_sn.txt
+#SBATCH -e logs/07_cluster_sn.txt
+#SBATCH --mail-type=ALL
+
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load the R module
+module load conda_R/4.3.x
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 07_cluster_sn.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using slurmjobs version 1.1.0
+## available from http://research.libd.org/slurmjobs/
