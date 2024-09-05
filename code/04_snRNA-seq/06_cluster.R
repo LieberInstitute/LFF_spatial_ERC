67ab07f8758b59dbd219ed74261194b7ff9d5181
Louise Huuki <lahuuki@gmail.com>
2024-09-05 11:12:18 -0400

    Copy clustering script from https://github.com/LieberInstitute/DLPFC_snRNAseq/blob/222850c7839b3029794b951f52f7bb6a76dbf6f7/code/03_build_sce/06_cluster.R

---
+++ code/04_snRNA-seq/06_cluster.R
@@ -0,0 +1,31 @@
+library("SingleCellExperiment")
+library("jaffelab")
+library("scater")
+library("scran")
+library("here")
+library("sessioninfo")
+
+## Best normalization result
+load(here("processed-data", "03_build_sce", "sce_harmony_Sample.Rdata"), verbose = TRUE)
+
+message("running buildSNNGraph - ", Sys.time())
+snn.gr <- buildSNNGraph(sce, k = 20, use.dimred = "HARMONY")
+
+message("running walktrap - ", Sys.time())
+clusters <- igraph::cluster_walktrap(snn.gr)$membership
+
+table(clusters)
+message("saving data - ", Sys.time())
+save(clusters, file = here("processed-data", "03_build_sce", "clusters.Rdata"))
+
+## Save final sce w/ annotations
+# save(sce, file = here("processed-data", "sce", "sce_DLPFC.Rdata"))
+
+# sgejobs::job_single('06_cluster', create_shell = TRUE, queue= 'bluejay', memory = '100G', command = "Rscript 06_cluster.R subject")
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
