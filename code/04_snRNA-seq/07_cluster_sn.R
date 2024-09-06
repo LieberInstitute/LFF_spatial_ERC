975e5f2c391ece23e907c1172ff05b020f9ed284
Louise Huuki <lahuuki@gmail.com>
2024-09-06 12:28:30 -0400

    Use slurm::job_loop to run k=10,15,20

--- code/04_snRNA-seq/07_cluster_sn.R
+++ code/04_snRNA-seq/07_cluster_sn.R
@@ -6,8 +6,27 @@ library("jaffelab")
 library("scater")
 library("scran")
 library("HDF5Array")
+library("getopt")
 library("here")
 library("sessioninfo")
+
+# Import command-line parameters
+spec <- matrix(
+    c(
+        c("k"),
+        c("k"),
+        rep("1", 1),
+        rep("integer", 1),
+        rep("k for buildSNNGraph", 1)
+    ),
+    ncol = 5
+)
+opt <- getopt(spec)
+
+print("Using the following parameters:")
+print(opt)
+
+k = opt$k
 
 ## Prep directories
 data_dir <- here("processed-data", "04_snRNA-seq", "07_cluster_sn")
@@ -18,8 +37,6 @@ message(Sys.time(), "- load Harmony corrected sce")
 sce <- loadHDF5SummarizedExperiment(dir = here("processed-data", "sce_objects", "sce_harmony"))
 sce
 
-## define k
-k=20
 
 ## set seed
 set.seed(20240905)
@@ -37,10 +54,12 @@ table(clusters)
 message(Sys.time() , " - saving data")
 save(clusters, file = here(data_dir, sprintf("walktrap_snn_k%02d_clusters.Rdata", k)))
 
-## Save final sce w/ annotations
-# save(sce, file = here("processed-data", "sce", "sce_DLPFC.Rdata"))
-
 # slurmjobs::job_single('07_cluster_sn', create_shell = TRUE, memory = '100G', command = "Rscript 07_cluster_sn.R")
+# slurmjobs::job_loop(
+#     loops = list(k = c("10", "15", "20")),
+#     name = "07_cluster_sn",
+#     create_shell = TRUE
+# )
 
 ## Reproducibility information
 print("Reproducibility information:")
--- code/04_snRNA-seq/07_cluster_sn.sh
+++ code/04_snRNA-seq/07_cluster_sn.sh
@@ -3,10 +3,19 @@
 #SBATCH --mem=200G
 #SBATCH --job-name=07_cluster_sn
 #SBATCH -c 1
-#SBATCH -o logs/07_cluster_sn.txt
-#SBATCH -e logs/07_cluster_sn.txt
+#SBATCH -o /dev/null
+#SBATCH -e /dev/null
 #SBATCH --mail-type=ALL
+#SBATCH --array=1-3%20
 
+## Define loops and appropriately subset each variable for the array task ID
+all_k=(10 15 20)
+k=${all_k[$(( $SLURM_ARRAY_TASK_ID / 1 % 3 ))]}
+
+## Explicitly pipe script output to a log
+log_path=logs/07_cluster_sn_loop_${k}_${SLURM_ARRAY_TASK_ID}.txt
+
+{
 set -e
 
 echo "**** Job starts ****"
@@ -26,10 +35,12 @@ module load conda_R/4.3.x
 module list
 
 ## Edit with your job command
-Rscript 07_cluster_sn.R
+Rscript 07_cluster_sn_loop.R --k ${k}
 
 echo "**** Job ends ****"
 date
+
+} > $log_path 2>&1
 
 ## This script was made using slurmjobs version 1.1.0
 ## available from http://research.libd.org/slurmjobs/
