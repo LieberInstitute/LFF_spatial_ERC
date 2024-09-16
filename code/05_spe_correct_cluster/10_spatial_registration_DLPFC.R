fbf938d17c47183a29365fa3c5cd5fa0ffb34e64
Louise Huuki <lahuuki@gmail.com>
2024-09-16 18:08:34 -0400

    Spatially register to spatiaDLPFC domains too

--- code/05_spe_correct_cluster/10_spatial_registration_DLPFC.R
+++ code/05_spe_correct_cluster/10_spatial_registration_DLPFC.R
@@ -14,8 +14,7 @@ plot_dir <- here("plots", "05_spe_correct_cluster", "10_spatial_registration_DLP
 if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
 #### get reference layer enrichment statistics ####
-layer_modeling_results <- fetch_data(type = "modeling_results")
-
+layer_modeling_results <- map(c(HumanPilot = "modeling_results", spatialDLPFC = "spatialDLPFC_Visium_modeling_results"), fetch_data)
 
 #### load data ####
 modeling_fn <- list.files(here("processed-data", "05_spe_correct_cluster", "08_model_pseudobulk", "BayesSpace_PCA_Harmony"),
@@ -34,23 +33,33 @@ cor_humanPilot <- map2(modeling_fn, names(modeling_fn), function(fn, name){
     registration_t_stats <- erc_modeling_results$enrichment[, grep("^t_stat", colnames(erc_modeling_results$enrichment))]
     colnames(registration_t_stats) <- gsub("^t_stat_", "", colnames(registration_t_stats))
     
-    cor_layer <- layer_stat_cor(
-        stats = registration_t_stats,
-        modeling_results = layer_modeling_results,
-        model_type = "enrichment",
-        top_n = 100
-    )
     
-    pdf(here(plot_dir, sprintf("layer_stat_cor-humanPilot_vs_ERC-%s.pdf", name)))
-    layer_stat_cor_plot(cor_layer, max = max(cor_layer))
-    dev.off()
+    cor_layer <- map2(layer_modeling_results, names(layer_modeling_results), function(layer_mod, dataset){
+        
+        cor_layer <- layer_stat_cor(stats = registration_t_stats,
+                                    modeling_results = layer_mod,
+                                    model_type = "enrichment",
+                                    top_n = 100)
+        
+        pdf(here(plot_dir, sprintf("layer_stat_cor-%s_vs_ERC-%s.pdf", dataset, name)))
+        layer_stat_cor_plot(cor_layer, max = max(cor_layer))
+        dev.off()
+        
+        return(cor_layer)
+        
+    })
     
-    anno <- annotate_registered_clusters(
-        cor_stats_layer = cor_layer,
+    
+    anno <- map(cor_layer, ~annotate_registered_clusters(
+        cor_stats_layer = .x,
         confidence_threshold = 0.25,
         cutoff_merge_ratio = 0.25
-    )
+    ))
     
     return(list(cor_layer = cor_layer, layer_anno = anno))
 })
+
+
+## spatialDLPFC spatial domain annotations 
+"processed-data/rdata/spe/08_spatial_registration/bayesSpace_layer_annotations.csv"
 
