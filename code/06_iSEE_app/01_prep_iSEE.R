ebef92ebf71d5cbc4a5e842b926f631856a3f1d0
Louise Huuki <lahuuki@gmail.com>
2024-09-17 17:04:26 -0400

    Rename to 01_prep_iSEE, convert logcounts to sparseMatrix for portability

--- code/06_iSEE_app/01_prep_iSEE.R
+++ code/06_iSEE_app/01_prep_iSEE.R
@@ -3,6 +3,7 @@ library("here")
 library("lobstr")
 library("sessioninfo")
 library("HDF5Array")
+library("Matrix")
 
 ## Load HD5F sce
 message(Sys.time(), "- load Harmony corrected sce")
@@ -14,6 +15,12 @@ assayNames(sce)
 # [1] "counts"                      "binomial_deviance_residuals" "logcounts"
 assays(sce)$counts <- NULL
 assays(sce)$binomial_deviance_residuals <- NULL
+
+class(logcounts(sce))
+# [1] "DelayedMatrix"
+
+message(Sys.time(), "- Convert logcounts to sparse Matrix")
+logcounts(sce) <- as(logcounts(sce), "sparseMatrix")  
 
 ## Drop metadata we don't need
 metadata(sce) <- list()
@@ -31,9 +38,10 @@ lobstr::obj_size(sce)
 # 290.37 MB
 
 sce 
-
 saveRDS(sce, file = here("code", "06_iSEE_app", "sce_ERC_iSEE.rds"))
-saveRDS(sn_colors, file = here("code", "06_iSEE_app", "sn_colors.rds"))
+# saveRDS(sn_colors, file = here("code", "06_iSEE_app", "sn_colors.rds"))
+
+# slurmjobs::job_single('01_prep', create_shell = TRUE, memory = '25G', command = "Rscript 02_prep.R")
 
 ## Reproducibility information
 print("Reproducibility information:")
