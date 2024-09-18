5e87417dd93a509ae33767af5b006d064c43333e
Louise Huuki <lahuuki@gmail.com>
2024-09-18 10:41:04 -0400

    Update iSEE app name + add rsconnect

--- code/06_iSEE_app/deploy.R
+++ code/06_iSEE_app/deploy.R
@@ -4,7 +4,7 @@ options(repos = BiocManager::repositories())
 rsconnect::deployApp(
     appDir = here("code", "06_iSEE_app"),
     appFiles = c("app.R", "sce_ERC_iSEE.rds"),
-    appName = "habenulaPilot_snRNAseq",
+    appName = "LFF_ERC_snRNA-seq",
     account = "libd",
     server = "shinyapps.io"
 )
