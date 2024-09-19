b38836ade74b9839e68fe64cbd770ee604d904aa
Louise Huuki <lahuuki@gmail.com>
2024-09-19 17:14:23 -0400

    Add modeling + pseudobulk data to app (ignore RDS objects)

--- code/07_spatialLIBD_app/app.R
+++ code/07_spatialLIBD_app/app.R
@@ -8,8 +8,11 @@ options("golem.app.prod" = TRUE)
 ## You need this to enable shinyapps to install Bioconductor packages
 options(repos = BiocManager::repositories())
 
-spe <- readRDS("spe_raw.rds")
-
+#### load data ####
+spe <- readRDS("spe_ERC_app.rds")
+## k09 data
+spe_pb_k09 <- readRDS("spe_pseudobulk-BayesSpace_PCA_Harmony_k09.rds")
+modeling_results_k09 <- readRDS("modeling_results-BayesSpace_PCA_Harmony_k09.rds")
 
 ## Quickly explore the data
 vars <- colnames(colData(spe))
@@ -26,7 +29,8 @@ spe_discrete_vars = c(
     "scran_qc_anno",
     "ss_qc_anno",
     "qc_anno_all",
-    "local_outliers"
+    "local_outliers",
+    sprintf("BayesSpace_PCA_Harmony_k%02d", 2:28)
 )
 
 
@@ -34,8 +38,8 @@ spe_discrete_vars <- spe_discrete_vars[spe_discrete_vars %in% vars]
 
 spatialLIBD::run_app(
     spe = spe,
-    sce_layer = NULL,
-    modeling_results = NULL,
+    sce_layer = spe_pb_k09,
+    modeling_results = modeling_results_k09,
     sig_genes = NULL,
     spe_discrete_vars = spe_discrete_vars,
     spe_continuous_vars = c(
