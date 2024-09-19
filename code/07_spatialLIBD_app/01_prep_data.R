3ea230f54895cafbb43527f10c2d34e1f3957811
Louise Huuki <lahuuki@gmail.com>
2024-09-19 16:51:13 -0400

    Reduce spe size, convert assays to sparseMatrix, qc anno already included

--- code/07_spatialLIBD_app/01_prep_data.R
+++ code/07_spatialLIBD_app/01_prep_data.R
@@ -1,45 +1,38 @@
 
 library("spatialLIBD")
+library("HDF5Array")
+library("Matrix")
 library("here")
 
 ## local data
-spe <- readRDS(here("processed-data", "spe_objects", "spe_raw.rds"))
+#### Load the data ####
+message(Sys.time(), " - Load HDF5 SPE")
+spe <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "spe_objects", "spe_ERC"))
+
 # lobstr::obj_size(spe)
-# 5.60 GB
+# 3.15 GB <- but assays are DelayedArray
 
+#### Convert Assays to sparaseMatrix for portability #### 
+assayNames(spe)
+# [1] "counts"    "logcounts"
 
-qc_file = here("processed-data", "05_QC_anno", "spe_qc_anno_clean.csv")
-if(file.exists(qc_file)){
-    qc_anno_clean <- read.csv(qc_file)
-    pd <- as.data.frame(colData(spe))
-    
-    pd <- pd |>
-        left_join(qc_anno_clean |> select(qc_anno, key = spot_name)) |>
-        mutate(qc_anno = factor(ifelse(is.na(qc_anno) & in_tissue, "None", qc_anno)),
-               scran_qc_anno = factor(ifelse(as.logical(scran_discard), paste0("scran-", qc_anno), as.character(qc_anno)))
-        )
-    
-    spe$qc_anno <- pd$qc_anno
-    spe$scran_qc_anno <- pd$scran_qc_anno
-}
+logcounts(spe) <- as(logcounts(spe), "sparseMatrix")  
+counts(spe) <- as(counts(spe), "sparseMatrix")  
 
 ## keep 10x UMAP
 reducedDimNames(spe)
-# [1] "10x_pca"  "10x_tsne" "10x_umap"
+# [1] "10x_pca"  "10x_tsne" "10x_umap" "PCA_p1"   "PCA_p2"   "TSNE"     "UMAP"     "HARMONY" 
 
-# reducedDims(spe)$`10x_tsne` <- NULL
-# reducedDims(spe)$`10x_pca` <- NULL
-
+reducedDims(spe)$`10x_pca` <- NULL
+reducedDims(spe)$`10x_tsne` <- NULL
+reducedDims(spe)$`10x_umap` <- NULL
 
 ## Drop images we don't really use in the app
 imgData(spe) <- imgData(spe)[
     !imgData(spe)$image_id %in% c("hires", "detected", "aligned"),
 ]
 
-## remove out_tissue spots?
-# spe <- spe[, spe$in_tissue]
-
 lobstr::obj_size(spe)
 # 2.79 GB
 
-saveRDS(spe, here("code", "02_build_spe", "02_QC_app", "spe_raw.rds"))
+saveRDS(spe, here("code", "07_spatialLIBD_app", "spe_ERC_app.rds"))
