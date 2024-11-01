c3492729d5b018386fd102721b3e9c6fce970907
Louise Huuki <lahuuki@gmail.com>
2024-11-01 17:47:56 +0000

    Process & save clusters by geneset, check PRECAST fail spots, improve ggsave params

--- code/05_spe_correct_cluster/06.5_PRECAST_plot.R
+++ code/05_spe_correct_cluster/06.5_PRECAST_plot.R
@@ -11,52 +11,83 @@ library("here")
 library("sessioninfo")
         
 #### define dirs ####
-data_dir <- here("processed-data", "05_spe_correct_cluster", "06_PRECAST")
+data_dir <- here("processed-data", "05_spe_correct_cluster", "06_PRECAST", "00_cluster_tab")
 if(!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
 
 plot_dir <- here("plots", "05_spe_correct_cluster", "06.5_PRECAST_plot")
 if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
-#### Load the data ####
+#### Load the spe data ####
 message(Sys.time(), " - Load HDF5 SPE")
 spe <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "spe_objects", "spe_ERC"))
 
-## PRECAST data
-precast_output <- list.files(data_dir, "*VG_k*", full.names = TRUE)
-names(precast_output) <- gsub(".csv", "", basename(precast_output))
+# clear any PRECAST columns
+colnames(colData(spe))
+colData(spe) <- colData(spe)[,!grepl("PRECAST", colnames(colData(spe)))]
 
-precast_cluster <- map2(precast_output, parse_number(names(precast_output)), 
-                        ~read.csv(.x) |>
-                           mutate(SpD = sprintf("Sp%02dD%02d", .y, cluster)))
+#### load PRECAST data ####
+genesets <- c("HVG", "SVG", "SVGplusMarkers")
+names(genesets) <- genesets
 
-head(precast_cluster[[1]])
+# Read in PRECAST outputs and add SpD formatting
+precast_cluster <- map(genesets, function(geneset){
+    ## list output files
+    precast_output <- list.files(data_dir, sprintf("*%s_k*", geneset), full.names = TRUE)
+    names(precast_output) <- gsub(".csv", "", basename(precast_output))
+    
+    ## read files
+    message(Sys.time(), sprintf(" - Reading %s output, %d files", geneset, length(precast_output)))
+    precast_cluster <- map2(precast_output, parse_number(names(precast_output)), 
+                            ~read.csv(.x) |>
+                                mutate(SpD = sprintf("Sp%02dD%02d", .y, cluster)))
+    
+    return(precast_cluster)
+})
 
-## key is in same order for all tables
-all(map_lgl(precast_cluster, ~identical(.x$key, precast_cluster$PRECAST_HVG_k02$key)))
+head(precast_cluster[[1]][[1]])
+map(precast_cluster, ~names(.x[1:5]))
 
-##combine
-precast_tab <- as.data.frame(map_dfc(precast_cluster, ~.x$SpD))
-rownames(precast_tab) <- precast_cluster$PRECAST_HVG_k02$key
-# precast_tab <- bind_cols(precast_cluster$k02[,c("key"),drop = FALSE], precast_tab)
-head(precast_tab)
+## Check key is in same order for all tables
+map(precast_cluster, function(pcc){
+    all(map_lgl(pcc, ~identical(.x$key, pcc[[1]]$key))) 
+    })
 
-## some spots are missing
-table(spe$key %in% rownames(precast_tab))
-# FALSE   TRUE 
-# 162 122040
+## combine to one cluster table per geneset
+precast_tab <- map(precast_cluster, function(cluster_data){
+    ## merge in to one table
+    precast_tab <- as.data.frame(map_dfc(cluster_data, ~.x$SpD))
+    rownames(precast_tab) <- cluster_data[[1]]$key
+    
+    ## some spots are missing
+    table(spe$key %in% rownames(precast_tab))
+    # FALSE   TRUE 
+    # 162 122040
+    
+    ## order and add rownames to match spe
+    precast_tab <- precast_tab[spe$key, ]
+    rownames(precast_tab) <- colnames(spe)
+    
+    return(precast_tab)
+})
 
-## order and add rownames to match spe
-precast_tab <- precast_tab[spe$key, ]
-rownames(precast_tab) <- colnames(spe)
+map(precast_tab, dim)
+map(precast_tab, ~.x[1:5,1:5])
 
 ## export to CVS
-write.csv(precast_tab, file = here(data_dir, "PRECAST_clusters.csv"))
+walk2(precast_tab, names(precast_tab), ~write.csv(.x, file = here(data_dir, sprintf("PRECAST_%s_clusters.csv", .y))))
 
-##Add data to spe
+#### Add data to spe ####
+precast_tab <- do.call("bind_cols", precast_tab)
+colnames(precast_tab)
+
 colData(spe) <- cbind(colData(spe), precast_tab)
 
 ## which spots are missing?
 table(is.na(spe$PRECAST_HVG_k02), spe$scran_discard)
+#         TRUE  FALSE
+# FALSE   6467 115573
+# TRUE     162      0
+
 table(is.na(spe$PRECAST_HVG_k02), spe$scran_low_lib_size)
 #         TRUE  FALSE
 # FALSE   5755 116285
@@ -76,7 +107,21 @@ table(is.na(spe$PRECAST_HVG_k02), spe$sample_id)
 sample_ids <- sort(unique(spe$sample_id))
 #   Use 'vis_grid_clus' to preserve all spots (including overlaps)
 
-walk(colnames(precast_tab)[36:55], function(precast_name){
+spe$PRECAST_FAIL <- is.na(spe$PRECAST_HVG_k02)
+
+vis_grid_clus(
+    spe = spe,
+    clustervar = "PRECAST_FAIL",
+    pdf_file = here(plot_dir, paste0("PRECAST_FAIL-ALL.pdf")),
+    sort_clust = FALSE,
+    # colors = cols,
+    spatial = FALSE,
+    point_size = 1,
+    sample_order = sample_ids
+)
+
+# walk(colnames(precast_tab)[grep("SVGplusMarkers",colnames(precast_tab))], function(precast_name){
+walk(colnames(precast_tab)[!grepl("SVGplusMarkers",colnames(precast_tab))], function(precast_name){
     
     k = parse_number(precast_name)
     
@@ -90,19 +135,19 @@ walk(colnames(precast_tab)[36:55], function(precast_name){
     plot_dir2 <- here(plot_dir, precast_name)
     if(!dir.exists(plot_dir2)) dir.create(plot_dir2, recursive = TRUE)
     
-    p_list = vis_grid_clus(
-        spe = spe,
-        clustervar = precast_name,
-        pdf_file = here(plot_dir2, paste0(precast_name, "-ALL.pdf")),
-        sort_clust = FALSE,
-        # colors = cols,
-        spatial = FALSE,
-        point_size = 1,
-        sample_order = sample_ids
-    )
+    # p_list = vis_grid_clus(
+    #     spe = spe,
+    #     clustervar = precast_name,
+    #     pdf_file = here(plot_dir2, paste0(precast_name, "-ALL.pdf")),
+    #     sort_clust = FALSE,
+    #     # colors = cols,
+    #     spatial = FALSE,
+    #     point_size = 1.2,
+    #     sample_order = sample_ids
+    # )
     
     #  Vis_clus for each sample
-    walk(c("Br5517"), function(samp){
+    walk(c("Br5517", "Br5460"), function(samp){
         spot_plot <- vis_clus(
             spe = spe,
             sampleid = samp,
@@ -111,8 +156,8 @@ walk(colnames(precast_tab)[36:55], function(precast_name){
             # colors = cols,
             point_size = 2
         )
-        ggsave(spot_plot, filename = here(plot_dir2, paste0(precast_name, "-", samp, ".pdf")))
-        ggsave(spot_plot, filename = here(plot_dir2, paste0(precast_name, "-", samp, ".png")))
+        ggsave(spot_plot, filename = here(plot_dir2, paste0(precast_name, "-", samp, ".pdf")), width = 7, height = 7)
+        ggsave(spot_plot, filename = here(plot_dir2, paste0(precast_name, "-", samp, ".png")), width = 7, height = 7)
     })
     
 })
