c5feab0968ddf58baec1aa3c96cda6bd4769fef7
Louise Huuki <lahuuki@gmail.com>
2024-11-14 17:41:42 +0000

    Fix shell script typo

--- code/05_spe_correct_cluster/05_BayesSpace_Markers.sh
+++ code/05_spe_correct_cluster/05_BayesSpace_Markers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -p shared
 #SBATCH --mem=25G
-#SBATCH --job-name=05_BayesSpace_Marker
+#SBATCH --job-name=05_BayesSpace_Markers
 #SBATCH -c 1
 #SBATCH -t 1-00:00:00
-#SBATCH -o logs/05_BayesSpace_Marker.txt
-#SBATCH -e logs/05_BayesSpace_Marker.txt
+#SBATCH -o logs/05_BayesSpace_Markers.txt
+#SBATCH -e logs/05_BayesSpace_Markers.txt
 #SBATCH --mail-type=ALL
 
 set -e
@@ -27,7 +27,7 @@ module load conda_R/4.4
 module list
 
 ## Edit with your job command
-Rscript 05_BayesSpace_Marker.R
+Rscript 05_BayesSpace_Markers.R
 
 echo "**** Job ends ****"
 date
