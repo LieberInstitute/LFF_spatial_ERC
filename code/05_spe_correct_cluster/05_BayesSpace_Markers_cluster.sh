3edbc22ba9de31adfbfc2bbcdd11b05c1827cd40
Louise Huuki <lahuuki@gmail.com>
2024-11-19 10:49:25 -0500

    Add shell script to run Markers BayesSpace for 2-28

---
+++ code/05_spe_correct_cluster/05_BayesSpace_Markers_cluster.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -p katun
+#SBATCH --mem=100G
+#SBATCH --job-name=05_BayesSpace_Markers_cluster
+#SBATCH -c 1
+#SBATCH -o /dev/null
+#SBATCH -e /dev/null
+#SBATCH --mail-type=ALL
+#SBATCH --array=2-28%20
+
+## Explicitly pipe script output to a log
+log_path=logs/05_BayesSpace_Markers_cluster_k${SLURM_ARRAY_TASK_ID}.txt
+
+{
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load the R module
+module load conda_R/4.4
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 05_BayesSpace_Markers_cluster.R
+
+echo "**** Job ends ****"
+date
+
+} > $log_path 2>&1
+
+## This script was made using slurmjobs version 1.1.0
+## available from http://research.libd.org/slurmjobs/
