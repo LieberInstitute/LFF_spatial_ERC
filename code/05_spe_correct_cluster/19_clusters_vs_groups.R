f001e75bce32688bd3f27f8404372b628fe6d6fe
Louise Huuki <lahuuki@gmail.com>
2024-12-11 15:47:52 -0500

    Use DFplyr to check cluster distribution

--- code/05_spe_correct_cluster/19_clusters_vs_groups.R
+++ code/05_spe_correct_cluster/19_clusters_vs_groups.R
@@ -3,6 +3,7 @@
 
 library("spatialLIBD")
 library("tidyverse")
+library("DFplyr")
 library("here")
 library("sessioninfo")
 
@@ -13,5 +14,23 @@ if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 message(Sys.time(), " - Load HDF5 SPE")
 spe <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "spe_objects", "spe_ERC"))
 
+table(spe$BayesSpace_SVGm_k09, spe$sample_id)
+
 spe_pb_k09 <- readRDS(here("processed-data", "05_spe_correct_cluster", "08_model_pseudobulk", "BayesSpace_SVGm","spe_pseudobulk-BayesSpace_SVGm_k09.rds"))
+
+
+as.data.frame(colData(spe)) |>
+    group_by(BayesSpace_SVGm_k09) |>
+    dplyr::summarise(n = n()) 
+
+# DFplyr
+# Error in `n()`:
+#     ! Must only be used inside data-masking verbs like `mutate()`, `filter()`, and `group_by()`
+
+cluster_prop <- as.data.frame(colData(spe)) |>
+    group_by(sample_id, APOE, Ancestry, Sex, BayesSpace_SVGm_k09) |>
+    summarise(n = n())  |>
+    group_by(sample_id, APOE, Ancestry, Sex) |>
+    mutate(cluster_prop = n/sum(n))
+
 
