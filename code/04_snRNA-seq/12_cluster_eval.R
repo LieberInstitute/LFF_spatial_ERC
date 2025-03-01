a7138c1580306922e6aae43a5d5fef40b296c37c
Louise Huuki <lahuuki@gmail.com>
2025-03-01 13:17:10 +0000

    save silhouette_score, fix plots, add sh file

--- code/04_snRNA-seq/12_cluster_eval.R
+++ code/04_snRNA-seq/12_cluster_eval.R
@@ -28,6 +28,7 @@ names(cluster_fn) <- ss(basename(cluster_fn), "_", 3)
 
 clusters <- map_dfc(cluster_fn, ~get(load(.x)))
 head(clusters)
+dim(clusters)
 
 map_int(clusters, max)
 # k10 k15 k20 
@@ -38,18 +39,20 @@ map_int(clusters, ~sum(table(.x) < 100))
 # 12   4   1 
 
 
-
 #### Calculate Silhouette Score ####
 message(Sys.time(), " - Calc Distantce matrix")
 dist_matrix <- dist(reducedDim(sce, "HARMONY"))
 
 message(Sys.time(), " - Calc Silhouette score")
-sil <- map(clusters, ~silhouette(.x, dist_matrix))
-silhouette_mean <- map(sil, ~mean(.x[, "sil_width"]))
+silhouette_score <- map(clusters, ~silhouette(.x, dist_matrix))
+save(silhouette_score, file = "silhouette_score.Rdata")
+
+## calc mean silhouette score
+silhouette_mean <- map(silhouette_score, ~mean(.x[, "sil_width"]))
 
 cluster_eval <- tibble(k = as.integer(gsub("k", "", colnames(clusters))),
                  n_clus = map_int(clusters, max),
-                 silh_score = silhouette_score)
+                 silh_score = silhouette_mean)
 
 write_csv(cluster_eval,  file = here(data_dir, "cluster_eval.csv"))
 
@@ -66,7 +69,7 @@ cluster_eval_k_vs_n <- ggplot(cluster_eval, aes(x = k, y = n_clus)) + ## add col
     geom_line() +
     theme_bw()
 
-ggsave(cluster_eval_k_vs_n, here(plot_dir, "cluster_eval_k_vs_n.png"))
+ggsave(cluster_eval_k_vs_n, filename = here(plot_dir, "cluster_eval_k_vs_n.png"))
 
 ## silhouette score vs. k
 cluster_eval_k_vs_silh <- ggplot(cluster_eval, aes(x = k, y = silh_score)) +
@@ -74,15 +77,15 @@ cluster_eval_k_vs_silh <- ggplot(cluster_eval, aes(x = k, y = silh_score)) +
     geom_line() +
     theme_bw()
 
-ggsave(cluster_eval_k_vs_silh, here(plot_dir, "cluster_eval_k_vs_silh.png"))
+ggsave(cluster_eval_k_vs_silh, filename = here(plot_dir, "cluster_eval_k_vs_silh.png"))
 
 ## silhouette score vs. n cluster
 cluster_eval_n_vs_silh <- ggplot(cluster_eval, aes(x = n_clus, y = silh_score)) +
     geom_point() +
-    geom_line() +
+    # geom_line() +
     theme_bw()
 
-ggsave(cluster_eval_n_vs_silh, here(plot_dir, "cluster_eval_n_vs_silh.png"))
+ggsave(cluster_eval_n_vs_silh, filename = here(plot_dir, "cluster_eval_n_vs_silh.png"))
 
 
 # slurmjobs::job_single('12_cluster_eval', create_shell = TRUE, memory = '5G', command = "12_cluster_eval.R")
