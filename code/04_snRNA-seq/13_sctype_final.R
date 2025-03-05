7b12fd8c298473e24d05dc7639e2623cd7e5edf0
Louise Huuki <lahuuki@gmail.com>
2025-03-05 17:51:19 +0000

    refine ct names for custom PEC input

--- code/04_snRNA-seq/13_sctype_final.R
+++ code/04_snRNA-seq/13_sctype_final.R
@@ -95,6 +95,7 @@ es.max[1:5, 1:5]
 # GABAergic neurons    1.4841430 2.3017959 4.7711181 1.622680 5.6416159
 
 save(es.max, file = here(data_dir, sprintf("sctype_es_max-%s.Rdata", opt$database)))
+# load(here(data_dir, sprintf("sctype_es_max-%s.Rdata", "custom")), verbose = TRUE)
 
 #### annotate clusters ####
 message(Sys.time(), " - Annotate clusters")
@@ -135,54 +136,90 @@ sctype_scores |> group_by(type) |>  summarize(ncells = sum(ncells), n_cluster = 
 # 7 Oligodendrocyte precursor cells  15373         5
 # 8 Oligodendrocytes                 52518         9
 
-## short names 
-type_short <- tibble(type = c(
-    'Astrocytes',
-    'Endothelial cells',
-    'Microglial cells',
-    'Oligodendrocytes',
-    'Oligodendrocyte precursor cells',
-    'Glutamatergic neurons',
-    'GABAergic neurons',
-    'Mature neurons'), 
-    cell_type_broad  = c(
+if(opt$database == "sctype"){
+    ## short names 
+    type_short <- tibble(type = c(
+        'Astrocytes',
+        'Endothelial cells',
+        'Microglial cells',
+        'Oligodendrocytes',
+        'Oligodendrocyte precursor cells',
+        'Glutamatergic neurons',
+        'GABAergic neurons',
+        'Mature neurons'), 
+        cell_type_broad  = c(
+            'Astro',
+            'Endo',
+            "Micro",
+            'Oligo',
+            'OPC',
+            'Excit',
+            'Inhib',
+            'Neu'),
+        cell_type_class =c(
+            rep("glia", 5),
+            rep("neuron", 3)
+        )
+    )
+    
+    
+    cell_type_levels = type_short$cell_type_broad
+    
+    sctype <- sctype_scores |> 
+        left_join(type_short) |>
+        group_by(type) |>
+        arrange(-ncells) |> 
+        mutate(type_rank = row_number(),
+               cell_type_fine = paste0(cell_type_broad, 
+                                       ".",
+                                       width = str_pad(type_rank, 
+                                                       nchar(as.character(max(type_rank))),
+                                                       side = "left",
+                                                       pad = "0")),
+               cell_type_broad = factor(cell_type_broad, levels = cell_type_levels),
+               database = "opt$database"
+        )
+    
+    ## add levels to cell type fine
+    fine_levels <- sctype |> arrange(cell_type_broad) |> pull(cell_type_fine)
+    sctype <- sctype|>
+        mutate(cell_type_fine = factor(cell_type_fine, levels = fine_levels)) |>
+        arrange(cell_type_fine)
+    
+} else if(opt$database == "custom"){
+   
+    cell_type_levels = c(
         'Astro',
-        'Endo',
         "Micro",
         'Oligo',
         'OPC',
+        'VLMC',
         'Excit',
-        'Inhib',
-        'Neu'),
-    cell_type_class =c(
-        rep("glia", 5),
-        rep("neuron", 3)
-    )
-)
-
-
-cell_type_levels = type_short$cell_type_broad
-
-sctype <- sctype_scores |> 
-    left_join(type_short) |>
-    group_by(type) |>
-    arrange(-ncells) |> 
-    mutate(type_rank = row_number(),
-           cell_type_fine = paste0(cell_type_broad, 
-                                   ".",
-                                   width = str_pad(type_rank, 
-                                                   nchar(as.character(max(type_rank))),
-                                                   side = "left",
-                                                   pad = "0")),
-           cell_type_broad = factor(cell_type_broad, levels = cell_type_levels),
-           database = "opt$database"
-    )
-
-## add levels to cell type fine
-fine_levels <- sctype |> arrange(cell_type_broad) |> pull(cell_type_fine)
-sctype <- sctype|>
-    mutate(cell_type_fine = factor(cell_type_fine, levels = fine_levels)) |>
-    arrange(cell_type_fine)
+        'Inhib')
+    
+    sctype <- sctype_scores |> 
+        group_by(type) |>
+        arrange(-ncells) |> 
+        mutate(type_rank = row_number(),
+               cell_type_fine = ifelse(n() >1, 
+                                       paste0(type, 
+                                       ".",
+                                       width = str_pad(type_rank, 
+                                                       nchar(as.character(max(type_rank))),
+                                                       side = "left",
+                                                       pad = "0")),
+                                       type),
+               cell_type_broad = factor(jaffelab::ss(type, "\\."), levels = cell_type_levels),
+               database = opt$database
+        )
+    
+    ## add levels to cell type fine
+    fine_levels <- sctype |> arrange(cell_type_broad) |> pull(cell_type_fine)
+    sctype <- sctype|>
+        mutate(cell_type_fine = factor(cell_type_fine, levels = fine_levels)) |>
+        arrange(cell_type_fine)
+    
+}
 
 write.csv(sctype, file = here(data_dir, sprintf("sctype_final-%s.csv", opt$database)), row.names = FALSE)
 save(sctype, file = here(data_dir, sprintf("sctype_final-%s.Rdata", opt$database)))
