4f76ac07ff24dbb6b873f016d35b7d5168a48136
Louise Huuki <lahuuki@gmail.com>
2025-03-05 08:34:48 +0000

    Build table of PsychENCODE marker genes

--- code/04_snRNA-seq/00_lit_marker_genes.R
+++ code/04_snRNA-seq/00_lit_marker_genes.R
@@ -15,22 +15,58 @@ if(!dir.exists(data_dir)) dir.create(data_dir)
 
 
 #### prep psychENCODE genes ####
-pec_json <- fromJSON(file=here("processed-data", "04_snRNA-seq","13_sctype_final", "AHBA_PFC_filtered.json"))
+pec_json <- fromJSON(file=here("processed-data", "04_snRNA-seq","00_lit_marker_genes", "AHBA_PFC_filtered.json"))
 
 pec_json$cell_types
-pec_json[[1]]$subtypes
+pec_json$cell_types[[1]]$subtypes
 
-map_chr(pec_json$cell_types, "name")
+(pec_broad_cell_types <- map_chr(pec_json$cell_types, "name"))
 # [1] "Excitatory neuron" "Inhibitory neuron" "Astro"             "Oligo"             "OPC"              
 # [6] "Endo"              "Micro/PVM"         "T cells"           "VLMC" 
 
-map_lgl(pec_json$cell_types, "subtypes" %in% names())
+## broad marker genes
+pec_broad_markers <- map(pec_json$cell_types, ~.x$markers[[1]]$genes)
 
-map_chr(pec_json$cell_types[[1]]$subtypes$cell_types, "name")
-# [1] "L2/3 IT"    "L5 IT"      "L6 IT"      "L4 IT"      "L6 CT"      "L6b"        "L5 ET"      "L5/6 NP"   
-# [9] "L6 IT Car3"
+pec_marker_broad_tab <- tibble(source_cell_type = rep(pec_broad_cell_types, map_int(pec_broad_markers, length)),
+                         gene = unlist(pec_broad_markers))
 
-map(pec_json$cell_types[[1]]$subtypes$cell_types$markers, "genes")
+## subtype markers
+pec_marker_sub_tab <- map2_dfr(c(Excit = 1, Inhib = 2), c("Excit", "Inhib"), function(i, ct){
+    subtypes <- map_chr(pec_json$cell_types[[i]]$subtypes$cell_types, "name")
+    
+    sub_markers <- map(pec_json$cell_types[[i]]$subtypes$cell_types, ~.x$markers[[1]]$genes)
+    
+    sub_marker_tab <- tibble(source_cell_type = rep(subtypes, map_int(sub_markers, length)),
+                             cell_type_broad = ct,
+                             cell_type_fine = gsub(" |/", "_", source_cell_type),
+                             gene = unlist(sub_markers))
+    return(sub_marker_tab)
+})
+
+pec_marker_broad_tab |> dplyr::count(source_cell_type)
+pec_marker_sub_tab |> dplyr::count(source_cell_type)
+
+pec_marker_tab <- pec_marker_broad_tab |>
+    left_join(tibble(source_cell_type = c("Excitatory neuron",
+                                          "Inhibitory neuron",
+                                          "Micro/PVM",
+                                          "T cells"),
+                     cell_type_broad = c("Excit",
+                                         "Inhib",
+                                         "Micro",
+                                         "Tcell"))) |>
+    mutate(cell_type_broad = ifelse(is.na(cell_type_broad), source_cell_type, cell_type_broad)) |>
+    bind_rows(pec_marker_sub_tab)
+
+
+pec_marker_tab |> dplyr::count(cell_type_broad, cell_type_fine)
+
+## Inhib
+map_chr(pec_json$cell_types[[2]]$subtypes$cell_types, "name")
+# [1] "Lamp5"      "Lamp5 Lhx6" "Pvalb"      "Chandelier" "Pax6"       "Sncg"       "Sst Chodl"  "Sst"        "Vip"   
+
+
+#### sctype genes ####
 
 ## summarize marker genes from lit recorded in reading
 lit_markers <- read.csv(here("processed-data", "04_snRNA-seq", "lit_marker_genes.csv")) |>
