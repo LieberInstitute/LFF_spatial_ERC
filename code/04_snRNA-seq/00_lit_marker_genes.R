6015d592fe4921c3bbdfc9236210b575c720c391
Louise Huuki <lahuuki@gmail.com>
2025-03-05 15:48:21 +0000

    Build custom PEC sctype DB, load and format sctype brain markers to comapre

--- code/04_snRNA-seq/00_lit_marker_genes.R
+++ code/04_snRNA-seq/00_lit_marker_genes.R
@@ -56,24 +56,85 @@ pec_marker_tab <- pec_marker_broad_tab |>
                                          "Micro",
                                          "Tcell"))) |>
     mutate(cell_type_broad = ifelse(is.na(cell_type_broad), source_cell_type, cell_type_broad)) |>
-    bind_rows(pec_marker_sub_tab)
+    bind_rows(pec_marker_sub_tab) |>
+    mutate(source = "PsychENCODE AHBA PFC",
+           gene = gsub("\\+", "", gene))
 
+
+pec_marker_tab |> dplyr::count(cell_type_broad)
+# cell_type_broad     n
+# <chr>           <int>
+# 1 Astro             221
+# 2 Endo              287
+# 3 Excit             232
+# 4 Inhib             241
+# 5 Micro             285
+# 6 OPC               122
+# 7 Oligo             255
+# 8 Tcell               2
+# 9 VLMC              218
 
 pec_marker_tab |> dplyr::count(cell_type_broad, cell_type_fine)
 
-## Inhib
-map_chr(pec_json$cell_types[[2]]$subtypes$cell_types, "name")
-# [1] "Lamp5"      "Lamp5 Lhx6" "Pvalb"      "Chandelier" "Pax6"       "Sncg"       "Sst Chodl"  "Sst"        "Vip"   
+## format for sctype Database
 
+pec_db <- pec_marker_tab |> 
+    mutate(cellName = ifelse(is.na(cell_type_fine), cell_type_broad, paste0(cell_type_broad,"." ,cell_type_fine))) |> 
+    group_by(cellName) |>
+    summarise(geneSymbolmore1 = paste(gene, collapse = ",")) |>
+    mutate(tissueType = "Brain",
+           geneSymbolmore2 = 0) |>
+    select(tissueType, cellName, geneSymbolmore1, geneSymbolmore2)
+
+writexl::write_xlsx(pec_db, path = here(data_dir, "scTypeDB_PEC_PFC_custom.xlsx"))
+
+## test
+gs_list_pec <- gene_sets_prepare(here(data_dir, "scTypeDB_PEC_PFC_custom.xlsx"), "Brain")
 
 #### sctype genes ####
+library("HGNChelper")
 
-## summarize marker genes from lit recorded in reading
-lit_markers <- read.csv(here("processed-data", "04_snRNA-seq", "lit_marker_genes.csv")) |>
+source("https://raw.githubusercontent.com/IanevskiAleksandr/sc-type/master/R/gene_sets_prepare.R")
+db_ <- "https://raw.githubusercontent.com/IanevskiAleksandr/sc-type/master/ScTypeDB_full.xlsx"
+gs_list <- gene_sets_prepare(db_, "Brain")
+
+sctype_marker_tab <- tibble(source_cell_type = rep(names(gs_list$gs_positive), map_int(gs_list$gs_positive, length)),
+                            gene = unlist(gs_list$gs_positive),
+                            ) |> left_join(tibble(source_cell_type = c(
+                                'Astrocytes',
+                                'Endothelial cells',
+                                'Microglial cells',
+                                'Oligodendrocytes',
+                                'Oligodendrocyte precursor cells',
+                                'Glutamatergic neurons',
+                                'GABAergic neurons',
+                                'Mature neurons'), 
+                                cell_type_broad  = c(
+                                    'Astro',
+                                    'Endo',
+                                    "Micro",
+                                    'Oligo',
+                                    'OPC',
+                                    'Excit',
+                                    'Inhib',
+                                    'Neu')
+                            )) |> mutate(source = "sctype Brain")
+
+sctype_marker_tab |> dplyr::count(cell_type_broad)
+
+
+
+#### summarize marker genes from lit recorded in reading ####
+lit_markers <- read.csv(here("processed-data", "04_snRNA-seq","00_lit_marker_genes", "lit_marker_genes.csv"))
+
+lit_marker_summary <- lit_markers |>
     group_by(gene_name, cell_type) |>
     summarize(n_studies = n(),
               studies = paste0(source, collapse = ",")) |>
     mutate(in_data = gene_name %in% rowData(sce)$Symbol)
 
-lit_markers |> write_csv(here("processed-data", "04_snRNA-seq", "lit_marker_summary.csv"))
+lit_marker_summary |> write_csv(here("processed-data", "04_snRNA-seq", "lit_marker_summary.csv"))
+
+#### compile all marker genes ####
+
 
