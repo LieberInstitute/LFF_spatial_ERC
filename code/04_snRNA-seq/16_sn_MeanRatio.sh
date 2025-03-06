b799bdab0ffc5975c1499904742da8f678071da8
Louise Huuki <lahuuki@gmail.com>
2025-03-06 12:10:40 +0000

    update cell type opt

--- code/04_snRNA-seq/16_sn_MeanRatio.sh
+++ code/04_snRNA-seq/16_sn_MeanRatio.sh
@@ -27,7 +27,7 @@ module load conda_R/4.4.x
 module list
 
 ## Edit with your job command
-Rscript 16_sn_MeanRatio.R --cluster 'ct_fine_k20'
+Rscript 16_sn_MeanRatio.R --cluster 'cell_type_fine'
 
 echo "**** Job ends ****"
 date
