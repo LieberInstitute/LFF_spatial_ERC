ade3d2e56724cda4eae466c6d3371d5e745d26f9
Louise Huuki <lahuuki@gmail.com>
2025-03-14 14:54:37 -0400

    Add color file to app deployment

--- code/06_iSEE_app/deploy.R
+++ code/06_iSEE_app/deploy.R
@@ -3,7 +3,7 @@ library("here")
 options(repos = BiocManager::repositories())
 rsconnect::deployApp(
     appDir = here("code", "06_iSEE_app"),
-    appFiles = c("app.R", "sce_ERC_iSEE.rds"),
+    appFiles = c("app.R", "sce_ERC_iSEE.rds","sn_colors.rds"),
     appName = "LFF_ERC_snRNA-seq",
     account = "libd",
     server = "shinyapps.io"
