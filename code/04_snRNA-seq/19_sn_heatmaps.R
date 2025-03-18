a0b56630c9be51a7eb41f096e613213b773a5a86
Louise Huuki <lahuuki@gmail.com>
2025-03-18 14:08:06 -0400

    Add heatmaps for MR marker genes, update cell type anno order

--- code/04_snRNA-seq/19_sn_heatmaps.R
+++ code/04_snRNA-seq/19_sn_heatmaps.R
@@ -30,10 +30,13 @@ cell_anno_df <- colData(sce_pb) |>
 
 cell_type_anno_levels <- cell_anno_df |> arrange(cell_type_broad, cell_type_anno) |> pull(cell_type_anno) |> unique()
 
-cell_anno_df$cell_type_anno <- factor(cell_anno_df$cell_type_anno, levels = cell_type_anno_levels)
-
 rownames(cell_anno_df) <- colnames(sce_pb)
- 
+
+cell_anno_df$cell_type_anno <- factor(cell_anno_df$cell_type_anno, levels = cell_type_anno_levels)
+cell_anno_df <- cell_anno_df |> arrange(cell_type_anno, sample_id)
+
+sce_pb <- sce_pb[,rownames(cell_anno_df)]
+
 ## load colors
 load(here("processed-data", "04_snRNA-seq", "cell_type_colors.Rdata"), verbose = TRUE)
 load(here("processed-data", "project_colors.Rdata"), verbose = TRUE)
@@ -46,6 +49,11 @@ cell_type_colors$anno <- cell_type_colors_anno$fine
 ## cell row annotations simple
 
 cell_row_ha_simple <- rowAnnotation(
+    df = cell_anno_df |> select(cell_type_anno),
+    col = list(cell_type_anno = cell_type_colors$anno)
+)
+
+cell_col_ha_simple <- HeatmapAnnotation(
     df = cell_anno_df |> select(cell_type_anno),
     col = list(cell_type_anno = cell_type_colors$anno)
 )
@@ -93,10 +101,76 @@ Heatmap(lit_markers_zscore,
 )
 dev.off()
 
+pdf(here(plot_dir, "ERC_sn_lit_gene_heatmap_cluster.pdf"), height = 8, width = 11)
+Heatmap(lit_markers_zscore,
+        name = "Z Score",
+        cluster_rows = TRUE,
+        cluster_columns = TRUE,
+        show_row_names = FALSE,
+        # column_split = lit_gene_annotation$cell_type_broad,
+        # row_split = cell_anno_df$cell_type_anno,
+        right_annotation = cell_row_ha_simple,
+        bottom_annotation = lit_gene_col_ha
+)
+dev.off()
+
 
 #### MeanRatio heatmap ####
 ## load marker gene data
 load(here("processed-data", "04_snRNA-seq", "16_sn_MeanRatio", "MarkerStats_cell_type_fine.Rdata"), verbose = TRUE)
+
+marker_stats_top <- marker_stats |>
+    filter(MeanRatio.rank <= 5, MeanRatio > 1, gene_name %in% rownames(sce_pb)) |>
+    select(gene, MeanRatio.rank, cell_type_fine = cellType.target) |>
+    left_join(anno_notes |> select(cell_type_fine, cell_type_anno = guess)) |>
+    mutate(cell_type_anno = factor(cell_type_anno, levels = cell_type_anno_levels)) |>
+    arrange(cell_type_anno) |>
+    column_to_rownames("gene")
+
+marker_stats_top |> count(cell_type_anno) |> arrange(n)
+
+
+MR_gene_row_ha <- rowAnnotation(df = marker_stats_top |> select(cell_type_anno),
+                                     col = list(cell_type_anno = cell_type_colors$anno))
+
+MR_gene_col_ha <- HeatmapAnnotation(df = marker_stats_top |> select(cell_type_anno),
+                                     col = list(cell_type_anno = cell_type_colors$anno))
+
+## extract z-scores
+MR_markers_zscore <- scale(t(logcounts(sce_pb)[rownames(marker_stats_top),]))
+dim(MR_markers_zscore)
+MR_markers_zscore[1:5,1:5]
+
+pdf(here(plot_dir, "ERC_sn_MR_gene_heatmap.pdf"), height = 14, width = 10)
+Heatmap(t(MR_markers_zscore),
+        name = "Z Score",
+        cluster_rows = FALSE,
+        cluster_columns = FALSE,
+        show_row_names = TRUE,
+        show_column_names = FALSE,
+        row_names_gp = grid::gpar(fontsize = 10),
+        # row_split_gp = grid::gpar(fontsize = 10),
+        # column_split = marker_stats_top$cell_type_anno,
+        row_split = gsub("\\.","\n",marker_stats_top$cell_type_anno),
+        bottom_annotation = cell_col_ha_simple,
+        right_annotation = MR_gene_row_ha
+)
+dev.off()
+
+pdf(here(plot_dir, "ERC_sn_MR_gene_heatmap_cluster.pdf"), height = 12, width = 10)
+Heatmap(t(MR_markers_zscore),
+        name = "Z Score",
+        cluster_rows = TRUE,
+        cluster_columns = TRUE,
+        show_row_names = TRUE,
+        show_column_names = FALSE,
+        row_names_gp = grid::gpar(fontsize = 9),
+        # column_split = marker_stats_top$cell_type_anno,
+        # row_split = cell_anno_df$cell_type_anno,
+        bottom_annotation = cell_col_ha_simple,
+        right_annotation = MR_gene_row_ha
+)
+dev.off()
 
 
 # slurmjobs::job_single('19_sn_heatmaps', create_shell = TRUE, memory = '25G', command = "Rcript 19_sn_heatmaps.R")
