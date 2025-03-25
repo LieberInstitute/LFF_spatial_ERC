99ac7081001c91f732f7ef866f7615d1b4c4c02e
Louise Huuki <lahuuki@gmail.com>
2025-03-25 15:52:45 -0400

    Plot vis_clus by APOE group, add slurmjobs + repoinfo

--- code/05_spe_correct_cluster/19_SpD_update_spe.R
+++ code/05_spe_correct_cluster/19_SpD_update_spe.R
@@ -7,6 +7,8 @@ library("HDF5Array")
 library("here")
 library("sessioninfo")
 library("readxl")
+library("jaffelab")
+library("cowplot")
 
 ## source reduced dims function
 source(here("code", "utils", "my_plot_reduced_dim.R"))
@@ -31,8 +33,7 @@ colnames(colData(spe))
 colData(spe) <- colData(spe)[,!grepl("Markers", colnames(colData(spe)))]
 colData(spe)$BayesSpace_SVGm_k09_anno <- NULL
 
-#### load SpD ####
-
+#### load SpD annotations ####
 spd_anno <- readxl::read_excel(here("processed-data","05_spe_correct_cluster", "10_spatial_registration_DLPFC", "ERC_SpD_spatial_registration_Annotations.xlsx"))
 
 spd_anno$cluster %in% spe$BayesSpace_SVGm_k09
@@ -53,14 +54,9 @@ table(anno_table$SpD)
 ## add to colData
 colData(spe) <- cbind(colData(spe), anno_table) 
 
-spe$Annotation <- NULL
-spe$SpD <- NULL
-
 table(spe$SpD, spe$BayesSpace_SVGm_k09)
 
 #### Define colors for SpD ####
-
-levels(spe$SpD)
 
 SpD_colors <- c("Vasc~Sp09D08" = "#E05AD2",
                 "L1~Sp09D05" = "#021380",
@@ -83,14 +79,94 @@ color_test <- vis_clus(
 ggsave(color_test, filename = here(plot_dir, "SpD_color_test_Br5517.png"))
 
 
-vis_grid_clus(
+vis_clus_plots <- vis_grid_clus(
     spe = spe,
     clustervar = "SpD",
     colors = SpD_colors,
+    sort_clust = FALSE,
+    return_plots = TRUE,
     point_size = 1,
     pdf_file = here(plot_dir, "SpD_color_test.pdf")
 )
 
+apoe_anc <- as.data.frame(colData(spe)) |> 
+    select(sample_id, APOE, Ancestry) |> 
+    unique() |>
+    mutate(apoe_anc = paste0(APOE, "_", Ancestry))
+
+all(apoe_anc$sample_id == names(vis_clus_plots))
+
+apoe_anc_split <- splitit(apoe_anc$apoe_anc)
+
+pdf(here(plot_dir, "ERC_SpD_vis_clus_split.pdf"), height = 24, width = 36)
+print(cowplot::plot_grid(plotlist = split_plots))
+dev.off()
+
+# pdf(here(plot_dir, "ERC_SpD_vis_clus_split.pdf"), height = 12, width = 18)
+# walk2(apoe_anc_split, names(apoe_anc_split), function(index,name){
+#     
+#     split_plots <- vis_clus_plots[index]
+#     message(name, "; ", length(split_plots))
+#     plots <- cowplot::plot_grid(plotlist = split_plots)
+#     
+#     # title
+#     title <- ggdraw() + 
+#         cowplot::draw_label(
+#             name,
+#             fontface = 'bold',
+#             x = 0,
+#             hjust = 0
+#         ) +
+#         theme(
+#             # add margin on the left of the drawing canvas,
+#             # so title is aligned with left edge of first plot
+#             plot.margin = margin(0, 0, 0, 7)
+#         ) 
+#         
+#    print(cowplot::plot_grid(
+#         title, plots,
+#         ncol = 1,
+#         # rel_heights values control vertical title margins
+#         rel_heights = c(0.1, 1)
+#     ))
+# })
+# dev.off()
+# 
+# cowplot_title <- function(my_title, plots){
+#     title <- ggdraw() + 
+#         cowplot::draw_label(
+#             my_title,
+#             fontface = 'bold',
+#             x = 0,
+#             hjust = 0
+#         ) +
+#         theme(
+#             # add margin on the left of the drawing canvas,
+#             # so title is aligned with left edge of first plot
+#             plot.margin = margin(0, 0, 0, 7)
+#         ) 
+#     
+#     print(cowplot::plot_grid(
+#         title, plots,
+#         ncol = 1,
+#         # rel_heights values control vertical title margins
+#         rel_heights = c(0.1, 1)
+#     ))
+# }
+
+## plot by APOE
+apoe_split <- splitit(apoe_anc$APOE)
+map2(apoe_split, names(apoe_split), 
+      ~vis_grid_clus(
+          spe = spe[,spe$sample_id %in% apoe_anc$sample_id[.x]],
+          clustervar = "SpD",
+          colors = SpD_colors,
+          sort_clust = FALSE,
+          return_plots = FALSE,
+          point_size =  2+1/length(apoe_split),
+          pdf_file = here(plot_dir, sprintf("ERC_SpD_%s.pdf", gsub("/","",.y)))
+      )
+      )
 
 ## save all color output
 save(SpD_colors, file = here("processed-data", "05_spe_correct_cluster", "SpD_colors.Rdata"))
@@ -117,7 +193,8 @@ cluster_info <- pd |>
               prop = n/ncol(spe),
               median_sum_umi = median(sum_umi),
               median_sum_gene = median(sum_gene),
-              median_chrM_ratio = median(expr_chrM_ratio)
+              median_chrM_ratio = median(expr_chrM_ratio),
+              median_Nmask = median(Nmask_dark_blue)
     )
 
 write.csv(cluster_info, file = here(data_dir, "ERC_spe_cluster_info.csv"), row.names = FALSE)
@@ -129,7 +206,10 @@ cluster_metrics_long <- pd |>
     summarize(median = median(value))
 
 qc_violin_plot_all <- pd |> 
-    select(SpD, sum_gene, sum_umi, expr_chrM_ratio)   |>
+    select(SpD,
+           sum_gene, 
+           sum_umi, 
+           expr_chrM_ratio)   |>
     pivot_longer(!c(SpD), names_to = "metric") |>
     ggplot() +
     geom_violin(aes(x = SpD, y = value, fill = SpD), 
@@ -180,8 +260,8 @@ SpD_proportion_bar_APOE <- SpD_proportions |>
               position = position_stack(vjust = .5),
               size = 2) +
     theme_bw() +
-    scale_fill_manual(values = SpD_colors, guide = "none") +
-    facet_wrap(~APOE, nrow = 1, scales = "free_x") +
+    scale_fill_manual(values = SpD_colors) +
+    facet_grid(.~APOE, scales = "free_x", space = "free") +
     labs(y = "SpD Proportion")  +
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
@@ -231,7 +311,15 @@ ggsave(sample_proportion_bar, filename = here(plot_dir, "ERC_SpD_barplot_sample_
 message(Sys.time(), " - Saving HDF5 SPE")
 saveHDF5SummarizedExperiment(
     spe,
-    dir = here("processed-data", "spe_objects", "spe_ERC"),
+    dir = here("processed-data", "spe_objects", "spe_ERC_annotated"),
     replace = TRUE
 )
 
+# slurmjobs::job_single('19_SpD_update_spe', create_shell = TRUE, memory = '25G', command = "Rscript 19_SpD_update_spe.R")
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
