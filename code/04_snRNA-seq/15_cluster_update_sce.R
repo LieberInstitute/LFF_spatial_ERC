7e1daa127ea5301e83e103c70c8bea571e5f6d9e
Louise Huuki <lahuuki@gmail.com>
2025-04-02 10:25:13 -0400

    Use updated cell_type_colors$anno, plot n donor and nuc barplots, add info from GTF file

--- code/04_snRNA-seq/15_cluster_update_sce.R
+++ code/04_snRNA-seq/15_cluster_update_sce.R
@@ -83,24 +83,9 @@ cell_type_colors <- DeconvoBuddies::create_cell_colors(cell_types = levels(sctyp
 # Excit.4   Excit.5   Excit.6   Excit.7   Excit.8   Excit.9   Inhib.1   Inhib.2   Inhib.3     Neu.1     Neu.2 
 # "#6DA9D2" "#85B7D9" "#9DC6E1" "#B5D4E8" "#CEE2F0" "#E6F0F7" "#E83E38" "#EF7E7A" "#F7BEBC" "#4E586A" "#A6ABB4" 
 
-cell_type_colors_anno <- DeconvoBuddies::create_cell_colors(cell_types = cell_type_anno_levels,
-                                                       pallet_name = "classic", 
-                                                       split = "\\.")
-# $broad
-# Astro      Endo     Micro       OPC     Oligo     Excit     Inhib 
-# "#3BB273" "#FF56AF" "#663894" "#F57A00" "#D2B037" "#247FBC" "#E83E38" 
-# 
-# $fine
-# Astro.1          Astro.2             Endo          Micro.1          Micro.2          Oligo.1          Oligo.2 
-# "#3BB273"        "#9DD8B9"        "#FF56AF"        "#663894"        "#B29BC9"        "#F57A00"        "#F8A655" 
-# Oligo.3              OPC     Excit.L2_3.1     Excit.L2_3.2     Excit.L2_3.3       Excit.L4_5         Excit.L5 
-# "#FBD2AA"        "#D2B037"        "#247FBC"        "#3F8FC4"        "#5A9FCC"        "#76AFD5"        "#91BFDD" 
-# Excit.L5_6_NP      Excit.L6_CT        Excit.L6b       Inhib.Pax6 Inhib.Lamp5_Lhx6      Inhib.Pvalb        Inhib.Vip 
-# "#ACCFE5"        "#C8DFEE"        "#E3EFF6"        "#E83E38"        "#EB5E59"        "#EF7E7A"        "#F39E9B" 
-# Inhib.Chandelier        Inhib.Sst 
-# "#F7BEBC"        "#FBDEDD" 
-
-cell_type_colors$anno <- cell_type_colors_anno$fine
+# Add annotation colors
+load(here("processed-data","00_project_prep","cell_type_colors_anno.Rdata"), verbose = TRUE)
+cell_type_colors$anno <- cell_type_colors_anno
 
 ## save all color output
 save(cell_type_colors, file = here("processed-data", "04_snRNA-seq", "cell_type_colors.Rdata"))
@@ -109,6 +94,15 @@ save(cell_type_colors, file = here("processed-data", "04_snRNA-seq", "cell_type_
 message(Sys.time(), " - Plot UMAP + TSNE")
 
 ## plot annotated clusters
+walk(c("UMAP", "TSNE"),
+     ~my_plot_reduced_dim(sce,
+                          prefix = "ERC_sn_reprocess",
+                          var_type = "cat",
+                          dimred = .x,
+                          my_var = "cell_type_broad",
+                          color_pal = cell_type_colors$broad,
+                          suffix = "sctype"))
+
 walk(c("UMAP", "TSNE"),
      ~my_plot_reduced_dim(sce,
                           prefix = "ERC_sn_reprocess",
@@ -123,7 +117,7 @@ walk(c("UMAP", "TSNE"),
                           prefix = "ERC_sn_reprocess",
                           var_type = "cat",
                           dimred = .x,
-                          my_var = "cell_type_fine",
+                          my_var = "cell_type_anno",
                           color_pal = cell_type_colors$anno,
                           suffix = "sctype"))
 
@@ -180,16 +174,16 @@ cluster_info |> arrange(median_sum)
 cell_class_cutoffs <- read.csv(here("processed-data", "04_snRNA-seq", "09_cluster_QC","ERC_sn_cell_class_cutoffs.csv"))
 
 cluster_metrics_long <- pd |> 
-    select(snn_kOpt, cell_type_class, cell_type_broad, cell_type_fine, sum, detected, subsets_Mito_percent, scDblFinder.score)  |>
-    pivot_longer(!c(snn_kOpt, cell_type_fine,cell_type_broad, cell_type_class), names_to = "metric") |>
-    group_by(snn_kOpt, cell_type_fine, cell_type_class, cell_type_broad, metric) |>
+    select(snn_kOpt, cell_type_class, cell_type_broad, cell_type_fine, cell_type_anno, sum, detected, subsets_Mito_percent, scDblFinder.score)  |>
+    pivot_longer(!c(snn_kOpt, cell_type_anno, cell_type_fine,cell_type_broad, cell_type_class), names_to = "metric") |>
+    group_by(snn_kOpt, cell_type_anno, cell_type_fine, cell_type_class, cell_type_broad, metric) |>
     summarize(median = median(value)) |>
     left_join(cell_class_cutoffs |> select(cell_type_class, metric, cutoff, cutoff_anno)) |>
     mutate(pass_metricQC = case_when(metric %in% c("scDblFinder.score", "subsets_Mito_percent") ~ median < cutoff,
                                      TRUE ~ median > cutoff),
            metric = factor(metric, levels = c("sum", "detected", "subsets_Mito_percent", "scDblFinder.score"))
     )|>
-    group_by(snn_kOpt, cell_type_fine, cell_type_class, cell_type_broad) |>
+    group_by(snn_kOpt, cell_type_anno, cell_type_fine, cell_type_class, cell_type_broad) |>
     mutate(passALL_metricQC = all(pass_metricQC))
 
 cluster_metrics_long |> ungroup() |> count(pass_metricQC, passALL_metricQC)
@@ -210,11 +204,11 @@ write.csv(cluster_info, file = here(data_dir, "ERC_sn_cluster_info.csv"), row.na
 qc_median_boxplot <- cluster_metrics_long |>
     ggplot(aes(x = cell_type_broad, y = median)) +
     geom_boxplot(outlier.shape = NA) +
-    geom_jitter(aes(fill = cell_type_fine, shape = passALL_metricQC)) +
+    geom_jitter(aes(fill = cell_type_anno, shape = passALL_metricQC)) +
     geom_hline(aes(yintercept = cutoff), color = "blue", linetype = "dashed") +
     geom_text(aes(y = cutoff, label = cutoff_anno), x = 1.5, color = "blue", vjust = -.5) +
     facet_grid(metric~cell_type_class, scales = "free") +
-    scale_fill_manual(values = cell_type_colors$fine, guide = "none") +
+    scale_fill_manual(values = cell_type_colors$anno, guide = "none") +
     scale_shape_manual(values = c(`TRUE` = 21, `FALSE` = 24)) +
     theme_bw()
 
@@ -222,7 +216,7 @@ ggsave(qc_median_boxplot, filename = here(plot_dir, "ERC_sn_QCmedian_boxplot.png
 
 qc_violin_plot_all <- pd |> 
     select(cell_type_class, cell_type_broad, cell_type_fine, passALL_metricQC, sum, detected, subsets_Mito_percent, scDblFinder.score)  |>
-    pivot_longer(!c(cell_type_fine,cell_type_broad, cell_type_class,passALL_metricQC), names_to = "metric") |>
+    pivot_longer(!c(cell_type_fine, cell_type_broad, cell_type_class,passALL_metricQC), names_to = "metric") |>
     mutate(metric = factor(metric, levels = c("sum", "detected", "subsets_Mito_percent", "scDblFinder.score"))) |>
     ggplot() +
     geom_violin(aes(x = cell_type_fine, y = value, fill = cell_type_fine, colour = passALL_metricQC), 
@@ -254,10 +248,10 @@ ggsave(n_cell_barplot, filename = here(plot_dir, "ERC_sn_barplot_ct_n_qc.png"), 
 ## with out QC fail clusters
 n_cell_barplot <- cluster_info |>
     filter(passALL_metricQC) |>
-    ggplot(aes(x = cell_type_fine, y = n, fill = cell_type_fine, color = passALL_metricQC)) +
+    ggplot(aes(x = cell_type_anno, y = n, fill = cell_type_anno, color = passALL_metricQC)) +
     geom_col() +
     geom_text(aes(label = n), size = 2, vjust = -0.5) +
-    scale_fill_manual(values = cell_type_colors$fine, guide = "none") +
+    scale_fill_manual(values = cell_type_colors$anno, guide = "none") +
     scale_color_manual(values = c(`TRUE` = "black", `FALSE` = "red")) +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
@@ -277,7 +271,6 @@ sce$cell_type_fine <- droplevels(sce$cell_type_fine)
 levels(sce$cell_type_fine)
 pd <- as.data.frame(colData(sce))
 ####  Cell Type Proportions ####
-
 cell_class_proportions <- pd |>
     group_by(sample_id, APOE, Sex, Ancestry, cell_type_class) |>
     summarize(n = n()) |> 
@@ -287,33 +280,65 @@ cell_class_proportions <- pd |>
 sample_neuron_order <- cell_class_proportions |> filter(cell_type_class == "neuron") |> arrange(prop) |> pull(sample_id)
 
 cell_type_proportions <- pd |>
-    group_by(sample_id, APOE, Sex, Ancestry, cell_type_fine) |>
+    group_by(sample_id, APOE, Sex, Ancestry, cell_type_anno) |>
     summarize(n = n()) |> 
     group_by(sample_id, APOE, Sex, Ancestry) |>
     mutate(prop = n/sum(n),
            sample_id = factor(sample_id, levels = sample_neuron_order))
 
+## plot n per sample
+cell_type_sample_n_bar <- cell_type_proportions |>
+    ggplot(aes(x = sample_id, y = n, fill = cell_type_anno)) +
+    geom_col() +
+    geom_text(aes(label = ifelse(n > 100, n, "")),
+              position = position_stack(vjust = .5),
+              size = 2) +
+    theme_bw() +
+    scale_fill_manual(values = cell_type_colors$anno) +
+    labs(y = "n Nucei")  +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))+
+    guides(fill=guide_legend(ncol=1))
+
+ggsave(cell_type_sample_n_bar, filename = here(plot_dir, "ERC_sn_barplot_ct_sample_n.png"), width = 10)
+
+## plot proprotions
 cell_type_proportion_bar <- cell_type_proportions |>
-    ggplot(aes(x = sample_id, y = prop, fill = cell_type_fine)) +
+    ggplot(aes(x = sample_id, y = prop, fill = cell_type_anno)) +
     geom_col() +
     geom_text(aes(label = ifelse(prop > .02, round(prop, 2), "")),
               position = position_stack(vjust = .5),
               size = 2) +
     theme_bw() +
-    scale_fill_manual(values = cell_type_colors$fine, guide = "none") +
+    scale_fill_manual(values = cell_type_colors$anno) +
     labs(y = "Cell Type Proportion")  +
-    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))+
+    guides(fill=guide_legend(ncol=1))
 
 ggsave(cell_type_proportion_bar, filename = here(plot_dir, "ERC_sn_barplot_ct_prop.png"), width = 10)
+
+cell_type_proportion_bar_facet <- cell_type_proportions |>
+    ggplot(aes(x = sample_id, y = prop, fill = cell_type_anno)) +
+    geom_col() +
+    geom_text(aes(label = ifelse(prop > .02, round(prop, 2), "")),
+              position = position_stack(vjust = .5),
+              size = 2) +
+    theme_bw() +
+    scale_fill_manual(values = cell_type_colors$anno) +
+    labs(y = "Cell Type Proportion")  +
+    facet_grid(.~APOE, scales = "free_x", space = "free") +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))+
+    guides(fill=guide_legend(ncol=1)) 
+
+ggsave(cell_type_proportion_bar_facet, filename = here(plot_dir, "ERC_sn_barplot_ct_prop_facet.png"), width = 10)
 
 ## evaluate proportion sample by cell 
 load(here("processed-data", "project_colors.Rdata"), verbose = TRUE)
 # we need sample_colors
 
 sample_proportions <- pd |>
-    group_by(sample_id, cell_type_fine) |>
+    group_by(sample_id, cell_type_anno) |>
     summarize(n = n()) |> 
-    group_by(cell_type_fine) |>
+    group_by(cell_type_anno) |>
     mutate(prop = n/sum(n),
            sample_id = factor(sample_id, levels = names(sample_colors))) 
 
@@ -321,7 +346,7 @@ sample_proportions <- pd |>
 sample_proportions |> arrange(-prop)
 
 sample_qc <- sample_proportions |> 
-    group_by(cell_type_fine) |>
+    group_by(cell_type_anno) |>
     summarize(n_samples = length(unique(sample_id)),
               max_prop = max(prop)) |>
     mutate(pass_sampleQC = max_prop < 0.5 & n_samples > 5)
@@ -330,13 +355,33 @@ sample_qc <- sample_proportions |>
 sample_qc |> arrange(n_samples)
 sample_qc |> arrange(-max_prop)
 
-## plot sample by cell type proportions bar
-sample_proportion_bar <- sample_proportions |>
-    ggplot(aes(x = cell_type_fine, y = prop, fill = sample_id)) +
+## n donor by cell type
+ct_n_donor <- sample_qc |>
+    ggplot(aes(y = reorder(cell_type_anno, n_samples), x = n_samples, fill = cell_type_anno)) +
     geom_col() +
-    geom_text(aes(label = ifelse(prop > 0.3, sprintf("%s - %g", sample_id, round(prop, 3)), "")),
-              position = position_stack(vjust = .5),
-              size = 2, angle = 90) +
+    geom_text(aes(label = n_samples), hjust = 1) +
+    scale_fill_manual(values = cell_type_colors$anno) +
+    theme_bw() +
+    labs(x = "n Donors", y = "Cell Type") +
+    theme(legend.position = "None")
+
+ggsave(ct_n_donor, filename = here(plot_dir, "ERC_sn_barplot_ct_n_donor.png"), width = 5, height = 6)
+
+
+## plot sample by cell type proportions bar
+sample_n_bar <- sample_proportions |>
+    ggplot(aes(x = cell_type_anno, y = n, fill = sample_id)) +
+    geom_col() +
+    theme_bw() +
+    scale_fill_manual(values = sample_colors) +
+    labs(y = "Cell Type Proportion")  +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) 
+
+ggsave(sample_n_bar, filename = here(plot_dir, "ERC_sn_barplot_sample_n.png"), width = 10)
+
+sample_proportion_bar <- sample_proportions |>
+    ggplot(aes(x = cell_type_anno, y = prop, fill = sample_id)) +
+    geom_col() +
     theme_bw() +
     scale_fill_manual(values = sample_colors) +
     labs(y = "Cell Type Proportion")  +
@@ -397,6 +442,26 @@ ggsave(sample_proportion_bar, filename = here(plot_dir, "ERC_sn_barplot_sample_p
 #     dev.off()
 #     })
 
+#### GTF file ####
+## Read in the gene information from the annotation GTF file
+gtf <-
+    rtracklayer::import(
+        "/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x/refdata-gex-GRCh38-2024-A/genes/genes.gtf.gz"
+    )
+gtf <- gtf[gtf$type == "gene"]
+names(gtf) <- gtf$gene_id
+
+## Match the genes
+all(rownames(sce) %in% gtf$gene_id)
+
+match_genes <- match(rownames(sce), gtf$gene_id)
+stopifnot(all(!is.na(match_genes)))
+
+## Keep only some columns from the gtf
+mcols(gtf) <- mcols(gtf)[, c("source", "type", "gene_id", "gene_version", "gene_name", "gene_type")]
+
+## Add the gene info to our SPE object
+rowRanges(sce) <- gtf[match_genes]
 
 #### Add colors to metadata ####
 
