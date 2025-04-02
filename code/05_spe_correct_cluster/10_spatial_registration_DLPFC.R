30f6f889a4a836d01fd656094cfed86ea214996b
Louise Huuki <lahuuki@gmail.com>
2025-04-02 14:58:37 -0400

    fix typo

--- code/05_spe_correct_cluster/10_spatial_registration_DLPFC.R
+++ code/05_spe_correct_cluster/10_spatial_registration_DLPFC.R
@@ -196,7 +196,7 @@ walk2(cor_anno, names(cor_anno), function(ca, name){
     
 })
 
-# slurmjobs::job_single('10_spatial_registration_DLPFC', create_shell = TRUE, memory = '25G', command = "Rcript 10_spatial_registration_DLPFC.R")
+# slurmjobs::job_single('10_spatial_registration_DLPFC', create_shell = TRUE, memory = '25G', command = "Rscript 10_spatial_registration_DLPFC.R")
 
 ## Reproducibility information
 print("Reproducibility information:")
