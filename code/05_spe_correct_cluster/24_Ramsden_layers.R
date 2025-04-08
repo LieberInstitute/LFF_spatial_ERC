ca297ff9108ae4a5653fb46a031edfde25c730fa
Louise Huuki <lahuuki@gmail.com>
2025-04-08 11:34:24 -0400

    Clean up script, improve emsembl lookup

--- code/05_spe_correct_cluster/24_Ramsden_layers.R
+++ code/05_spe_correct_cluster/24_Ramsden_layers.R
@@ -7,16 +7,11 @@ library("here")
 library("sessioninfo")
 library("readxl")
 library("jaffelab")
-
-# library("cowplot")
-# library("patchwork")
-
 library("org.Hs.eg.db")
-# library("RCurl")
-library("AnnotationHub")
+library("org.Mm.eg.db")
+library("Orthology.eg.db")
 
-## source reduced dims function
-
+## set up dirs
 plot_dir <- here("plots", "05_spe_correct_cluster", "24_Ramsden_layers")
 if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
@@ -26,64 +21,48 @@ if(!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
 #### Load Ramsden Gene lists ####
 # Ensembl mouse database (release 73)
 
-# ramsden_sheets <- c("L2only", "L3only", "L5only")
-# names(ramsden_sheets) <- ramsden_sheets
-# ramsden_data <- map(ramsden_sheets, ~read_excel(here("external-data", "Ramsden2015", "pcbi.1004032.s011.xls"), sheet = .x))
+ramsden_sheets <- c("L2only", "L3only", "L5only")
+names(ramsden_sheets) <- ramsden_sheets
+ramsden_data <- map(ramsden_sheets, ~read_excel(here("external-data", "Ramsden2015", "pcbi.1004032.s011.xls"), sheet = .x))
 
+## All Gene sheet
 ramsden_data_all <- read_excel(here("external-data", "Ramsden2015", "pcbi.1004032.s011.xls"), sheet = "AllGene")
 
 ramsden_data_all |> count(Vis) |> print(n = 32)
-
-ramsden_data_all <- ramsden_data |> filter(grepl("^L", Vis))
+# filter to layer specific - drop None, Uniform, error ect. 
+ramsden_data_all <- ramsden_data_all |> filter(grepl("^L", Vis))
 
 ramsden_data_list <- map(splitit(ramsden_data_all$Vis), ~ramsden_data_all$Name[.x])
+map_int(ramsden_data_list, length)
 
-# Basic function to convert mouse to human gene names from https://support.bioconductor.org/p/9153600/
-library(org.Hs.eg.db)
-library(org.Mm.eg.db)
-library(Orthology.eg.db)
+## all the same as individual lists? - Yes
+map2_lgl(ramsden_data, names(ramsden_data), ~setequal(.x$Name, ramsden_data_all |> filter(Vis == .y) |> pull(Name)))
 
-mouse_to_human <- function(mouseids, horg, morg, orth){
-    mouseg <- mapIds(morg, mouseids, "ENTREZID", "SYMBOL")
-    mapped <- select(orth, mouseg, "Homo_sapiens","Mus_musculus")
-    names(mapped) <- c("Mus_egid","Homo_egid")
-    husymb <- select(horg, as.character(mapped[,2]), "SYMBOL","ENTREZID")
-    # huensembl <- select(horg, as.character(mapped[,2]), "ENSEMBL","ENTREZID")
-    return(data.frame(Mus_symbol = mouseids,
-                      mapped,
-                      Homo_symbol = husymb[,2]))
+## look up emsembl w/ entrez
+mouse_entrez_to_human <- function(mouse_entrez){
+    mouse_entrez <- mouse_entrez[mouse_entrez != "0"]
+    mapped <- select(Orthology.eg.db, mouse_entrez, "Homo_sapiens","Mus_musculus")
+    hu_ensembl <- select(org.Hs.eg.db, as.character(mapped[,2]), "ENSEMBL","ENTREZID")
+    return(hu_ensembl)
 }
+#test
+# mouse_entrez_to_human(c("66643", "16498"))
 
-# ramsden_data_human <- map(ramsden_data, ~mouse_to_human(.x$Name, org.Hs.eg.db, org.Mm.eg.db, Orthology.eg.db))
-ramsden_data_human <- map(ramsden_data_list, ~mouse_to_human(.x, org.Hs.eg.db, org.Mm.eg.db, Orthology.eg.db))
+ramsden_data_list_entrez <- map(splitit(ramsden_data_all$Vis), ~ramsden_data_all$Entrez[.x])
+ramsden_data_human <- map(ramsden_data_list_entrez, ~mouse_entrez_to_human(.x))
 
-# TODO debug ENSEMBL lookup
-# mouse_to_human_ensembl <- function(mouseids, horg, morg, orth){
-#     mouseg <- mapIds(morg, mouseids, "ENTREZID", "SYMBOL")
-#     mapped <- select(orth, mouseg, "Homo_sapiens","Mus_musculus")
-#     names(mapped) <- c("Mus_egid","Homo_egid")
-#     husymb <- select(horg, as.character(mapped[,2]), "ENSEMBL","ENTREZID")
-#     return(data.frame(Mus_symbol = mouseids,
-#                       mapped,
-#                       Homo_symbol = husymb[,2]))
-# }
-# 
-# mouse_to_human_ensembl(c('Tle1', "Grp"), org.Hs.eg.db, org.Mm.eg.db, Orthology.eg.db)
-
-# ramsden_data_human <- map(ramsden_data, ~mouse_to_human_ensembl(.x$Name, org.Hs.eg.db, org.Mm.eg.db, Orthology.eg.db))
-# ℹ With name: L2only.
-# Caused by error in `data.frame()`:
-#     ! arguments imply differing number of rows: 76, 63
-
-ramsden_human_genes <- map(ramsden_data_human, ~unique(.x$Homo_symbol[!is.na(.x$Homo_symbol)]))
+ramsden_human_genes <- map(ramsden_data_human, "ENSEMBL")
 map_int(ramsden_human_genes, length)
-# L2only L3only L5only 
-# 58     21     46
+
+# L232    L233 L23even    L252    L255 L25even  L2high   L2mid  L2only  L2weak    L355 L35even  L3high   L3mid 
+# 45      22     290      42      29     169     371     709      76     124       5      24      59      31 
+# L3only  L5high   L5mid  L5only  L5weak 
+# 31      83     241      56      19 
 
 #### gene enrichment ####
 
 ## get reference layer enrichment statistics
-
+# 
 # erc_modeling <- readRDS(here("processed-data", "05_spe_correct_cluster", "08_model_pseudobulk", "BayesSpace_SVGm", "modeling_results-BayesSpace_SVGm_k09.rds"))
 # 
 # erc_spd_anno <- readxl::read_excel(here("processed-data","05_spe_correct_cluster", "10_spatial_registration_DLPFC", "ERC_SpD_spatial_registration_Annotations.xlsx")) |>
@@ -100,18 +79,6 @@ map_int(ramsden_human_genes, length)
 
 erc_modeling <- readRds(here("processed-data", "05_spe_correct_cluster", "22_SpD_clean_plots", "modeling_results-BayesSpace_SVGm_k09_annotated.rds"))
 
-erc_modeling$enrichment$gene
-
-table(duplicated(erc_modeling$enrichment$gene))
-
-map_lgl(ramsden_human_genes, ~"HSPA14" %in% .x)
-
-erc_modeling$enrichment <- erc_modeling$enrichment[erc_modeling$enrichment$gene != "HSPA14",]
-
-rownames(erc_modeling$enrichment) <- erc_modeling$enrichment$gene
-erc_modeling$enrichment$ensembl <- erc_modeling$enrichment$gene
-
-
 ## enrichment
 
 ramsden_enrichment <- gene_set_enrichment(
@@ -120,9 +87,8 @@ ramsden_enrichment <- gene_set_enrichment(
     model_type = "enrichment"
 )
 
-
-
-pdf(here(plot_dir, "ramsden_ernichment.pdf"))
+pdf(here(plot_dir, "ramsden_ernichment.pdf"), width = 10)
 gene_set_enrichment_plot(enrichment = ramsden_enrichment,
-                         plot_SetSize_bar = TRUE)
+                         plot_SetSize_bar = TRUE,
+                         )
 dev.off()
