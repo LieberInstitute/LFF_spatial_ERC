bd9d921347adb0e778aeb0038913af5109959bda
Louise Huuki <lahuuki@gmail.com>
2025-04-14 12:29:48 -0400

    Clean-up input w/ new data

--- code/04_snRNA-seq/19_sn_heatmaps.R
+++ code/04_snRNA-seq/19_sn_heatmaps.R
@@ -16,25 +16,25 @@ if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
 #### load data ####
 ## load psuedobulked sce data
-sce_pb <- readRDS(here("processed-data", "04_snRNA-seq", "17_sn_model_pseudobulk","sce_pseudobulk-cell_type_fine.rds"))
+sce_pb <- readRDS(here("processed-data", "04_snRNA-seq", "17_sn_model_pseudobulk","sce_pseudobulk-cell_type_anno.rds"))
 dim(sce_pb)
 
-rownames(sce_pb) <- rowData(sce_pb)$Symbol
+rownames(sce_pb) <- rowData(sce_pb)$gene_name
 
-anno_notes <- readxl::read_excel(here("processed-data", "04_snRNA-seq", "13_sctype_final", "sctype_final-NOTES.xlsx"))
+table(sce_pb$cell_type_anno)
+
+# anno_notes <- readxl::read_excel(here("processed-data", "04_snRNA-seq", "13_sctype_final", "sctype_final-NOTES.xlsx"))
 
 cell_anno_df <- colData(sce_pb) |>
     as.data.frame() |>
-    select(sample_id, APOE, Ancestry, Sex, Age, cell_type_broad, cell_type_fine, cell_type_fine, ncells) |>
-    left_join(anno_notes |> select(cell_type_fine, cell_type_anno = guess)) |>
-    mutate(APOE = gsub("^(E[2,3,4])(E[2,3,4])","\\1/\\2", APOE))
-
-cell_type_anno_levels <- cell_anno_df |> arrange(cell_type_broad, cell_type_anno) |> pull(cell_type_anno) |> unique()
+    select(sample_id, APOE, Ancestry, Sex, Age, cell_type_broad, cell_type_anno, cell_type_anno, ncells) |>
+    mutate(APOE = gsub("^(E[2,3,4])(E[2,3,4])","\\1/\\2", APOE),
+           cell_type_anno = droplevels(cell_type_anno)) |> 
+    arrange(cell_type_anno, sample_id)
 
 rownames(cell_anno_df) <- colnames(sce_pb)
 
-cell_anno_df$cell_type_anno <- factor(cell_anno_df$cell_type_anno, levels = cell_type_anno_levels)
-cell_anno_df <- cell_anno_df |> arrange(cell_type_anno, sample_id)
+cell_type_anno_levels <- levels(sce_pb$cell_type_anno)
 
 sce_pb <- sce_pb[,rownames(cell_anno_df)]
 
@@ -42,12 +42,12 @@ sce_pb <- sce_pb[,rownames(cell_anno_df)]
 load(here("processed-data", "04_snRNA-seq", "cell_type_colors.Rdata"), verbose = TRUE)
 load(here("processed-data", "project_colors.Rdata"), verbose = TRUE)
 
-setequal(names(APOE_genotype_colors), unique(cell_anno_df$APOE))
+## Check colors
+cell_type_colors$anno <- cell_type_colors$anno[cell_type_anno_levels]
+setequal(names(cell_type_colors$anno), cell_type_anno_levels)
+# setdiff(names(cell_type_colors$anno), cell_type_anno_levels)
 
-cell_type_colors_anno <- DeconvoBuddies::create_cell_colors(cell_types = cell_type_anno_levels,
-                                                            pallet_name = "classic", 
-                                                            split = "\\.")
-cell_type_colors$anno <- cell_type_colors_anno$fine
+setequal(names(APOE_genotype_colors), unique(cell_anno_df$APOE))
 
 ## cell annotations simple
 cell_row_ha_simple <- rowAnnotation(
@@ -93,7 +93,7 @@ cell_col_ha_details <- HeatmapAnnotation(
 #### Lit gene heatmap ####
 ## read in marker genes from lit
 lit_markers <- read_csv(here("processed-data","04_snRNA-seq", "00_lit_marker_genes", "lit_marker_summary.csv")) |>
-    mutate(in_data = gene_name %in% rowData(sce_pb)$Symbol,
+    mutate(in_data = gene_name %in% rowData(sce_pb)$gene_name,
            cell_type_broad = factor(cell_type_broad, levels = levels(sce_pb$cell_type_broad))) |>
     arrange(cell_type_broad)
 
@@ -102,12 +102,13 @@ lit_markers |> filter(is.na(cell_type_broad))
 lit_markers |> filter(!in_data)
 lit_markers |> filter(gene_name == "SYT1")
 
+lit_markers |> count(studies)
+
 lit_markers <- lit_markers |> filter(in_data & !is.na(cell_type_broad))
 
 nrow(lit_markers)
 
 ## gene annotation df
-
 lit_gene_annotation <- lit_markers |>
     select(gene_name, cell_type, cell_type_broad) |>
     column_to_rownames("gene_name")
@@ -160,15 +161,45 @@ Heatmap(lit_markers_zscore,
 )
 dev.off()
 
+#### Excit subtype heatmap  ####
+lit_markers_Excit_subtype <- read_csv(here("processed-data","04_snRNA-seq", "00_lit_marker_genes","Davila-Velderrain_Excit_subtype_markers.csv"))
+
+subtype_gene_annotation <- lit_markers_Excit_subtype |>
+    filter(in_data) |>
+    select(gene_name, cell_type) |>
+    column_to_rownames("gene_name")
+
+subtype_gene_col_ha <- HeatmapAnnotation(df = subtype_gene_annotation |> select(cell_type))
+
+## extract z-scores
+subtype_markers_zscore <- scale(t(logcounts(sce_pb)[rownames(subtype_gene_annotation), grep("Excit", colnames(sce_pb))]))
+dim(subtype_markers_zscore)
+subtype_markers_zscore[1:5,1:5]
+
+## plot heatmaps
+pdf(here(plot_dir, "ERC_sn_Excit_subtype_gene_heatmap.pdf"), height = 8, width = 11)
+Heatmap(subtype_markers_zscore,
+        name = "Z Score",
+        cluster_rows = TRUE,
+        cluster_columns = TRUE,
+        show_row_names = FALSE,
+        # row_split = cell_anno_df$cell_type_anno,
+        # right_annotation = cell_row_ha_simple,
+        bottom_annotation = subtype_gene_col_ha
+)
+dev.off()
+
+
+
 
 #### MeanRatio heatmap ####
 ## load marker gene data
-load(here("processed-data", "04_snRNA-seq", "16_sn_MeanRatio", "MarkerStats_cell_type_fine.Rdata"), verbose = TRUE)
+load(here("processed-data", "04_snRNA-seq", "16_sn_MeanRatio", "MarkerStats_cell_type_anno.Rdata"), verbose = TRUE)
 
 marker_stats_top <- marker_stats |>
     filter(MeanRatio.rank <= 5, MeanRatio > 1, gene_name %in% rownames(sce_pb)) |>
-    select(gene, MeanRatio.rank, cell_type_fine = cellType.target) |>
-    left_join(anno_notes |> select(cell_type_fine, cell_type_anno = guess)) |>
+    select(gene, MeanRatio.rank, cell_type_anno = cellType.target) |>
+    left_join(anno_notes |> select(cell_type_anno, cell_type_anno = guess)) |>
     mutate(cell_type_anno = factor(cell_type_anno, levels = cell_type_anno_levels)) |>
     arrange(cell_type_anno) |>
     column_to_rownames("gene")
@@ -235,7 +266,7 @@ dev.off()
 
 #### Enrichment heatmap ####
 ## load marker gene data
-modeling_results <- readRDS(here("processed-data", "04_snRNA-seq", "17_sn_model_pseudobulk", "modeling_results-cell_type_fine.rds"))
+modeling_results <- readRDS(here("processed-data", "04_snRNA-seq", "17_sn_model_pseudobulk", "modeling_results-cell_type_anno.rds"))
 
 rowData(sce_pb)$gene_name <- rowData(sce_pb)$ID
 
@@ -246,8 +277,8 @@ enrichment_stats_top <- sig_genes_extract(
     reverse = FALSE,
     sce_layer = sce_pb
 ) |>
-    select(ensembl, fdr, top, logFC, cell_type_fine = test) |>
-    left_join(anno_notes |> select(cell_type_fine, cell_type_anno = guess)) |>
+    select(ensembl, fdr, top, logFC, cell_type_anno = test) |>
+    left_join(anno_notes |> select(cell_type_anno, cell_type_anno = guess)) |>
     mutate(cell_type_anno = factor(cell_type_anno, levels = cell_type_anno_levels)) |>
     arrange(cell_type_anno) |>
     column_to_rownames("ensembl")
