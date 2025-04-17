08428356d925f43a5c51aa70024746cd3fe4cd74
Louise Huuki <lahuuki@gmail.com>
2025-04-17 13:12:02 -0400

    Fix aggregateAcrossCells SpatialExperiment bug

--- code/05_spe_correct_cluster/25_SpD_pseudobulk.R
+++ code/05_spe_correct_cluster/25_SpD_pseudobulk.R
@@ -16,6 +16,10 @@ spec <- matrix(
 opt <- getopt(spec)
 print(opt)
 
+cluster_var <- opt$cluster
+# cluster_var <- "SpD"
+
+## prep dir
 data_dir <- here("processed-data", "04_snRNA-seq", "25_SpD_pseudobulk")
 if(!dir.exists(data_dir)) dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
 
@@ -23,38 +27,33 @@ if(!dir.exists(data_dir)) dir.create(data_dir, showWarnings = FALSE, recursive =
 message(Sys.time(), " - Load HDF5 SPE")
 spe <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "spe_objects", "spe_ERC_annotated"))
 
-#### Run Spatial Registration Function ####
-cluster_var <- opt$cluster
-
+#### Prep for pseudobulk ####
 stopifnot(cluster_var %in% colnames(colData(spe)))
 
 table(spe[[cluster_var]])
+class(spe)
+
+spatialCoords(spe) <- NULL
+imgData(spe) <- NULL
+
+## make single cell 
+spe <- as(spe, "SingleCellExperiment")
 
 if(cluster_var == "SpD"){
     ## add syntacticly valid version of SpD
-    spe$SpD_syn <- gsub("~", "_", spe$SpD)
+    spe$SpD_syn <- factor(gsub("~", "_", spe$SpD), levels = gsub("~", "_", levels(spe$SpD)))
     table(spe$SpD_syn)
     
     cluster_var <- "SpD_syn"
 }
 
-
+#### pseudobulk ####
 message(Sys.time(), " make pseudobulk object")
 spe_pseudo <- scuttle::aggregateAcrossCells(
     spe,
-    DataFrame(
-        cluster_var = spe[[cluster_var]]
-    )
+    ids = spe[["cluster_var"]]
 )
-# Error in .local(x, ..., value) : New 'sample_id's must map uniquely
 
-colnames(spe_pseudo) <- spe_pseudo$cluster_var
-
-## Drop lowly-expressed genes
-# message(Sys.time(), " drop lowly expressed genes")
-# keep_expr <-
-#     edgeR::filterByExpr(spe_pseudo, group = spe_pseudo$cluster_var)
-# spe_pseudo <- spe_pseudo[which(keep_expr), ]
 
 message(sprintf("pseudobulk dims: %d row, %d col", nrow(spe_pseudo), ncol(spe_pseudo)))
 
