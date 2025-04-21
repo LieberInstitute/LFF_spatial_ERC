0987c749bf101ce51f89ef88a3baf6837b93a8a9
Louise Huuki <lahuuki@gmail.com>
2025-04-21 17:31:00 -0400

    Run variance partition on sample pseudobulk sn data

--- code/08_pseudoBulkDGE_sn/02_covariate_analysis.R
+++ code/08_pseudoBulkDGE_sn/02_covariate_analysis.R
@@ -24,16 +24,16 @@ if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 # message(Sys.time(), " - Load HDF5 sce")
 # sce <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "sce_objects", "sce_ERC"))
 
-data <- readRDS(here("processed-data", "08_pseudoBulkDGE_sn", "01_pseudobulk_data","sce_pseudo_DGE.RDS"))
+sce_pb <- readRDS(here("processed-data", "08_pseudoBulkDGE_sn", "01_pseudobulk_data","sce_pseudo_DGE.RDS"))
 
 ## load colors
 load(here("processed-data", "04_snRNA-seq", "cell_type_colors.Rdata"), verbose = TRUE)
 load(here("processed-data", "project_colors.Rdata"), verbose = TRUE)
 
-colnames(colData(data))
+colnames(colData(sce_pb))
 
 # Extract the relevant columns from the data
-plot_data <- as.data.frame(colData(data)) |>
+plot_data <- as.data.frame(colData(sce_pb)) |>
     select(sample_id, Age, Sex, Ancestry, Anc_Afr, APOE_carrier, APOE,
            cell_type_anno, ncells, exp_round, seq_round,
            pseudo_sum_umi, pseudo_expr_chrM, pseudo_expr_chrM_ratio) 
@@ -101,7 +101,50 @@ for (cell_type in levels(plot_data$cell_type_anno)) {
 }
 dev.off()
 
+#### variance parition on Sample level data ####
+library("variancePartition")
+# load sample level data
 
+sce_pb_sample <- readRDS(here("processed-data", "04_snRNA-seq", "20_sn_pseudobulk", "sce_pseudobulk_only-sample_id.rds"))
+dim(sce_pb_sample)
+table(sce_pb_sample$APOE)
+# E2/E2 E2/E3 E3/E4 E4/E4 
+# 6     8    10     7 
+table(sce_pb_sample$APOE_carrier)
+# E2+ E4+ 
+# 14  17 
+
+pd_sample <- as.data.frame(colData(sce_pb_sample))
+
+# Assess correlation between all pairs of variables
+colnames(pd_sample)
+
+# form <- ~ APOE + sample_id + Sex + Age + Anc_Afr + exp_round + seq_round + ncells
+form <- ~ APOE + sample_id + exp_round
+# form <- ~ APOE + Sex + Age + Anc_Afr + exp_round + seq_round + ncells
+
+C <- canCorPairs(form, pd_sample)
+# ~ APOE + sample_id + Sex + Age + Anc_Afr + exp_round + seq_round + ncells
+# APOE and sample_id
+# sample_id and Sex
+# sample_id and Age
+# sample_id and Anc_Afr
+# sample_id and exp_round
+# sample_id and seq_round
+# sample_id and ncells
+
+pdf(here(plot_dir, "sn_variable_cor_matrix.pdf"))
+plotCorrMatrix(C)
+dev.off()
+
+## test variance partition 
+form <- ~ (1 | APOE_carrier) + (1 | APOE) + (1 | Sex) + Anc_Afr + Age + (1 | exp_round) + (1 | seq_round)
+varPart <- fitExtractVarPartModel(logcounts(sce_pb_sample), form, pd_sample)
+vp <- sortCols(varPart)
+
+## violin plot 
+sn_vp_violin <- plotVarPart(vp)
+ggsave(sn_vp_violin, filename = here(plot_dir, "sn_vp_violin.png"))
 
 # slurmjobs::job_single('01_create_pseudobulk_data', create_shell = TRUE, memory = '25G', command = "Rscript 01_create_pseudobulk_data.R")
 
