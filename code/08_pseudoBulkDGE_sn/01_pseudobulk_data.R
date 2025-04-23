50831b66e24c2b468004cc34d478b255e93bc37b
lahuuki <lahuuki@gmail.com>
2025-04-23 15:58:56 -0400

    pseudobulk broad cell types too

--- code/08_pseudoBulkDGE_sn/01_pseudobulk_data.R
+++ code/08_pseudoBulkDGE_sn/01_pseudobulk_data.R
@@ -21,10 +21,19 @@ sce <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "sce_objec
 
 # rowData(sce)
 
-#### run pseudbulk ####
+## add syntactic APOE vars
+sce_pseudo$APOE_syn <- factor(gsub("/", ".", sce_pseudo$APOE))
+levels(sce_pseudo$APOE_syn)
+
+sce_pseudo$APOE_carrier_syn <- factor(gsub("\\+", "", sce_pseudo$APOE_carrier))
+levels(sce_pseudo$APOE_carrier_syn)
+
+## get mito genes
 mito_genes <- as.logical(seqnames(sce) == "chrM")
 table(mito_genes)
 
+#### run pseudobulk - cell_type_anno ####
+message(Sys.time(), "pseudobulk cell_type_anno")
 sce_pseudo <- registration_pseudobulk(
     sce,
     var_registration = "cell_type_anno",
@@ -41,30 +50,46 @@ message(Sys.time(), " - Done pseudobulk")
 message(sprintf("nrow: %d, ncol: %d", nrow(sce_pseudo), ncol(sce_pseudo)))
 
 ## Drop cell types w/ not enough pseudobulk samples?
-table(sce_pseudo$APOE_carrier, sce_pseudo$cell_type_anno)
-# Astro.1 Astro.2 Endo Micro.1 Micro.2 Oligo.1 Oligo.2 OPC Excit.L2 Excit.L2_5.1 Excit.L2_5.2 Excit.L5.1 Excit.L5.2
-# E2+      14      14   14      14      14      14      14  14       10           14            6         11          4
-# E4+      17      17   16      17      17      17      17  17       15           17            9         14          6
-# 
-# Excit.L5_6_NP Excit.L6_CT Excit.L6b Inhib.Pax6 Inhib.Lamp5_Lhx6 Inhib.Pvalb Inhib.Vip Inhib.Chandelier Inhib.Sst
-# E2+             1           7        10          0               12          11        14                4        12
-# E4+             5          10         9          2               15          16        17               11        15
+table(sce_pseudo$APOE_carrier, sce_pseudo$cell_type_bulk)
 
-#### Additional edits ####
+#### Additional edits + Save ####
 ## drop all NA cols
 all_na <- sapply(colData(sce_pseudo), function(x)all(is.na(x)))
 colData(sce_pseudo) <- colData(sce_pseudo)[, names(all_na)[!all_na]]
 
-## add syntactic APOE vars
-sce_pseudo$APOE_syn <- factor(gsub("/", ".", sce_pseudo$APOE))
-levels(sce_pseudo$APOE_syn)
-
-sce_pseudo$APOE_carrier_syn <- factor(gsub("\\+", "", sce_pseudo$APOE_carrier))
-levels(sce_pseudo$APOE_carrier_syn)
-
-#### save ####
+## save
 message(Sys.time(), " - Save")
 saveRDS(sce_pseudo, file = here(data_dir, "sce_pseudo_DGE.RDS"))
+
+#### run pseudbulk - cell_type_bulk ####
+message(Sys.time(), "pseudobulk cell_type_broad")
+sce_pseudo <- registration_pseudobulk(
+    sce,
+    var_registration = "cell_type_bulk",
+    var_sample_id = "sample_id",
+    covars = NULL,
+    min_ncells = 10,
+    pseudobulk_rds_file = NULL,
+    filter_expr = FALSE,
+    mito_gene = mito_genes
+)
+
+message(Sys.time(), " - Done pseudobulk")
+
+message(sprintf("nrow: %d, ncol: %d", nrow(sce_pseudo), ncol(sce_pseudo)))
+
+## Drop cell types w/ not enough pseudobulk samples?
+table(sce_pseudo$APOE_carrier, sce_pseudo$cell_type_bulk)
+
+#### Additional edits + Save ####
+## drop all NA cols
+all_na <- sapply(colData(sce_pseudo), function(x)all(is.na(x)))
+colData(sce_pseudo) <- colData(sce_pseudo)[, names(all_na)[!all_na]]
+
+## save 
+message(Sys.time(), " - Save")
+saveRDS(sce_pseudo, file = here(data_dir, "sce_pseudo_DGE_broad.RDS"))
+
 
 # sce_pseudo <- readRDS(here("processed-data", "08_pseudoBulkDGE_sn", "sce_pseudo_DGE.RDS"))
 
