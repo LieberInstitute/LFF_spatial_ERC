10b49cb9f6f5ac2a8f03747fd2e1e4e3e44420dd
Louise Huuki <lahuuki@gmail.com>
2025-04-25 13:57:14 -0400

    Create script to sub-cluster glial cell types

---
+++ code/04_snRNA-seq/25_sn_cluster_subtype.R
@@ -0,0 +1,120 @@
+## Louise Huuki-Myers, April 2025
+## Run subtype clustering on sn data
+
+## Required libraries
+library("here")
+library("sessioninfo")
+library("SingleCellExperiment")
+library("HDF5Array")
+library("getopt")
+library("scater")
+library("scran")
+
+# Import command-line parameters
+scec <- matrix(
+    c("cell_type", "c", "1", "character", "Name of cell type to sub-cluster"),
+    ncol = 5, byrow = TRUE
+)
+opt <- getopt(scec)
+print(opt)
+
+cell_type <- opt$cell_type
+
+# cell_type = "Astro"
+
+data_dir <- here("processed-data", "04_snRNA-seq", "25_sn_cluster_subtype", cell_type)
+if(!dir.exists(data_dir)) dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
+
+# plot_dir <- here("plots", "04_snRNA-seq", "25_sn_cluster_subtype")
+# if(!dir.exists(plot_dir)) dir.create(plot_dir, showWarnings = FALSE, recursive = TRUE)
+
+#### Load the data ####
+message(Sys.time(), " - Load HDF5 sce")
+sce <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "sce_objects", "sce_ERC"))
+
+stopifnot(cell_type %in% levels(sce$cell_type_broad))
+
+## subset by Broad cell type
+sce <- sce[,sce$cell_type_broad == cell_type]
+message(Sys.time(), sprintf(" - Subset to %s, ncells = %i", cell_type, ncol(sce)))
+
+table(sce$sample_id)
+
+
+#### GLM PCA ####
+set.seed(425)
+
+message(Sys.time(), " - running Deviance Feat. Selection")
+sce <- scry::devianceFeatureSelection(sce,
+                                      assay = "counts", fam = "binomial", sorted = F,
+                                      batch = as.factor(sce$seq_round)
+)
+
+## plot biononial deviance distribution
+pdf(here(plot_dir, sprintf("sn_reprocess_binomial_deviance_%s.pdf", cell_type)))
+plot(sort(rowData(sce)$binomial_deviance, decreasing = T),
+     type = "l", xlab = "ranked genes",
+     ylab = "binomial deviance", main = "Feature Selection with Deviance"
+)
+abline(v = 2000, lty = 2, col = "red")
+dev.off()
+
+message(Sys.time(), " - running nullResiduals")
+sce <- nullResiduals(sce,
+                     assay = "counts", 
+                     fam = "binomial", # default params
+                     type = "deviance"
+)
+
+
+hdgs <- rownames(sce)[order(rowData(sce)$binomial_deviance, decreasing = T)][1:2000]
+
+message(Sys.time(), " - running PCA")
+sce <- runPCA(sce,
+              exprs_values = "binomial_deviance_residuals",
+              subset_row = hdgs,
+              ncomponents = 100,
+              name = "GLMPCA_approx",
+              BSPARAM = BiocSingular::IrlbaParam()
+)
+
+#### Batch Correction ####
+message(Sys.time(), " - HARMONY Correction")
+
+## Run harmony
+## needs PCA
+reducedDim(sce, "PCA") <- reducedDim(sce, "GLMPCA_approx")
+
+message("running Harmony - ", Sys.time())
+sce <- harmony::RunHarmony(sce, group.by.vars = "sample_id", verbose = TRUE)
+
+## Remove redundant PCA
+reducedDim(sce, "PCA") <- NULL
+
+save(reducedDim(sce, "HARMONY"), file = here(data_dir, sprintf("subcluster_HARMONY_pca_%.rdata", cell_type)))
+
+#### SNN + Walktrap cluster ####
+
+## Build SNN graph
+k = 20
+message(Sys.time(), " - running buildSNNGraph: k =", k)
+snn.gr <- buildSNNGraph(sce, k = k, use.dimred = "HARMONY")
+
+## Run walk trap clustering
+message(Sys.time(), " - running walktrap")
+clusters <- igraph::cluster_walktrap(snn.gr)$membership
+table(clusters)
+
+## save data
+message(Sys.time() , " - saving data")
+save(clusters, file = here(data_dir, sprintf("walktrap_snn_k%02d_subclusters_%s.Rdata", k, cell_type)))
+
+slurmjobs::job_loop(loops = list(cell_type = c("Oligo", "Astro", "Micro", "Endo")), create_shell = TRUE, name = "25_sn_cluster_subtype", create_script = FALSE)
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
+
