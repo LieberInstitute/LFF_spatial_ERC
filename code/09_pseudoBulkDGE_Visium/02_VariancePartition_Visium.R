2d8208f7d6b1901841bff73c13f341ed86b8b017
Louise Huuki <lahuuki@gmail.com>
2025-05-06 11:29:57 -0400

    scale and add pseudo_sum_umi to forms

--- code/09_pseudoBulkDGE_Visium/02_VariancePartition_Visium.R
+++ code/09_pseudoBulkDGE_Visium/02_VariancePartition_Visium.R
@@ -38,7 +38,8 @@ message(sprintf("Subset to %s - %i spots", opt$spd, nrow(spe_pb)))
 spe_pb$APOE_E4E4 <- spe_pb$APOE == "E4/E4"
 table(spe_pb$APOE_E4E4)
 
-## scale Mitocondrial ratio
+## scale sum_umi & Mitocondrial ratio
+spe_pb$pseudo_sum_umi <- scale(spe_pb$pseudo_sum_umi)
 spe_pb$pseudo_expr_chrM_ratio <- scale(spe_pb$pseudo_expr_chrM_ratio)
 
 #### Assess correlation between variables ####
@@ -59,10 +60,10 @@ dev.off()
 #### Variance Partition ####
 
 my_forms <- list(
-    global = ~ (1 | APOE_carrier) + APOE_num + Anc_Afr + (1 | Sex) + Age + Rin + (1 | Visium_slide) + (1 | round) + ncells + pseudo_expr_chrM_ratio,
-    carrier_i = ~ (1 | APOE_carrier:Anc_Afr) + (1 | APOE_carrier:Sex) + (1 | APOE_carrier) + Anc_Afr  + (1 | Sex) + Age + Rin + (1 | Visium_slide) + (1 | round) + ncells + pseudo_expr_chrM_ratio,
-    numeric_i = ~ APOE_num:Anc_Afr + (1 | APOE_num:Sex) + APOE_num + Anc_Afr + (1 | Sex) + Age + Rin + (1 | Visium_slide) + (1 | round) + ncells + pseudo_expr_chrM_ratio,
-    e4e4_i = ~ (1 | APOE_E4E4:Anc_Afr) + (1 | APOE_E4E4:Sex) + (1 | APOE_E4E4) + Anc_Afr  + (1 | Sex) + Age + Rin + (1 | Visium_slide) + (1 | round) + ncells + pseudo_expr_chrM_ratio
+    global = ~ (1 | APOE_carrier) + APOE_num + Anc_Afr + (1 | Sex) + Age + Rin + (1 | Visium_slide) + (1 | round) + ncells + pseudo_expr_chrM_ratio + pseudo_sum_umi,
+    carrier_i = ~ (1 | APOE_carrier:Anc_Afr) + (1 | APOE_carrier:Sex) + (1 | APOE_carrier) + Anc_Afr  + (1 | Sex) + Age + Rin + (1 | Visium_slide) + (1 | round) + ncells + pseudo_expr_chrM_ratio + pseudo_sum_umi,
+    numeric_i = ~ APOE_num:Anc_Afr + (1 | APOE_num:Sex) + APOE_num + Anc_Afr + (1 | Sex) + Age + Rin + (1 | Visium_slide) + (1 | round) + ncells + pseudo_expr_chrM_ratio + pseudo_sum_umi,
+    e4e4_i = ~ (1 | APOE_E4E4:Anc_Afr) + (1 | APOE_E4E4:Sex) + (1 | APOE_E4E4) + Anc_Afr  + (1 | Sex) + Age + Rin + (1 | Visium_slide) + (1 | round) + ncells + pseudo_expr_chrM_ratio + pseudo_sum_umi
 )
 
 varPart_summary <- map2_dfr(my_forms, names(my_forms), function(form, form_name){
