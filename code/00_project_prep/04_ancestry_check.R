bbda15385eceb010d8e72b5208a7d698ad5c682d
lahuuki <lahuuki@gmail.com>
2025-05-13 15:00:56 -0400

    Update ancestry check to use Feb 2025 FLARE Data, improve plots

--- code/00_project_prep/04_ancestry_check.R
+++ code/00_project_prep/04_ancestry_check.R
@@ -1,25 +1,28 @@
+## Louise Huuki-Myers, May 2025
+## Read and check ancestry data for donors
 
-library(tidyverse)
-library(here)
-library(sessioninfo)
+library("tidyverse")
+library("here")
+library("sessioninfo")
 
 plot_dir <- here("plots", "00_project_prep", "04_ancestry_check")
 if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
-ancestry_tab <- read.table(here("raw-data", "ancestry", "structure.out_ancestry_proportion_raceDemo_compare"),
-                           header = TRUE) |>
-    rename(BrNum = id)
+## Feb 2025 update
+## Shizhong FLARE estimates
+ancestry_tab <- read.csv("/dcs04/lieber/shared/statsgen/local_ancestry/flare_array_data/global_ancestry.csv", row.names = 1) |>
+    select(BrNum = ID, Afr = YRI, Eur = CEU)
+
 head(ancestry_tab)
-# id   Afr   Eur Race
-# 1 Br1857 0.000 1.000 CAUC
-# 2 Br1306 0.791 0.209   AA
-# 3 Br6171 0.000 1.000 CAUC
-# 4 Br8404 0.000 1.000 CAUC
-# 5 Br1802 0.854 0.146   AA
-# 6 Br6345 0.735 0.265   AA
+
+## Old data 
+# ancestry_tab <- read.table(here("raw-data", "ancestry", "structure.out_ancestry_proportion_raceDemo_compare"),
+#                            header = TRUE) |>
+#     rename(BrNum = id)
+
 
 dim(ancestry_tab)
-# [1] 2743    4
+# [1] 3134    4
 
 metadata_visium_list <- read_csv(file = here("processed-data", "00_project_prep", "02_get_online_metadata", "metadata_visium_list.csv")) |> 
     left_join(ancestry_tab) |>
@@ -28,6 +31,12 @@ metadata_visium_list <- read_csv(file = here("processed-data", "00_project_prep"
 samples_ancestry <- metadata_visium_list |> select(BrNum, Afr, Eur)
 save(samples_ancestry, file = here("processed-data","00_project_prep", "04_ancestry_check", "sample_ancestry.Rdata"))
 write.csv(samples_ancestry, file = here("processed-data","00_project_prep", "04_ancestry_check", "sample_ancestry.csv"), row.names = FALSE)
+
+
+#### plot ancestry fractions ####
+load(here("processed-data", "project_colors.Rdata"), verbose = TRUE)
+
+names(ancestry_colors) <- c("Eur", "Afr")
 
 sample_ancestry_long <- metadata_visium_list |> 
     select(BrNum, APOE, Ancestry, Afr, Eur) |>
@@ -40,22 +49,33 @@ sample_APOE_bar <- sample_ancestry_long |>
     mutate(APOE_anno = paste(APOE, "n=", n()/2)) |>
     ggplot(aes(x = BrNum, y = anc_prop, fill = anc_type)) +
     geom_col() +
-    facet_wrap(~APOE_anno, scales = "free_x", ncol = 1)
+    facet_wrap(~APOE_anno, scales = "free_x", ncol = 1) +
+    scale_fill_manual(values = ancestry_colors) +
+    theme_bw() 
 
-ggsave(sample_APOE_bar, filename = here(plot_dir, "Ancestry_APOE_bar.png"))
+ggsave(sample_APOE_bar, filename = here(plot_dir, "Ancestry_APOE_bar.png"), width = 8)
 
-sample_ancestry_long
-    ggplot(aes(x = BrNum, y = anc_prop, fill = anc_type)) +
-    geom_col() +
-    facet_grid(APOE~Ancestry)
+# sample_ancestry_long |>
+#     ggplot(aes(x = BrNum, y = anc_prop, fill = anc_type)) +
+#     geom_col() +
+#     facet_grid(APOE~Ancestry)
 
 sample_ancestry_bar <- sample_ancestry_long |> 
     group_by(APOE, Ancestry) |>
     mutate(APOE_anno = paste(APOE, Ancestry, "n=", n()/2)) |>
     ggplot(aes(x = BrNum, y = anc_prop, fill = anc_type)) +
     geom_col() +
-    facet_wrap(~APOE_anno, scales = "free_x", ncol = 2)
+    facet_wrap(~APOE_anno, scales = "free_x", ncol = 2)   +
+    scale_fill_manual(values = ancestry_colors) +
+    theme_bw() 
 
 ggsave(sample_ancestry_bar, filename = here(plot_dir, "Ancestry_cat_APOE_bar.png"))
 
+# slurmjobs::job_single('04_ancestry_check', create_shell = TRUE, memory = '5G', command = "Rscript 04_ancestry_check.R")
 
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
