b59bc1a86c0855b8aac3ab27e9567d7c70148aed
lahuuki <lahuuki@gmail.com>
2025-05-16 13:21:41 -0400

    Pseudobulk only by SpD

--- code/05_spe_correct_cluster/25_SpD_pseudobulk.sh
+++ code/05_spe_correct_cluster/25_SpD_pseudobulk.sh
@@ -27,7 +27,7 @@ module load conda_R/4.4.x
 module list
 
 ## Edit with your job command
-Rscript 25_SpD_pseudobulk.R --cluster 'sample_id'
+Rscript 25_SpD_pseudobulk.R --cluster 'SpD'
 
 echo "**** Job ends ****"
 date
