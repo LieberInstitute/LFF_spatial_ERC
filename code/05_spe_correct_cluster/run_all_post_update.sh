70e697c347ff033227d5a94fbcb3974b128151a7
lahuuki <lahuuki@gmail.com>
2025-05-16 13:33:13 -0400

    rename w/o number

--- code/05_spe_correct_cluster/run_all_post_update.sh
+++ code/05_spe_correct_cluster/run_all_post_update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -p shared
 #SBATCH --mem=25G
-#SBATCH --job-name=19_SpD_update_spe
+#SBATCH --job-name=run_all_post_update
 #SBATCH -c 1
 #SBATCH -t 1-00:00:00
-#SBATCH -o logs/19_SpD_update_spe.txt
-#SBATCH -e logs/19_SpD_update_spe.txt
+#SBATCH -o logs/run_all_post_update.txt
+#SBATCH -e logs/run_all_post_update.txt
 #SBATCH --mail-type=ALL
 
 set -e
@@ -25,6 +25,8 @@ CODEDIR="${MAINDIR}/code/05_spe_correct_cluster/"
 
 cd ${CODEDIR}
 
+## Run all (most) code post 19_SpD_update_spe
+
 ## run modeling on annotated data
 rm -f "logs/20_model_pseudobulk_anno.txt"
 id1=$(sbatch --parsable 20_model_pseudobulk_anno.sh)
@@ -34,15 +36,18 @@ rm -f "logs/22_SpD_clean_plots.txt"
 sbatch --parsable --dependency=afterok:$id1 22_SpD_clean_plots.sh
 
 ## pseudobulk by SpD
+rm -f "logs/25_SpD_pseudobulk.txt"
 id2=$(sbatch --parsable 25_SpD_pseudobulk.sh)
 
 ## mean ratio
-sbatch --parsable 27_SpD_MeanRatio.sh
+rm -f "logs/27_SpD_MeanRatio.txt"
+id3=$(sbatch --parsable 27_SpD_MeanRatio.sh)
 
 ## plot heatmaps - dependent on 20_model_pseudobulk_anno & 25_SpD_pseudobulk
-sbatch --parsable --dependency=afterok:$id1 23_SpD_heatmaps.sh
+rm -f "logs/23_SpD_heatmaps.txt"
+sbatch --parsable --dependency=afterok:$id1:$id2:$id3 23_SpD_heatmaps.sh
 
+echo "**** Job ends ****"
+date
 
-
-28_SpD_pseudobulk_pca.R
 
