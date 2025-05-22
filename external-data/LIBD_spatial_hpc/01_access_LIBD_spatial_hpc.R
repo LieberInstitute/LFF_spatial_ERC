6996e75ab7c3d5faf336cf379eb0e762f5820ba4
lahuuki <lahuuki@gmail.com>
2025-05-22 08:26:16 -0400

    Note paths for spatialHPC data

---
+++ external-data/LIBD_spatial_hpc/01_access_LIBD_spatial_hpc.R
@@ -0,0 +1,6 @@
+
+
+#### 1-vs-all: superfine cell class (60 groups) ####
+# sn superfine DE code: https://github.com/LieberInstitute/spatial_hpc/blob/main/snRNAseq_hpc/code/revision/methods_pseudobulk-DEGs.R
+
+reg = read.csv("/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/snRNAseq_hpc/processed-data/revision/sn_enrichment_stats_superfine.csv", row.names=1)
