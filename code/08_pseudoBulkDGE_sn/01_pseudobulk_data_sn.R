c1cbf17d1aee1e449408ca5ab5a22d523e0e95b9
lahuuki <lahuuki@gmail.com>
2025-05-27 16:08:47 -0400

    Add e4 variable in pseudobulk stage

--- code/08_pseudoBulkDGE_sn/01_pseudobulk_data_sn.R
+++ code/08_pseudoBulkDGE_sn/01_pseudobulk_data_sn.R
@@ -39,6 +39,10 @@ levels(sce$APOE_syn)
 sce$APOE_carrier_syn <- factor(gsub("\\+", "", sce$APOE_carrier))
 levels(sce$APOE_carrier_syn)
 
+## add E4/E4 variable
+sce_pb$APOE_E4E4 <- sce_pb$APOE == "E4/E4"
+table(sce_pb$APOE_E4E4)
+
 ## add continuous APOE variable
 # APOE_num <- data.frame(APOE_num = 0:3)
 # rownames(APOE_num) <- c("E2/E2", "E2/E3", "E3/E4", "E4/E4")
