e4185cc2ee5d2d70efe6440d9f08d251e10165f1
lahuuki <lahuuki@gmail.com>
2025-05-28 16:16:40 -0400

    Update models and coef for DGE, add sh script

--- code/08_pseudoBulkDGE_sn/05_pseudoBulkDGE_sn_broad.R
+++ code/08_pseudoBulkDGE_sn/05_pseudoBulkDGE_sn_broad.R
@@ -1,4 +1,4 @@
-## Louise Huuki-Myers, April 2025
+## Louise Huuki-Myers, May 2025
 ## Run  pseudoBulkDGE on cell types from sn data
 
 library("SingleCellExperiment")
@@ -10,7 +10,7 @@ library("here")
 library("sessioninfo")
 
 #### Set up dirs ####
-plot_dir <- here("plots", "08_pseudoBulkDGE_sn", "03_pseudoBulkDGE_sn")
+data_dir <- here("processed-data", "08_pseudoBulkDGE_sn", "05_pseudoBulkDGE_sn_broad")
 if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
 #### Load the data ####
@@ -21,58 +21,62 @@ table(sce_pb$cell_type_broad)
 
 #### Design model + run DGE ####
 
-models <- list(carrier = ~APOE_carrier + Anc_Afr + Age + Sex + Rin + ncells + pseudo_expr_chrM_ratio + exp_round)
+models <- list(carrier = ~APOE_carrier + Anc_Afr + Age + Sex + Rin + ncells + pseudo_expr_chrM_ratio + exp_round,
+               carrier_i = ~APOE_carrier*Anc_Afr + Age + Sex + Rin + ncells + pseudo_expr_chrM_ratio + exp_round,
+               APOE = ~APOE + Anc_Afr + Anc_Afr + Age + Sex + Rin + ncells + pseudo_expr_chrM_ratio + exp_round,
+               APOE_i = ~APOE*Anc_Afr + Age + Sex + Rin + ncells + pseudo_expr_chrM_ratio + exp_round)
 
-model.matrix(models$carrier , colData(sce_pb))
+map(models, ~colnames(model.matrix(.x , colData(sce_pb))))
 
-de_results <- pseudoBulkDGE(
-    sce_pb,
-    label = sce_pb$cell_type_broad,
-    design = models$carrier,
-    coef = "APOE_carrierE4+",
-    row.data = rowData(sce_pb),
-    method = "edgeR"
+
+dge_design <- list(
+    ## carrier
+    carrier = c(mod = models$carrier, coef = "APOE_carrierE4+"),
+    carrier_i = c(mod = models$carrier_i, coef = "APOE_carrierE4+:Anc_Afr"),
+    ## APOE
+    APOE = c(mod = models$APOE, coef = c("APOEE2/E3","APOEE3/E4","APOEE4/E4")),
+    APOE_i = c(mod = models$APOE_i, coef = c("APOEE2/E3:Anc_Afr","APOEE3/E4:Anc_Afr","APOEE4/E4:Anc_Afr")),
+    ## E4E4
+    E4E4 = c(mod = models$APOE, coef = "APOEE4/E4"),
+    E4E4_i = c(mod = models$APOE_i, coef = "APOEE4/E4:Anc_Afr")
 )
 
-map_int(de_results, ~sum(.x$FDR<0.05, na.rm = TRUE))
-map_int(de_results, ~sum(.x$FDR<0.2, na.rm = TRUE))
+de.results <- map2(dge_design, names(dge_design), function(design, des_name){
+    
+    message(Sys.time(), " - Run pseudoBulkDGE on model:", mod_name , " coef:", coef[[mod_name]])
+    
+    de.results <- pseudoBulkDGE(
+        sce_pb,
+        label = sce_pb$cell_type_broad,
+        condition = sce_pb$carrier, # controls filtering, use consistently
+        design = design$mod, # map model
+        coef = dge_design$coef, #map coef
+        row.data = rowData(sce_pb),
+        method = "edgeR"
+    )
 
-map(de_results, ~min(.x$FDR, na.rm = TRUE))
+    return(de.results)
+})
 
-map_int(de_results, ~sum(!is.na(.x$FDR)))
+# de.results$test <- pseudoBulkDGE(
+#     sce_pb,
+#     label = sce_pb$cell_type_broad,
+#     condition = sce_pb$carrier, # controls filtering, use consistently
+#     design = models$APOE,
+#     coef = c("APOEE2/E3","APOEE3/E4","APOEE4/E4"),
+#     row.data = rowData(sce_pb),
+#     method = "edgeR"
+# )
 
-de_results_tb <- map2_dfr(de_results, names(de_results), ~as.data.frame(.x) |> mutate(cell_type_anno = .y)) |>
-    mutate(DE_class = case_when(logFC > 1 & PValue < 0.05 ~ "E4+",
-                                logFC < -1 & PValue < 0.05 ~ "E2+",
-                                TRUE ~"Other")) |>
-    filter(!is.na(logFC))
+map(de.results, ~summarizeTestsPerLabel(decideTestsPerLabel(.x, threshold=0.1)))
 
-de_results_tb |> filter(FDR < 0.05) |> dplyr::count(cell_type_anno)
-de_results_tb |> filter(PValue < 0.05) |> dplyr::count(cell_type_anno)
+saveRDS(de.results, file = here(data_dir, "sn_pseudoBulkDGE_broad.rds"))
 
-de_results_tb |> dplyr::count(cell_type_anno, DE_class)
+# slurmjobs::job_single('03_covariate_analysis_broad', create_shell = TRUE, memory = '50G', command = "Rscript 03_covariate_analysis_broad.R")
 
-
-de_results_tb |> group_by(cell_type_anno) |> dplyr::arrange(FDR) |> dplyr::slice(1)
-
-de_results_tb |> filter(gene_name == "APOE") |> arrange(FDR)
-
-load(here("processed-data", "project_colors.Rdata"), verbose = TRUE)
-
-(pval_lim <- min(de_results_tb$PValue[de_results_tb$FDR < 0.05]))
-
-violin_plot_cell_type_anno <- de_results_tb |>
-    ggplot(aes(x = logFC, y = -log10(PValue), color = DE_class)) +
-    geom_point(size = .6) +
-    geom_text_repel(aes(label = ifelse(PValue < 0.05 | abs(logFC) > 1, gene_name, "")), size = 1.7) +
-    facet_wrap(~cell_type_anno) +
-    geom_vline(xintercept = c(-1,1), linetype = "dashed") +
-    geom_hline(yintercept = -log10(0.05), linetype = "dashed") +
-    geom_hline(yintercept = -log10(pval_lim), linetype = "dashed", color = "red") +
-    scale_color_manual(values = c(APOE_carrier_colors, Other = "grey")) +
-    theme_bw() +
-    labs(title = "Visium PseudoBulkDGE", 
-         subtitle = "~APOE_carrier + Anc_Afr + Age + Sex + exp_round")
-
-ggsave(violin_plot_cell_type_anno, filename = here(plot_dir, "sn_DGE_violin_plot.png"), width = 12, height = 10)
-
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
