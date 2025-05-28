1f7bff34173cf99a9bebc240d9341db46bf36dca
lahuuki <lahuuki@gmail.com>
2025-05-28 11:31:32 -0400

    plot sn variance partition data, update other sn covariate plots

--- code/08_pseudoBulkDGE_sn/03_covariate_analysis_broad.R
+++ code/08_pseudoBulkDGE_sn/03_covariate_analysis_broad.R
@@ -6,7 +6,7 @@ library("scran")
 library("tidyverse")
 library("ggrepel")
 library("ggpubr")
-library("variancePartition")
+# library("variancePartition")
 library("here")
 library("sessioninfo")
 
@@ -22,15 +22,17 @@ if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 # message(Sys.time(), " - Load HDF5 sce")
 # sce <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "sce_objects", "sce_ERC"))
 
-sce_pb <- readRDS(here("processed-data", "08_pseudoBulkDGE_sn", "01_pseudobulk_data","sce_pseudo_DGE_broad.RDS"))
+sce_pb <- readRDS(here("processed-data", "08_pseudoBulkDGE_sn", "01_pseudobulk_data_sn","sce_pseudo_DGE-cell_type_broad.RDS"))
 
 ## load colors
-load(here("processed-data", "04_snRNA-seq", "cell_type_colors.Rdata"), verbose = TRUE)
+cell_type_colors <- metadata(sce_pb)$cell_type_colors
 load(here("processed-data", "project_colors.Rdata"), verbose = TRUE)
 
 colnames(colData(sce_pb))
 
 table(sce_pb$cell_type_broad)
+# Astro Macro Micro   OPC Oligo  Vasc Excit Inhib 
+# 31    19    31    31    31    30    31    31 
 
 # Extract the relevant columns from the data
 pd <- as.data.frame(colData(sce_pb)) |>
@@ -49,7 +51,8 @@ pb_cell_type_bar_APOE_carrier <- pd |>
     geom_col() +
     geom_text(aes(label = n), position = position_stack(vjust = .5)) +
     coord_flip() +
-    scale_fill_manual(values = APOE_carrier_colors)
+    scale_fill_manual(values = APOE_carrier_colors) +
+    theme_bw()
 
 ggsave(pb_cell_type_bar_APOE_carrier, filename = here(plot_dir, 'pb_cell_type_broad_bar_APOE_carrier.png'))
 
@@ -59,7 +62,8 @@ pb_cell_type_bar_APOE <- pd |>
     geom_col() +
     geom_text(aes(label = n), position = position_stack(vjust = .5)) +
     coord_flip() +
-    scale_fill_manual(values = APOE_genotype_colors)
+    scale_fill_manual(values = APOE_genotype_colors) +
+    theme_bw()
 
 ggsave(pb_cell_type_bar_APOE, filename = here(plot_dir, 'pb_cell_type_broad_bar_APOE.png'))
 
@@ -112,54 +116,101 @@ map(var_t_test, ~.x|> filter(p.adj < 0.05))
 dim(sce_pb)
 table(sce_pb$APOE)
 # E2/E2 E2/E3 E3/E4 E4/E4 
-# 47    64    78    56 
+# 46    62    73    54 
 table(sce_pb$APOE_carrier)
 # E2+ E4+ 
-# 111 134
+# 108 127
 
-pd |> filter(is.na(Rin))
+any(is.na(pd$Rin))
 
-table(is.na(pd$Rin))
-# FALSE  TRUE 
-# 30     1 
+#### Variance Partition data ####
+varPart_summary <- map_dfr(list.files(here("processed-data", "08_pseudoBulkDGE_sn", "02_VariancePartition_sn"), full.names = TRUE),
+                           read.csv) 
 
-# Assess correlation between all pairs of variables
-colnames(pd)
+any(is.na(varPart_summary$cell_type))
 
-form <- ~ cell_type_broad + APOE + sample_id + Sex + Age + Anc_Afr + Rin + exp_round + seq_round + ncells + pseudo_sum_umi + pseudo_expr_chrM_ratio
-# form <- ~ APOE + sample_id + exp_round
-# form <- ~ APOE + Sex + Age + Anc_Afr + exp_round + seq_round + ncells
+head(varPart_summary)
 
-C <- canCorPairs(form, pd)
-# ~ APOE + sample_id + Sex + Age + Anc_Afr + exp_round + seq_round + ncells
-# APOE and sample_id
-# sample_id and Sex
-# sample_id and Age
-# sample_id and Anc_Afr
-# sample_id and Rin
-# sample_id and exp_round
-# sample_id and seq_round
-# sample_id and ncells
+varPart_summary |> count(cell_type)
+varPart_summary |> filter(metric == "Median", name != "Residuals") |> group_by(cell_type, form) |> slice_max(value)
+varPart_summary |> filter(metric == "Mean", name != "Residuals")  |> group_by(cell_type, form) |> slice_max(value)
 
-pdf(here(plot_dir, "sn_broad_variable_cor_matrix.pdf"))
-plotCorrMatrix(C)
+varPart_heatmap <- function(my_form = "carrier", my_metric = "Mean"){
+    
+    varPart_summary_matrix <- varPart_summary |> 
+        filter(metric == my_metric, form == my_form,  name != "Residuals") |>
+        select(cell_type, name, value) |>
+        pivot_wider(values_from = "value", names_from = "cell_type") |>
+        column_to_rownames("name") |>
+        as.matrix()
+    
+    ## order by rowSums    
+    vp_metric_sum <- rowSums(varPart_summary_matrix)
+    varPart_summary_matrix <- varPart_summary_matrix[order(vp_metric_sum, decreasing = TRUE), ]
+    
+    col_fun = colorRamp2(c(0, max(varPart_summary_matrix)), c("white", "red"))
+    
+    pdf(here(plot_dir, sprintf("sn_varPart_summary-%s-%s.pdf", my_form, my_metric)), height = 8, width = 11)
+    print(Heatmap(varPart_summary_matrix,
+                  name = my_metric,
+                  col = col_fun,
+                  cluster_rows = FALSE,
+                  cluster_columns = TRUE,
+                  column_title = sprintf("Visium VarPart - %s", my_form)
+    ))
+    dev.off()
+    
+    rownames(varPart_summary_matrix) <- paste0(my_form, "-", rownames(varPart_summary_matrix))
+    
+    return(varPart_summary_matrix)
+}
+
+## plot heatmaps
+(form_list <- unique(varPart_summary$form))
+names(form_list) <- form_list
+
+var_part_mean <- map(form_list, ~varPart_heatmap(my_form = .x, my_metric = "Mean"))
+
+## make combined heatmap
+var_part_mean_all <- do.call("rbind", var_part_mean)
+
+covar_index <- order(rowSums(var_part_mean_all))
+
+var_part_mean_all <- var_part_mean_all[covar_index,]
+
+model_row_ha_simple <- rowAnnotation(
+    mod = jaffelab::ss(rownames(var_part_mean_all), "-"),
+    covar = jaffelab::ss(rownames(var_part_mean_all), "-",2)
+)
+
+col_fun = colorRamp2(c(0, max(var_part_mean_all)), c("white", "red"))
+pdf(here(plot_dir, "sn_varPart_summary-ALL-Mean.pdf"), height = 12, width = 11)
+print(Heatmap(var_part_mean_all,
+              name = "Mean",
+              col = col_fun,
+              cluster_rows = FALSE,
+              cluster_columns = TRUE,
+              right_annotation = model_row_ha_simple,
+              column_title = sprintf("sn VarPart - %s", "ALL")
+))
 dev.off()
 
-## test variance partition 
-## TODO add Rin
 
-message(Sys.time(), " - fitExtractVarPartModel")
+## plot medians
+walk(form_list, ~varPart_heatmap(my_form = .x, my_metric = "Median"))
 
-# Some predictor variables are on very different scales: consider rescaling
-# form <- ~ (1 | cell_type_broad) + (1 | APOE_carrier) + (1 | APOE) + (1 | Sex) + Anc_Afr + Age + (1 | exp_round) + (1 | seq_round) + pseudo_sum_umi + pseudo_expr_chrM_ratio
+#### colinearity ####
 
-form <- ~ (1 | cell_type_broad) + (1 | APOE_carrier) + (1 | APOE) + (1 | Sex) + Anc_Afr + Age + (1 | exp_round) + (1 | seq_round)
-varPart <- fitExtractVarPartModel(logcounts(sce_pb), form, pd)
-vp <- sortCols(varPart)
+log_fn <- list.files(here("code", "08_pseudoBulkDGE_sn", "logs"), pattern = "02_VariancePartition_sn", full.names = TRUE)
+names(log_fn) <- gsub(".txt", "",gsub("02_VariancePartition_Visium_", "", basename(log_fn)))
+logs <- map(log_fn, readLines)
 
-## violin plot 
-sn_vp_violin <- plotVarPart(vp)
-ggsave(sn_vp_violin, filename = here(plot_dir, "sn_broad_vp_violin.png"))
+colinear_pairs <- map(logs, ~.x[(grep("High colinearity between variables:", .x)+1):(grep("null device ", .x)-1)])
+map_int(colinear_pairs, length)
+
+all(map_lgl(colinear_pairs[-1], ~setequal(.x, colinear_pairs[[1]])))
+
+colinear_pairs[[1]]
 
 # slurmjobs::job_single('03_covariate_analysis_broad', create_shell = TRUE, memory = '50G', command = "Rscript 03_covariate_analysis_broad.R")
 
