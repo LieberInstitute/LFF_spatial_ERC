b4aba8925fed7a1903f4dbdaef3dabdd4686daec
lahuuki <lahuuki@gmail.com>
2025-05-29 10:15:19 -0400

    Fix coef, start pairwise design

--- code/09_pseudoBulkDGE_Visium/04_pseudoBulk_DGE_Visium.R
+++ code/09_pseudoBulkDGE_Visium/04_pseudoBulk_DGE_Visium.R
@@ -18,7 +18,7 @@ spe_pb <- readRDS(here("processed-data", "09_pseudoBulkDGE_Visium", "01_pseudobu
 ## temp
 spe_pb$nspots <- spe_pb$ncells
 
-#### Design model + run DGE ####
+#### Design model ####
 
 models <- list(carrier = ~APOE_carrier + Anc_Afr + Age + Sex + Rin + nspots + pseudo_expr_chrM_ratio + Visium_slide,
                carrier_i = ~APOE_carrier*Anc_Afr + Age + Sex + Rin + nspots + pseudo_expr_chrM_ratio + Visium_slide,
@@ -33,21 +33,22 @@ dge_design <- list(
     carrier = list(mod = models$carrier, coef = "APOE_carrierE4+"),
     carrier_i = list(mod = models$carrier_i, coef = "APOE_carrierE4+:Anc_Afr"),
     ## APOE
-    # APOE = list(mod = models$APOE, coef = list("APOEE2/E3","APOEE3/E4","APOEE4/E4")),
-    # APOE_i = list(mod = models$APOE_i, coef = list("APOEE2/E3:Anc_Afr","APOEE3/E4:Anc_Afr","APOEE4/E4:Anc_Afr")),
+    APOE = list(mod = models$APOE, coef = c("APOEE2/E3","APOEE3/E4","APOEE4/E4")),
+    APOE_i = list(mod = models$APOE_i, coef = c("APOEE2/E3:Anc_Afr","APOEE3/E4:Anc_Afr","APOEE4/E4:Anc_Afr")),
     ## E4E4
     E4E4 = list(mod = models$APOE, coef = "APOEE4/E4"),
     E4E4_i = list(mod = models$APOE_i, coef = "APOEE4/E4:Anc_Afr")
 )
 
+#### Run DGE ####
 de.results <- map2(dge_design, names(dge_design), function(design, des_name){
     
-    message(Sys.time(), " - Run pseudoBulkDGE on model:", des_name , " coef:", design$coef)
+    message(Sys.time(), " - Run pseudoBulkDGE on model: ", des_name , " coef: ", design$coef)
     
     de.results <- pseudoBulkDGE(
         spe_pb,
         label = spe_pb$SpD,
-        condition = spe_pb$carrier, # controls filtering, use consistently
+        condition = spe_pb$APOE, # controls filtering, use consistently
         design = design$mod, # map model
         coef = design$coef, #map coef
         row.data = rowData(spe_pb),
@@ -59,8 +60,8 @@ de.results <- map2(dge_design, names(dge_design), function(design, des_name){
 
 # de.results$test <- pseudoBulkDGE(
 #     spe_pb,
-#     label = spe_pb$cell_type_broad,
-#     condition = spe_pb$carrier, # controls filtering, use consistently
+#     label = spe_pb$SpD,
+#     condition = spe_pb$APOE, # controls filtering, use consistently
 #     design = models$APOE,
 #     coef = c("APOEE2/E3","APOEE3/E4","APOEE4/E4"),
 #     row.data = rowData(spe_pb),
@@ -69,6 +70,22 @@ de.results <- map2(dge_design, names(dge_design), function(design, des_name){
 
 map(de.results, ~summarizeTestsPerLabel(decideTestsPerLabel(.x, threshold=0.1)))
 
+#### pariwise ####
+
+apoe <- unique(spe_pb$APOE)
+apoe_combn <- combn(apoe, 2)
+
+map2(apoe_combn[1,], apoe_combn[2,], function(apoe1, apoe2){
+    
+    message(Sys.time(), sprintf("pairwise DE: %s vs. %s", apoe1, apoe2))
+    spe_pb <- spe_pb[, spe_pb$APOE %in% c(apoe1, apoe2)]
+    
+    message(ncol(spe_pb))
+    return(ncol(spe_pb))
+    
+})
+
+### save ####
 saveRDS(de.results, file = here(data_dir, "Visium_pseudoBulkDGE.rds"))
 
 # slurmjobs::job_single('04_pseudoBulkDGE_Visium', create_shell = TRUE, memory = '50G', command = "Rscript 04_pseudoBulkDGE_Visium.R")
