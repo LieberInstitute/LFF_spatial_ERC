d5525cec27fa04043c546094f80b117e55abfe73
lahuuki <lahuuki@gmail.com>
2025-06-03 19:42:42 +0100

    loop dreamlet over 6 models

--- code/10_dreamlet_sn/03_run_dreamlet_sn.R
+++ code/10_dreamlet_sn/03_run_dreamlet_sn.R
@@ -5,53 +5,79 @@ library("SpatialExperiment")
 library("dreamlet")
 library("here")
 library("sessioninfo")
+library("getopt")
+
+# Import command-line parameters
+scec <- matrix(
+    c("model", "m", "1", "character", "Model name"),
+    ncol = 5, byrow = TRUE
+)
+opt <- getopt(scec)
+print(opt)
+
+opt$model <- "carrier"
 
 #### Set up dirs ####
-data_dir <- here("processed-data", "10_dreamlet_sn", "02_run_dreamlet_sn")
+data_dir <- here("processed-data", "10_dreamlet_sn", "03_run_dreamlet_sn")
 if (!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
 
-plot_dir <- here("plots", "10_dreamlet_sn", "02_run_dreamlet_sn")
+plot_dir <- here("plots", "10_dreamlet_sn", "03_run_dreamlet_sn")
 if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
 #### Load data ####
-res.proc <- saveRDS(here("processed-data", "10_dreamlet_sn", "01_prep_dreamlet_sn", "sn_res_proc.rds"))
+res.proc <- readRDS(here("processed-data", "10_dreamlet_sn", "01_prep_dreamlet_sn", "sn_res_proc.rds"))
 
 #### Differential Expression ####
-message(Sys.time(), ' - Differential Expression')
+source(here("code", "10_dreamlet_sn", "dreamlet_models_sn.R"))
+stopifnot(opt$model %in% names(dreamlet_models_sn))
+
+message(Sys.time(), ' - Differential Expression, model = ', opt$model)
+mod = dreamlet_models_sn[[opt$model]]
+print(mod)
 
 # Differential expression analysis within each assay,
 # evaluated on the voom normalized data
-res.dl <- dreamlet(res.proc, ~ (1 | APOE_carrier) + Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round))
-
-saveRDS(res.dl, file = here(data_dir, "dreamlet_sn.RDS"))
+res.dl <- dreamlet(res.proc, mod)
 
 # names of estimated coefficients
+message("coef")
 coefNames(res.dl)
 
-# results from full analysis
-topTable(res.dl, coef = "APOE_carrierE4+")
+message(Sys.time(), " - Save Data")
+saveRDS(res.dl, file = here(data_dir, sprintf("dreamlet_sn-%s.RDS", opt$model)))
 
-pdf(here(plot_dir, "sn_dreamlet_VolcanoPlot.pdf"), height = 11, width = 8)
-plotVolcano(res.dl, coef = "APOE_carrierE4+")
-dev.off()
+# 
+# # results from full analysis
+# topTable(res.dl, coef = "APOE_carrierE4+")
+# 
+# pdf(here(plot_dir, "sn_dreamlet_VolcanoPlot.pdf"), height = 11, width = 8)
+# plotVolcano(res.dl, coef = "APOE_carrierE4+")
+# dev.off()
+# 
+# #### plot genes ####
+# # get data
+# df <- extractData(res.proc, "Inhib", genes = "NBPF12")
+# 
+# # expression boxplot
+# expression_plot <- ggplot(df, aes(APOE_carrier, NBPF12)) +
+#     geom_boxplot() +
+#     ylab(bquote(Expression ~ (log[2] ~ CPM))) +
+#     ggtitle("NBPF12") +
+#     theme_bw()
+# 
+# ggsave(expression_plot, filename = here(plot_dir, "sn_dreamlet_expression_boxplot.png"))
+# 
+# ## forest plot
+# plotForest(res.dl, coef = "APOE_carrierE4+", gene = "NBPF12")
 
-#### plot genes ####
-# get data
-df <- extractData(res.proc, "Inhib", genes = "NBPF12")
+# slurmjobs::job_single('03_run_dreamlet_sn', create_shell = TRUE, memory = '50G', command = "Rscript 03_run_dreamlet_sn.R")
 
-# expression boxplot
-expression_plot <- ggplot(df, aes(APOE_carrier, NBPF12)) +
-    geom_boxplot() +
-    ylab(bquote(Expression ~ (log[2] ~ CPM))) +
-    ggtitle("NBPF12") +
-    theme_bw()
-
-ggsave(expression_plot, filename = here(plot_dir, "sn_dreamlet_expression_boxplot.png"))
-
-## forest plot
-plotForest(res.dl, coef = "APOE_carrierE4+", gene = "NBPF12")
-
-# slurmjobs::job_single('02_run_dreamlet_sn', create_shell = TRUE, memory = '50G', command = "Rscript 02_run_dreamlet_sn.R")
+# slurmjobs::job_loop(
+#     loops = list(model = names(dreamlet_models_sn)),
+#     name = "03_run_dreamlet_sn",
+#     create_shell = TRUE,
+#     create_script = FALSE
+# )
 
 ## Reproducibility information
 print("Reproducibility information:")
---
+++ code/10_dreamlet_sn/03_run_dreamlet_sn.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH --mem=10G
+#SBATCH --job-name=03_run_dreamlet_sn
+#SBATCH -c 1
+#SBATCH -t 1-00:00:00
+#SBATCH -o /dev/null
+#SBATCH -e /dev/null
+#SBATCH --mail-type=ALL
+#SBATCH --array=1-6%20
+
+## Define loops and appropriately subset each variable for the array task ID
+all_model=(carrier apoe e4e4 carrier_i apoe_i e4e4_i)
+model=${all_model[$(( $SLURM_ARRAY_TASK_ID / 1 % 6 ))]}
+
+## Explicitly pipe script output to a log
+log_path=logs/03_run_dreamlet_sn_${model}_${SLURM_ARRAY_TASK_ID}.txt
+
+{
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load the R module
+module load conda_R/4.5
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 03_run_dreamlet_sn.R --model ${model}
+
+echo "**** Job ends ****"
+date
+
+} > $log_path 2>&1
+
+## This script was made using slurmjobs version 1.3.0
+## available from http://research.libd.org/slurmjobs/
+
