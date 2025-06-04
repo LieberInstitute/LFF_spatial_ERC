7d4504dbbc3d4261f151bca11c2aa8f44d54f8af
lahuuki <lahuuki@gmail.com>
2025-06-04 16:04:39 +0100

    pseudobulk subcluster by cell_type_broad

--- code/04_snRNA-seq/20_sn_pseudobulk.sh
+++ code/04_snRNA-seq/20_sn_pseudobulk.sh
@@ -27,7 +27,7 @@ module load conda_R/4.4.x
 module list
 
 ## Edit with your job command
-Rscript 20_sn_pseudobulk.R --cluster 'cell_type_anno' --sce "sce_ERC_subcluster"
+Rscript 20_sn_pseudobulk.R --cluster 'cell_type_broad' --sce "sce_ERC_subcluster"
 
 echo "**** Job ends ****"
 date
