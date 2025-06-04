d4edeabcd16a3f43108ad4bbf8eb38a5cc6c2c9a
lahuuki <lahuuki@gmail.com>
2025-06-04 13:10:03 +0100

    Add models to script

--- code/10_dreamlet_sn/03_run_dreamlet_sn.R
+++ code/10_dreamlet_sn/03_run_dreamlet_sn.R
@@ -29,7 +29,20 @@ if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 res.proc <- readRDS(here("processed-data", "10_dreamlet_sn", "01_prep_dreamlet_sn", "sn_res_proc.rds"))
 
 #### Differential Expression ####
-source(here("code", "10_dreamlet_sn", "dreamlet_models_sn.R"))
+
+# The variable to be tested must be a fixed effect
+dreamlet_models_sn <- list(
+    carrier = ~ APOE_carrier + Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round),
+    apoe = ~ APOE + Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round),
+    e4e4 = ~ APOE_E4E4 + Anc_Afr  + (1 | Sex) + Age + Rin + (1 | exp_round) ,
+    contrast = ~0 + APOE_syn  + Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round),
+    # interaction syntax x + (x | g)
+    carrier_i = ~ APOE_carrier*Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round),
+    apoe_i = ~ APOE*Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round),
+    e4e4_i = ~ APOE_E4E4*Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round)
+)
+
+# source(here("code", "10_dreamlet_sn", "dreamlet_models_sn.R"))
 stopifnot(opt$model %in% names(dreamlet_models_sn))
 
 message(Sys.time(), ' - Differential Expression, model = ', opt$model)
@@ -40,45 +53,16 @@ print(mod)
 # evaluated on the voom normalized data
 res.dl <- dreamlet(res.proc, mod)
 
+message(Sys.time(), ' - DONE Differential Expression')
+
 # names of estimated coefficients
 message("coef")
 coefNames(res.dl)
 
+topTable(res.dl, coef = "APOE_carrierE4+")
+
 message(Sys.time(), " - Save Data")
 saveRDS(res.dl, file = here(data_dir, sprintf("dreamlet_sn-%s.RDS", opt$model)))
-
-# 
-# # results from full analysis
-# topTable(res.dl, coef = "APOE_carrierE4+")
-# 
-# pdf(here(plot_dir, "sn_dreamlet_VolcanoPlot.pdf"), height = 11, width = 8)
-# plotVolcano(res.dl, coef = "APOE_carrierE4+")
-# dev.off()
-# 
-# #### plot genes ####
-# # get data
-# df <- extractData(res.proc, "Inhib", genes = "NBPF12")
-# 
-# # expression boxplot
-# expression_plot <- ggplot(df, aes(APOE_carrier, NBPF12)) +
-#     geom_boxplot() +
-#     ylab(bquote(Expression ~ (log[2] ~ CPM))) +
-#     ggtitle("NBPF12") +
-#     theme_bw()
-# 
-# ggsave(expression_plot, filename = here(plot_dir, "sn_dreamlet_expression_boxplot.png"))
-# 
-# ## forest plot
-# plotForest(res.dl, coef = "APOE_carrierE4+", gene = "NBPF12")
-
-# slurmjobs::job_single('03_run_dreamlet_sn', create_shell = TRUE, memory = '50G', command = "Rscript 03_run_dreamlet_sn.R")
-
-# slurmjobs::job_loop(
-#     loops = list(model = names(dreamlet_models_sn)),
-#     name = "03_run_dreamlet_sn",
-#     create_shell = TRUE,
-#     create_script = FALSE
-# )
 
 ## Reproducibility information
 print("Reproducibility information:")
