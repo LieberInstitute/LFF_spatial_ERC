f58513d80cd079dc5770ffab76b3b51335665131
lahuuki <lahuuki@gmail.com>
2025-06-05 11:35:00 +0100

    tidy up output

--- code/10_dreamlet_sn/04_plot_dreamlet_sn.R
+++ code/10_dreamlet_sn/04_plot_dreamlet_sn.R
@@ -1,8 +1,9 @@
 ## Louise Huuki-Myers, June 2025
-## Run  dreamlet on sn data
+## Compile and plot dreamlet sn data
 
 library("SpatialExperiment")
 library("dreamlet")
+library("tidyverse")
 library("here")
 library("sessioninfo")
 library("getopt")
@@ -27,8 +28,8 @@ if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
 #### Load data ####
 res.proc <- readRDS(here("processed-data", "10_dreamlet_sn", "01_prep_dreamlet_sn", "sn_res_proc.rds"))
-
-res.dl <- readRDS(here("processed-data", "10_dreamlet_sn", "03_run_dreamlet_sn", sprintf("dreamlet_sn-%s.RDS", opt$model)))
+# 
+# res.dl <- readRDS(here("processed-data", "10_dreamlet_sn", "03_run_dreamlet_sn", sprintf("dreamlet_sn-%s.RDS", opt$model)))
 
 res.dl.fn <- list.files(here("processed-data", "10_dreamlet_sn", "03_run_dreamlet_sn"), full.names = TRUE)
 names(res.dl.fn) <- gsub(".RDS", "", gsub("dreamlet_sn-", "", basename(res.dl.fn)))
@@ -36,23 +37,51 @@ names(res.dl.fn) <- gsub(".RDS", "", gsub("dreamlet_sn-", "", basename(res.dl.fn
 res.dl.list <- purrr::map(res.dl.fn, readRDS)
 purrr::map(res.dl.list, coefNames)
 
+coef_list <- list(apoe_i = c("APOEE2/E3:Anc_Afr", "APOEE3/E4:Anc_Afr", "APOEE4/E4:Anc_Afr"),
+                  apoe = c("APOEE2/E3", "APOEE3/E4","APOEE4/E4"),
+                  carrier_i = "APOE_carrierE4+:Anc_Afr",
+                  carrier = "APOE_carrierE4+",
+                  e4e4_i = "APOE_E4E4TRUE:Anc_Afr",
+                  e4e4 = "APOE_E4E4TRUE")
 
 details(res.dl)
 coefNames(res.dl)
 
 # results from full analysis
-topTable(res.dl, coef = "Anc_Afr")
+tt <- map2(res.dl.list, coef_list, ~topTable(.x, .y, number = Inf, p.value = 0.1))
+
+map(tt, ~.x |> as.data.frame() |> filter(adj.P.Val < 0.1))
+
+map(tt, ~.x |> as.data.frame() |> group_by(assay) |> filter(adj.P.Val < 0.2) |> count())
+
+## check VarPart genes
+tt[["carrier"]] |>
+    as.data.frame() |>
+    filter(ID %in% c("CLOCK", "ABCC4"))
+
+tt[["apoe"]] |>
+    as.data.frame() |>
+    filter(ID %in% c("ADAM10", "ADAM17"))
+
+tt[["e4e4"]] |>
+    as.data.frame() |>
+    filter(ID %in% c("SRGAP2C", "NAALADL2", "APP"))
+
+
 
 pdf(here(plot_dir, "sn_dreamlet_VolcanoPlot.pdf"), height = 11, width = 8)
-plotVolcano(res.dl, coef = "APOE_carrierE4+")
+# plotVolcano(res.dl, coef = "APOE_carrierE4+")
+
+map(c("carrier","e4e4","carrier_i","e4e4_i"), ~plotVolcano(res.dl.list[[.x]], coef_list[[.x]]) + labs(title = .x))
+
 dev.off()
 
 #### plot genes ####
 # get data
-df <- extractData(res.proc, "Inhib", genes = "NBPF12")
+df <- extractData(res.proc, "Vasc", genes = "TAFA1")
 
 # expression boxplot
-expression_plot <- ggplot(df, aes(APOE_carrier, NBPF12)) +
+expression_plot <- ggplot(df, aes(APOE_carrier, TAFA1)) +
     geom_boxplot() +
     ylab(bquote(Expression ~ (log[2] ~ CPM))) +
     ggtitle("NBPF12") +
