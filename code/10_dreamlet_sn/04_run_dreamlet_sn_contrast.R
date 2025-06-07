999d24495d3637fcb95bb80ad0177834c264b044
lahuuki <lahuuki@gmail.com>
2025-06-07 10:59:07 +0100

    refine contrast script, add het contrast

--- code/10_dreamlet_sn/04_run_dreamlet_sn_contrast.R
+++ code/10_dreamlet_sn/04_run_dreamlet_sn_contrast.R
@@ -16,7 +16,7 @@ scec <- matrix(
 opt <- getopt(scec)
 
 # test 
-# opt$model <- "carrier"
+# opt$model <- "contrast"
 # opt$ddf <- "Kenward-Roger"
 
 ## use default "Satterthwaite" for df approximation
@@ -27,14 +27,14 @@ print(opt)
 ddf_suffix <- ifelse(opt$ddf == "Kenward-Roger", "_kr", "")
 
 #### Set up dirs ####
-data_dir <- here("processed-data", "10_dreamlet_sn", "03_run_dreamlet_sn")
+data_dir <- here("processed-data", "10_dreamlet_sn", "04_run_dreamlet_contrast_sn")
 if (!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
 
-plot_dir <- here("plots", "10_dreamlet_sn", "03_run_dreamlet_sn")
+plot_dir <- here("plots", "10_dreamlet_sn", "04_run_dreamlet_contrast_sn")
 if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
 #### Load data ####
-res.proc <- readRDS(here("processed-data", "10_dreamlet_sn", "01_prep_dreamlet_sn", "sn_res_proc.rds"))
+pb <- readRDS(here("processed-data", "10_dreamlet_sn", "01_prep_dreamlet_sn", "sn_dreamlet_pb.rds"))
 
 #### Differential Expression ####
 
@@ -42,65 +42,55 @@ res.proc <- readRDS(here("processed-data", "10_dreamlet_sn", "01_prep_dreamlet_s
 dreamlet_contrast_models_sn <- list(
     contrast = ~0 + APOE_syn  + Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round) + subsets_Mito_percent
 )
+
 contrasts <-  c(
-    `E2E2_E4E4` = "APOE_synE2.E2 - APOE_synE4.E4",
-    `E3E4_E4E4` = "APOE_synE3.E4 - APOE_synE4.E4",
-    `E2E3_E4E4` = "APOE_synE2.E3 - APOE_synE4.E4",
-    `E2E2_E3E4` = "APOE_synE2.E2 - APOE_synE3.E4",
-    `E2E2_E2E3` = "APOE_synE2.E2 - APOE_synE2.E3",
-    `E2E3_E3E4` = "APOE_synE2.E3 - APOE_synE3.E4")
+    E2E2_E4E4 = "APOE_synE2.E2 - APOE_synE4.E4",
+    E3E4_E4E4 = "APOE_synE3.E4 - APOE_synE4.E4",
+    E2E3_E4E4 = "APOE_synE2.E3 - APOE_synE4.E4",
+    E2E2_E3E4 = "APOE_synE2.E2 - APOE_synE3.E4",
+    E2E2_E2E3 = "APOE_synE2.E2 - APOE_synE2.E3",
+    E2E3_E3E4 = "APOE_synE2.E3 - APOE_synE3.E4",
+    E4E4_anyE2 = "APOE_synE4.E4 - (0.5*APOE_synE2.E3 + 0.5*APOE_synE2.E2)",
+    anyE4_anyE2 = "APOE_synE3.E4 - (0.5*APOE_synE2.E3 + 0.5*APOE_synE2.E2)",
+    E2E2_anyE4 = "APOE_synE2.E2 - (0.5*APOE_synE4.E4 + 0.5*APOE_synE3.E4)",
+    E2E3_anyE4 = "APOE_synE2.E3 - (0.5*APOE_synE4.E4 + 0.5*APOE_synE3.E4)")
 
 
+stopifnot(opt$model %in% names(dreamlet_contrast_models_sn))
 
-metadata(res.proc)
+message('model = ', opt$model, ", ddf = ", opt$ddf)
+mod = dreamlet_contrast_models_sn[[opt$model]]
+print(mod)
 
-form <- ~ APOE + Anc_Afr + (1 | Sex) + Age + Rin + (1 | Visium)
+message(Sys.time(), " - Apply Voom")
+# Normalize and apply voom/voomWithDreamWeights
+res.proc <- processAssays(pb, mod, min.count = 5, min.cells = 10)
+
+# show voom plot for each cell clusters
+pdf(here(plot_dir, sprintf("sn_dreamlet_voom-%s.pdf", opt$model)))
+plotVoom(res.proc)
+dev.off()
 
 model.matrix(~0+APOE_syn, colData(res.proc))
 
-# L <- makeContrastsDream(~0+APOE, colData(res.proc),
-#                         contrasts = c(
-#                             `E2/E2-E4/E4` = "APOEE2/E2 - APOEE4/E4",
-#                             `E3/E4-E4/E4` = "APOEE3/E4 - APOEE4/E4",
-#                             `E2/E3-E4/E4` = "APOEE2/E3 - APOEE4/E4",
-#                             `E2/E2-E3/E4` = "APOEE2/E2 - APOEE3/E4",
-#                             `E2/E2-E2/E3` = "APOEE2/E2 - APOEE2/E3",
-#                             `E2/E3-E3/E4` = "APOEE2/E3 - APOEE3/E4")
-# )
+#### Visualize contrast matrix ####
+L <- makeContrastsDream(mod, contrasts)
+contrast_plot <- plotContrasts(L) + labs(title = "sn dreamlet contrast",
+                                         subtitle = mod)
 
-L <- makeContrastsDream(~0 + APOE_syn  + Anc_Afr + (1 | Sex) + Age + Rin + (1 | Visium_slide), colData(res.proc),
-                        contrasts = c(
-                            `E2E2_E4E4` = "APOE_synE2.E2 - APOE_synE4.E4",
-                            `E3E4_E4E4` = "APOE_synE3.E4 - APOE_synE4.E4",
-                            `E2E3_E4E4` = "APOE_synE2.E3 - APOE_synE4.E4",
-                            `E2E2_E3E4` = "APOE_synE2.E2 - APOE_synE3.E4",
-                            `E2E2_E2E3` = "APOE_synE2.E2 - APOE_synE2.E3",
-                            `E2E3_E3E4` = "APOE_synE2.E3 - APOE_synE3.E4")
-)
+ggsave(contrast_plot, filename = here(plot_dir, "sn_dreamlet_contrast.png"))
 
-# Visualize contrast matrix
-contrast_plot <- plotContrasts(L) + labs(title = "DREAM contrast",
-                                         subtitle = "~0 + APOE_syn  + Anc_Afr + (1 | Sex) + Age + Rin + (1 | Visium_slide)")
-
-ggsave(contrast_plot, filename = here(plot_dir, "Visium_dream_contrast.png"))
-
-
-
-# source(here("code", "10_dreamlet_sn", "dreamlet_contrast_models_sn.R"))
-stopifnot(opt$model %in% names(dreamlet_contrast_models_sn))
-
-message(Sys.time(), ' - Differential Expression, model = ', opt$model, ", ddf = ", opt$ddf)
-mod = dreamlet_contrast_models_sn[[opt$model]]
-print(mod)
+message(Sys.time(), " - dreamlet")
 
 # Differential expression analysis within each assay,
 # evaluated on the voom normalized data
 
-param <- SnowParam(4, "SOCK", progressbar = TRUE)
+# param <- SnowParam(4, "SOCK", progressbar = TRUE)
+
 res.dl <- dreamlet(res.proc, mod, ddf = opt$ddf, contrast)
 
 message(Sys.time(), " - DONE Differential Expression...Save Data")
-saveRDS(res.dl, file = here(data_dir, sprintf("dreamlet_sn-%s%s.RDS", opt$model, ddf_suffix)))
+saveRDS(res.dl, file = here(data_dir, sprintf("dreamlet_contrast_sn-%s%s.RDS", opt$model, ddf_suffix)))
 
 # names of estimated coefficients
 message("coef")
@@ -108,7 +98,7 @@ coefNames(res.dl)
 
 # topTable(res.dl, coef = "APOE_carrierE4+")
 
-# slurmjobs::job_single('03_run_dreamlet_sn_kr', create_shell = TRUE, memory = '10G', command = "Rscript 03_run_dreamlet_sn.R --model carrier --ddf 'Kenward-Roger'")
+# slurmjobs::job_single('04_run_dreamlet_contrast_sn', create_shell = TRUE, memory = '10G', command = "Rscript 03_run_dreamlet_sn.R --model carrier --ddf 'Kenward-Roger'")
 
 # slurmjobs::job_loop(
 #     loops = list(model = c("carrier_n0","carrier_n1","carrier_n2","carrier_n3","carrier_n4","carrier_sf","apoe_n0","e4e4_n0", "contrast")),
