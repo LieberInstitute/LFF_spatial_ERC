49304f2da6fb91ca4972da36e24121eaf2cddddc
lahuuki <lahuuki@gmail.com>
2025-06-12 15:08:05 +0100

    design contrast for apoe*anc interaction

--- code/10_dreamlet_sn/04_run_dreamlet_contrast_sn.R
+++ code/10_dreamlet_sn/04_run_dreamlet_contrast_sn.R
@@ -16,7 +16,7 @@ scec <- matrix(
 opt <- getopt(scec)
 
 # test 
-# opt$model <- "contrast"
+# opt$model <- "contrast_i"
 # opt$ddf <- "Kenward-Roger"
 
 ## use default "Satterthwaite" for df approximation
@@ -39,53 +39,81 @@ pb <- readRDS(here("processed-data", "10_dreamlet_sn", "01_prep_dreamlet_sn", "s
 #### Differential Expression ####
 
 # The variable to be tested must be a fixed effect
-dreamlet_contrast_models_sn <- list(
-    contrast = ~0 + APOE_syn  + Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round) + subsets_Mito_percent
+dreamlet_contrast_models <- list(
+    contrast = ~0 + APOE_syn  + Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round) + subsets_Mito_percent,
+    contrast_i = ~0 + APOE_syn*Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round) + subsets_Mito_percent
 )
 
-stopifnot(opt$model %in% names(dreamlet_contrast_models_sn))
-
-contrasts <-  c(
-    E2E2_E4E4 = "APOE_synE2.E2 - APOE_synE4.E4",
-    E3E4_E4E4 = "APOE_synE3.E4 - APOE_synE4.E4",
-    E2E3_E4E4 = "APOE_synE2.E3 - APOE_synE4.E4",
-    E2E2_E3E4 = "APOE_synE2.E2 - APOE_synE3.E4",
-    E2E2_E2E3 = "APOE_synE2.E2 - APOE_synE2.E3",
-    E2E3_E3E4 = "APOE_synE2.E3 - APOE_synE3.E4",
-    E4E4_anyE2 = "APOE_synE4.E4 - (0.5*APOE_synE2.E3 + 0.5*APOE_synE2.E2)",
-    anyE4_anyE2 = "APOE_synE3.E4 - (0.5*APOE_synE2.E3 + 0.5*APOE_synE2.E2)",
-    E2E2_anyE4 = "APOE_synE2.E2 - (0.5*APOE_synE4.E4 + 0.5*APOE_synE3.E4)",
-    E2E3_anyE4 = "APOE_synE2.E3 - (0.5*APOE_synE4.E4 + 0.5*APOE_synE3.E4)")
+stopifnot(opt$model %in% names(dreamlet_contrast_models))
 
 message('model = ', opt$model, ", ddf = ", opt$ddf)
-mod = dreamlet_contrast_models_sn[[opt$model]]
+mod = dreamlet_contrast_models[[opt$model]]
 print(mod)
 
 message(Sys.time(), " - Apply Voom")
 # Normalize and apply voom/voomWithDreamWeights
 res.proc <- processAssays(pb, mod, min.count = 5, min.cells = 10)
 
-saveRDS(res.proc, file = here(data_dir, "rse.proc.contrast.Rds"))
+saveRDS(res.proc, file = here(data_dir, sprintf("rse.proc.%s.Rds", opt$model)))
 
 # show voom plot for each cell clusters
 pdf(here(plot_dir, sprintf("sn_dreamlet_voom-%s.pdf", opt$model)))
 plotVoom(res.proc)
 dev.off()
 
+#### Design Contrasts ####
+
 # model.matrix(~0+APOE_syn, colData(res.proc))
+# model.matrix(~0+APOE_syn*Anc_Afr, colData(res.proc))
+# model.matrix(~0+(APOE_syn*Anc_Afr), colData(res.proc))
+
+my_contrasts <- list(
+    contrast =  c(
+        E2E2_E4E4 = "APOE_synE2.E2 - APOE_synE4.E4",
+        E3E4_E4E4 = "APOE_synE3.E4 - APOE_synE4.E4",
+        E2E3_E4E4 = "APOE_synE2.E3 - APOE_synE4.E4",
+        E2E2_E3E4 = "APOE_synE2.E2 - APOE_synE3.E4",
+        E2E2_E2E3 = "APOE_synE2.E2 - APOE_synE2.E3",
+        E2E3_E3E4 = "APOE_synE2.E3 - APOE_synE3.E4",
+        E4E4_anyE2 = "APOE_synE4.E4 - (0.5*APOE_synE2.E3 + 0.5*APOE_synE2.E2)",
+        anyE4_anyE2 = "APOE_synE3.E4 - (0.5*APOE_synE2.E3 + 0.5*APOE_synE2.E2)",
+        E2E2_anyE4 = "APOE_synE2.E2 - (0.5*APOE_synE4.E4 + 0.5*APOE_synE3.E4)",
+        E2E3_anyE4 = "APOE_synE2.E3 - (0.5*APOE_synE4.E4 + 0.5*APOE_synE3.E4)"),
+    contrast_i =  c(
+        # E2E2_E4E4 = "APOE_synE2.E2:Anc_Afr - APOE_synE4.E4:Anc_Afr",
+        E3E4_E4E4 = "APOE_synE3.E4:Anc_Afr - APOE_synE4.E4:Anc_Afr",
+        E2E3_E4E4 = "APOE_synE2.E3:Anc_Afr - APOE_synE4.E4:Anc_Afr",
+        # E2E2_E3E4 = "APOE_synE2.E2:Anc_Afr - APOE_synE3.E4:Anc_Afr",
+        # E2E2_E2E3 = "APOE_synE2.E2:Anc_Afr - APOE_synE2.E3:Anc_Afr",
+        E2E3_E3E4 = "APOE_synE2.E3:Anc_Afr - APOE_synE3.E4:Anc_Afr",
+        # E4E4_anyE2 = "APOE_synE4.E4:Anc_Afr - (0.5*APOE_synE2.E3:Anc_Afr + 0.5*APOE_synE2.E2:Anc_Afr)",
+        # anyE4_anyE2 = "APOE_synE3.E4:Anc_Afr - (0.5*APOE_synE2.E3:Anc_Afr + 0.5*APOE_synE2.E2:Anc_Afr)",
+        # E2E2_anyE4 = "APOE_synE2.E:Anc_Afr2 - (0.5*APOE_synE4.E4:Anc_Afr + 0.5*APOE_synE3.E4:Anc_Afr)",
+        E2E3_anyE4 = "APOE_synE2.E3:Anc_Afr - (0.5*APOE_synE4.E4:Anc_Afr + 0.5*APOE_synE3.E4:Anc_Afr)")
+)
+
+my_contrasts <- my_contrasts[[opt$model]]
+
 
 #### Visualize contrast matrix ####
-## cant add subsets_Mito_percent as a variable 
+## cant add subsets_Mito_percent as a variable  - exclude metadata vars here
 
-L <- makeContrastsDream(~0 + APOE_syn  + Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round), 
-                        colData(res.proc),
-                        contrasts = contrasts
+dreamlet_contrast_models_noMeta <- list(
+    contrast = ~0 + APOE_syn  + Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round),
+    contrast_i = ~0 + APOE_syn*Anc_Afr + (1 | Sex) + Age + Rin + (1 | exp_round)
 )
+
+L <- makeContrastsDream(dreamlet_contrast_models_noMeta[[opt$model]], 
+                        colData(res.proc),
+                        # contrasts = my_contrasts
+                        contrasts = list(test = "0.5*(APOE_synE2.E2 + Anc_Afr) - APOE_synE4.E4:Anc_Afr")
+)
+
 
 contrast_plot <- plotContrasts(L) + labs(title = "sn dreamlet contrast",
                                          subtitle = mod)
 
-ggsave(contrast_plot, filename = here(plot_dir, "sn_dreamlet_contrast.png"))
+ggsave(contrast_plot, filename = here(plot_dir, spritf("sn_dreamlet_%s.png", opt$model)))
 
 message(Sys.time(), " - dreamlet")
 
