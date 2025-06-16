60b6c449bad64c026e9c4cf663753ca20f308fb2
lahuuki <lahuuki@gmail.com>
2025-06-16 13:53:57 +0100

    Add clusterwise voomLmFit script

---
+++ code/12_voomLmFit/01_Clusterwise_voomLmFit.R
@@ -0,0 +1,118 @@
+## Louise Huuki-Myers & Bernie Mulvey, June 2025
+## Compile and plot dreamlet sn data
+## Adapted from https://github.com/LieberInstitute/LFF_spatial_LC/blob/0347daa995d7b4d035b3d2ad2efdd4f381a1387f/code/12_DEanalyses_removedsampsAndFinalNMseg/02c-Clusterwise_DEanalysis.Rmd
+
+library("data.table")
+library("edgeR")
+library("limma")
+library("SingleCellExperiment")
+library("here")
+library("data.table")
+library("getopt")
+library("tidyverse")
+
+# Import command-line parameters
+scec <- matrix(
+    c("datatype", "d", "1", "character", "Data type")
+    ncol = 5, byrow = TRUE
+)
+opt <- getopt(scec)
+
+## test
+# opt$datatype <- "sn_broad"
+
+print(opt)
+
+if(opt$datatype == "Visium"){
+    pb_fn <- here("processed-data", "09_pseudoBulkDGE_Visium", "01_pseudobulk_data_Visium", "spe_pseudo_DGE.RDS")
+    batch <- "Visium_slide"
+} else if(opt$datatype == "sn_broad"){
+    pb_fn <- here("processed-data", "08_pseudoBulkDGE_sn", "01_pseudobulk_data_sn","sce_pseudo_DGE-cell_type_broad.RDS")
+    batch <- "exp_round"
+}else if(opt$datatype == "sn_fine"){
+    pb_fn <- here("processed-data", "08_pseudoBulkDGE_sn", "01_pseudobulk_data_sn","sce_pseudo_DGE-cell_type_anno.RDS")
+    batch <- "exp_round"
+} else {
+    stop("non-valid datatype")
+}
+
+message(Sys.time(), sprintf(" - Datatype = %s, loading '%s'", opt$datatype, basename(pb_fn)))
+
+#### Set up dirs ####
+data_dir <- here("processed-data", "12_voomLmFit", "01_Clusterwise_voomLmFit", sprintf("vlmf_%s", opt$datatype))
+if (!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
+
+#### Load the data ####
+sce_pb <- readRDS(pb_fn)
+dim(sce_pb)
+table(sce_pb$registration_variable)
+
+clusters <- levels(sce_pb$registration_variable)
+names(clusters) <- clusters
+
+message(Sys.time(), " - Loop voomlmFit by cluster")
+
+lmf_summary <- map_dfr(clusters, function(clus){
+    
+    dge <- sce_pb[,sce_pb$registration_variable ==clus]
+
+    des <- model.matrix(~0+APOE_syn+Sex+Age+Anc_Afr, data = colData(dge))
+    des <- as.data.frame(des)
+    
+    # filter low expression genes
+    dge <- edgeR::calcNormFactors(dge)
+    keep <- edgeR::filterByExpr.DGEList(dge,design=des)
+    dge <- dge[keep,,keep.lib.sizes=FALSE]
+    dge <- edgeR::calcNormFactors(dge)
+    
+    message(Sys.time(), sprintf(" - voomLmFit - cluster: %s, block= '%s', ncol: %s, ngene: %i", clus, batch, ncol(dge), nrow(dge$genes)))
+    
+    # make these more readable
+    colnames(des) <- gsub(colnames(des),pattern="_syn",replacement="_")
+    
+    ## run voomLmFit for the pseudobulked data, referring donor to duplicateCorrelation; 
+    ## using an adaptive span (number of genes, based on the number of genes in the dge) for smoothing the mean-variance trend
+    v.swt <- voomLmFit(dge,design = des,block = as.factor(dge$samples[[batch]]),adaptive.span = T,sample.weights = T)
+    
+    cont <- makeContrasts(
+        E2E2_E4E4 = "-APOE_E2.E2 + APOE_E4.E4",
+        E3E4_E4E4 = "-APOE_E3.E4 + APOE_E4.E4",
+        E2E3_E4E4 = "-APOE_E2.E3 + APOE_E4.E4",
+        E2E2_E3E4 = "-APOE_E2.E2 + APOE_E3.E4",
+        E2E2_E2E3 = "-APOE_E2.E2 + APOE_E2.E3",
+        E2E3_E3E4 = "-APOE_E2.E3 + APOE_E3.E4",
+        anyE2_E4E4 = "- 0.5*(APOE_E2.E3 + APOE_E2.E2) + APOE_E4.E4",
+        anyE2_anyE4 = "-0.5*(APOE_E2.E2 + APOE_E2.E3) + 0.5*(APOE_E3.E4 + APOE_E4.E4)",
+        E2E2_anyE4 = "-APOE_E2.E2 + 0.5*(APOE_E3.E4 + APOE_E4.E4)",
+        E2E3_anyE4 = "-APOE_E2.E3 + 0.5*(APOE_E3.E4 + APOE_E4.E4)",
+        E4E4_other = "-APOE_E4.E4 + (APOE_E2.E2 + APOE_E2.E3 + APOE_E3.E4)/3",
+        Sex=SexM,
+        Anc=Anc_Afr,
+        levels=des
+    )
+    
+    v.swt.fit <- contrasts.fit(v.swt,contrasts=cont)
+    v.swt.fit.e <- eBayes(v.swt.fit)
+    
+    ## run top table over contrasts
+    v.swt.e.tt <- purrr::map(colnames(cont), ~topTable(v.swt.fit.e,coef = .x, number=Inf, adjust.method = "BH") |>
+                                 mutate(data_type = opt$datatype, 
+                                        cluster = clus,
+                                        contrast = .x) |>
+                                 arrange(adj.P.Val)) 
+    
+    names(v.swt.e.tt) <- colnames(cont)
+    
+    message("Done - Save data")
+    saveRDS(v.swt.e.tt, file = here(data_dir, sprintf("voomLmFit_%s_%s.rds", opt$datatype, clus)))
+    return(purrr::map_int(v.swt.e.tt, ~sum(.x$adj.P.Val < 0.2)))
+})
+
+
+
+#### Reproducibility information ####
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
