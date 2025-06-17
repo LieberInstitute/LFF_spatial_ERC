e6a9811d3e631434fe55c649e6a4dc7120b5e5a2
lahuuki <lahuuki@gmail.com>
2025-06-17 16:34:29 +0100

    Update dirs & plot APOE over cell types

--- code/04_snRNA-seq/33_sn_subcluster_plots.R
+++ code/04_snRNA-seq/33_sn_subcluster_plots.R
@@ -17,21 +17,25 @@ library("DeconvoBuddies")
 source(here("code", "utils", "my_plot_reduced_dim.R"))
 
 ## Prep directories
-plot_dir <- here("plots", "04_snRNA-seq", "28_sn_clean_plot")
+plot_dir <- here("plots", "04_snRNA-seq", "33_sn_subcluster_plots")
 if(!dir.exists(plot_dir)) dir.create(plot_dir)
 
-data_dir <- here("processed-data", "04_snRNA-seq", "28_sn_clean_plot")
+data_dir <- here("processed-data", "04_snRNA-seq", "33_sn_subcluster_plots")
 if(!dir.exists(data_dir)) dir.create(data_dir)
 
 ## Load HD5F sce
 message(Sys.time(), " - Load HDF5 sce")
-sce <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "sce_objects", "sce_ERC"))
+sce <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "sce_objects", "sce_ERC_subcluster"))
 
 pd <- as.data.frame(colData(sce))
 
+table(sce$cell_type_anno)
+
 ## load colors
-load(here("processed-data", "04_snRNA-seq", "cell_type_colors.Rdata"), verbose = TRUE) 
+# load(here("processed-data", "04_snRNA-seq", "cell_type_colors.Rdata"), verbose = TRUE) 
 load(here("processed-data", "project_colors.Rdata"), verbose = TRUE)
+
+cell_type_colors <- metadata(sce)$cell_type_colors
 
 #### Quality Metrics ####
 
@@ -44,5 +48,17 @@ sum_umi_violin <- ggplot(pd, aes(x = cell_type_anno, y = sum, fill = cell_type_a
           axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 ggsave(sum_umi_violin, filename = here(plot_dir, "ERC_sn_sum_umi_violin.png"), width = 10)
+
+#### violin plots of select genes ####
+rownames(sce) <- rowData(sce)$gene_name
+
+apoe_plot <- plot_gene_express(sce, genes = "APOE", 
+                               category = "cell_type_anno", 
+                               color_pal = cell_type_colors$anno) +
+    labs(x = "Cell Type") +
+    coord_flip()
+
+ggsave(apoe_plot, filename = here(plot_dir, "ERC_sn_gene_expres_APOE.png"), width = 4)
+
 
 
