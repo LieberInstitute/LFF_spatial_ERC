2a3397255335f5f073b3b740593b90c5e3b7d590
lahuuki <lahuuki@gmail.com>
2025-06-18 11:46:58 +0100

    fix sh files

--- code/13_compile_DGE/01_compile_DGE_Visium.sh
+++ code/13_compile_DGE/01_compile_DGE_Visium.sh
@@ -27,7 +27,7 @@ module load conda_R/4.5
 module list
 
 ## Edit with your job command
-Rscript 01_compile_DGE --datatype Visium
+Rscript 01_compile_DGE.R --datatype Visium
 
 echo "**** Job ends ****"
 date
--- code/13_compile_DGE/01_compile_DGE_sn_broad.sh
+++ code/13_compile_DGE/01_compile_DGE_sn_broad.sh
@@ -27,7 +27,7 @@ module load conda_R/4.5
 module list
 
 ## Edit with your job command
-Rscript 01_compile_DGE --datatype sn_broad
+Rscript 01_compile_DGE.R --datatype sn_broad
 
 echo "**** Job ends ****"
 date
