982ab6a374ab1c017f1bd328939fc03b3f4ac185
lahuuki <lahuuki@gmail.com>
2025-06-30 16:33:22 -0400

    Start GO anlaysis

---
+++ code/13_compile_DGE/02_GO_anlaysis.R
@@ -0,0 +1,31 @@
+## Louise Huuki-Myers, June 2025
+## Compile and plot all DGE data
+
+#### Set up ####
+library("tidyverse")
+library("here")
+library("sessioninfo")
+library("getopt")
+
+# Import command-line parameters
+scec <- matrix(
+    c("datatype", "d", "1", "character", "Data type"),
+    ncol = 5, byrow = TRUE
+)
+opt <- getopt(scec)
+
+## test
+# opt$datatype = "sn_broad"
+# opt$datatype = "Visium"
+
+data_dir <- here("processed-data", "13_compile_DGE", "02_GO_analysis")
+if (!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
+
+plot_dir <- here("plots", "13_compile_DGE", "02_GO_analysis")
+if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
+
+load(here("processed-data", "project_colors.Rdata"))
+
+## DE output data
+DE_data_dir <- here("processed-data", "13_compile_DGE", "01_compile_DGE", opt$datatype)
+dir.exists(data_dir)
