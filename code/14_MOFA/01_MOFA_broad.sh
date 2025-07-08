c6620404d76a985e8f9c92cea8edaadf8bb25f94
lahuuki <lahuuki@gmail.com>
2025-07-08 16:33:03 -0400

    Fix sh files

--- code/14_MOFA/01_MOFA_broad.sh
+++ code/14_MOFA/01_MOFA_broad.sh
@@ -27,7 +27,7 @@ module load conda_R/4.5
 module list
 
 ## Edit with your job command
-Rscript 04_DEG_boxplots.R --datatype sn_broad
+Rscript 01_MOFA.R --datatype sn_broad
 
 echo "**** Job ends ****"
 date
