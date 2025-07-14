f97affb4c45703e58fb5d17e22b55afe285edb2d
lahuuki <lahuuki@gmail.com>
2025-07-14 12:43:01 -0400

    Check n gene p-value<0.05

--- code/12_voomLmFit/02.1_optimal_cluster_vlmf_check.R
+++ code/12_voomLmFit/02.1_optimal_cluster_vlmf_check.R
@@ -17,18 +17,32 @@ load(here("processed-data", "00_project_prep", "cell_type_colors.V2.Rdata"), ver
 
 #### Read summary ####
 
-broad_summary <- read.csv(here("processed-data", "12_voomLmFit", "01_Clusterwise_voomLmFit", "vlmf_sn_broad", "vlmf_FDR05_summary-sn_broad.csv")) |>
-    dplyr::select(cell_type_broad = cluster, n_FDR05 = carrier) |>
-    mutate(n = 1, hc = "cell_type_broad")
+broad_summary <- readRDS(here("processed-data", "13_compile_DGE", "01_compile_DGE","sn_broad", "DGE_results_carrier_sn_broad.Rds")) |>
+    group_by(cell_type_broad = cluster) |>
+    summarise(n_FDR05 = sum(vlmf_adj.P.Val < 0.05),
+               n_pval05 = sum(vlmf_P.Value < 0.05)) |>
+    mutate(hc = "cell_type_broad",
+           n= 1)
 
 fine_count <- read.csv(here("processed-data", "12_voomLmFit", "01_Clusterwise_voomLmFit", "vlmf_sn_fine", "vlmf_FDR05_summary-sn_fine.csv")) |>
     dplyr::select(cell_type_fine = cluster, n_FDR05 = carrier) |>
     mutate(cell_type_broad = jaffelab::ss(cell_type_fine, "\\."),
            hc = "cell_type_fine")
 
+fine_count <- readRDS(here("processed-data", "13_compile_DGE", "01_compile_DGE","sn_fine", "DGE_results_carrier_sn_fine.Rds")) |>
+    group_by(cell_type_fine = cluster) |>
+    summarise(n_FDR05 = sum(vlmf_adj.P.Val < 0.05),
+              n_pval05 = sum(vlmf_P.Value < 0.05))  |>
+    mutate(cell_type_fine = as.character(cell_type_fine),
+           cell_type_broad = jaffelab::ss(cell_type_fine, "\\."),
+           hc = "cell_type_fine") 
+
+list.files(here("processed-data", "13_compile_DGE", "01_compile_DGE","sn_broad", "DGE_results_carrier_sn_broad.Rds"))
+
 fine_summary <- fine_count |>
     group_by(cell_type_broad, hc) |>
     summarise(n_FDR05 = sum(n_FDR05),
+              n_pval05 = sum(n_pval05),
               n = n())
     
 
@@ -37,6 +51,7 @@ opt_clus_count <- map_dfr(list.files(data_dir, pattern = "optimal_cluster_FDR05_
 opt_clus_summary <- opt_clus_count |>
     group_by(cell_type_broad, hc) |>
     summarise(n_FDR05 = sum(n_FDR05),
+              n_pval05 = sum(n_pval05),
               n = n()) |>
     bind_rows(broad_summary) |>
     bind_rows(fine_summary) |>
@@ -48,6 +63,7 @@ opt_clus_summary |> count(hc)
 sort(unique(opt_clus_summary$hc))
 
 opt_clus_summary |> group_by(cell_type_broad)|> slice_max(n_FDR05) 
+opt_clus_summary |> group_by(cell_type_broad)|> slice_max(n_pval05) 
 
 # cell_type_broad hc              n_FDR05     n
 # <chr>           <fct>             <int> <dbl>
@@ -86,6 +102,26 @@ nFDR_line <- opt_clus_summary |>
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
 ggsave(nFDR_line, filename = here(plot_dir, "hc_nFDR_line.png"))
+
+nPval_line <- opt_clus_summary |> 
+    ggplot(aes(x = hc, y = n_pval05, color = cell_type_broad)) +
+    geom_line(aes(group = cell_type_broad)) +
+    geom_point(aes(size = n)) +
+    theme_bw() +
+    scale_color_manual(values = cell_type_colors$broad) + 
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+
+ggsave(nFDR_line, filename = here(plot_dir, "hc_nPval_line.png"))
+
+FDR_pval_scatter <- opt_clus_summary |> 
+    ggplot(aes(x = n_pval05, y = n_FDR05, color = cell_type_broad)) +
+    # geom_line(aes(group = cell_type_broad)) +
+    geom_point(aes(size = n)) +
+    theme_bw() +
+    scale_color_manual(values = cell_type_colors$broad) + 
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+
+ggsave(FDR_pval_scatter, filename = here(plot_dir, "hc_FDR_pval_scatter.png"))
 
 
 
