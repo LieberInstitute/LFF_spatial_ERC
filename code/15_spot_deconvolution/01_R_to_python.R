3c62e43ccad96a392de1e62e0fa96e5d73df0ff0
lahuuki <lahuuki@gmail.com>
2025-07-30 09:02:54 -0400

    comment out marker gene selection for now

--- code/15_spot_deconvolution/01_R_to_python.R
+++ code/15_spot_deconvolution/01_R_to_python.R
@@ -120,39 +120,39 @@ message(Sys.time(), " - Converting SPE objects AnnDatas...")
 write_anndata(spe, spe_out)
 
 
-#-------------------------------------------------------------------------------
-#   Rank marker genes
-#-------------------------------------------------------------------------------
-
-#   We won't consider genes that aren't in both the spatial objects
-keep <- (rowData(sce)$gene_id %in% rowData(spe_IF)$gene_id) &
-    (rowData(sce)$gene_id %in% rowData(spe_nonIF)$gene_id)
-sce <- sce[keep, ]
-
-perc_keep <- 100 * (1 - length(which(keep)) / length(keep))
-print(
-    paste0(
-        "Dropped ", round(perc_keep, 1), "% of potential marker genes ",
-        "that were not present in all the spatial data"
-    )
-)
-
-print("Running getMeanRatio2 and findMarkers_1vAll to rank genes as markers...")
-marker_stats <- get_mean_ratio2(
-    sce,
-    cellType_col = cell_type_var, assay_name = "logcounts"
-)
-marker_stats_1vall <- findMarkers_1vAll(
-    sce,
-    cellType_col = cell_type_var, assay_name = "logcounts",
-    mod = "~BrNum"
-)
-marker_stats <- left_join(
-    marker_stats, marker_stats_1vall,
-    by = c("gene", "cellType.target")
-)
-
-saveRDS(marker_stats, marker_object_out)
+# #-------------------------------------------------------------------------------
+# #   Rank marker genes
+# #-------------------------------------------------------------------------------
+# 
+# #   We won't consider genes that aren't in both the spatial objects
+# keep <- (rowData(sce)$gene_id %in% rowData(spe_IF)$gene_id) &
+#     (rowData(sce)$gene_id %in% rowData(spe_nonIF)$gene_id)
+# sce <- sce[keep, ]
+# 
+# perc_keep <- 100 * (1 - length(which(keep)) / length(keep))
+# print(
+#     paste0(
+#         "Dropped ", round(perc_keep, 1), "% of potential marker genes ",
+#         "that were not present in all the spatial data"
+#     )
+# )
+# 
+# print("Running getMeanRatio2 and findMarkers_1vAll to rank genes as markers...")
+# marker_stats <- get_mean_ratio2(
+#     sce,
+#     cellType_col = cell_type_var, assay_name = "logcounts"
+# )
+# marker_stats_1vall <- findMarkers_1vAll(
+#     sce,
+#     cellType_col = cell_type_var, assay_name = "logcounts",
+#     mod = "~BrNum"
+# )
+# marker_stats <- left_join(
+#     marker_stats, marker_stats_1vall,
+#     by = c("gene", "cellType.target")
+# )
+# 
+# saveRDS(marker_stats, marker_object_out)
 
 # slurmjobs::job_single('01_R_to_python', create_shell = TRUE, memory = '25G', command = "Rscript 01_R_to_python.R")
 
