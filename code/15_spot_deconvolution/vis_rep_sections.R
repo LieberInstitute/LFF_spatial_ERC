e2a78c4227f6b637a9e58bd9469347cd73bfb276
lahuuki <lahuuki@gmail.com>
2025-08-04 16:14:21 -0400

    Add function to visualize rep sections

---
+++ code/15_spot_deconvolution/vis_rep_sections.R
@@ -0,0 +1,29 @@
+
+rep_sections_tb <- read.csv(here("processed-data", "05_spe_correct_cluster", "22_SpD_clean_plots", "rep_section.csv")) |>
+    filter(rep_section)
+
+vis_rep_sections <- function(geneid, 
+                             assayname = "weights",
+                             point_size = 1){
+    message("vis_ct: ", ct)
+    ct_plots <- map(c("AA", "EA"), function(anc) {
+        samples <- rep_sections_tb |> filter(Ancestry == anc) |> arrange(APOE)
+        
+        cluster_row_plots <- map(samples$sample_id, function(s) {
+            vis_clus_plot <- vis_gene(
+                spe = rcdt_spe,
+                geneid = geneid,
+                assayname = assayname,
+                point_size = point_size,
+                sampleid = s,
+                cont_colors = viridisLite::rocket(10, direction = -1)
+            ) 
+            return(vis_clus_plot)
+        })
+        cluster_row <- Reduce("+", cluster_row_plots) + plot_layout(nrow = 1)
+        return(cluster_row)
+    })
+    
+    ct_grid <- Reduce("/", ct_plots)
+    return(ct_grid)
+}
