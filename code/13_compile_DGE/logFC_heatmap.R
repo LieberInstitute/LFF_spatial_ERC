cba6d6e3ffeb3b7ad85c9775dd8e232ec924b573
lahuuki <lahuuki@gmail.com>
2025-08-06 13:25:56 -0400

    make data more flexible, add orange/teal color scale

--- code/13_compile_DGE/logFC_heatmap.R
+++ code/13_compile_DGE/logFC_heatmap.R
@@ -1,16 +1,16 @@
-logFC_Heatmap_contrast <- function(data, gene_list, title, h = 4, w = 10, cluster_col = FALSE){
+logFC_Heatmap_contrast <- function(dge_data, gene_list, title, h = 4, w = 10, cluster_col = FALSE){
     
-    data <- data |>
+    dge_data_filter <- dge_data |>
         filter(gene_name %in% gene_list) |>
         mutate(cluster_contrast = paste0(cluster, gsub("carrier","", contrast))) 
     
-    logFC_matrix <- data|>
+    logFC_matrix <- dge_data_filter|>
         select(cluster_contrast, gene_name, vlmf_logFC) |>
         pivot_wider(names_from = gene_name, values_from = vlmf_logFC) |>
         column_to_rownames("cluster_contrast") |>
         as.matrix()
     
-    pval_matrix <- data |>
+    pval_matrix <- dge_data_filter |>
         mutate(signif = case_when(vlmf_adj.P.Val < 0.001 ~ "***",
                                   vlmf_adj.P.Val < 0.01 ~ "**",
                                   vlmf_adj.P.Val < 0.05 ~ "*",
@@ -37,8 +37,16 @@ logFC_Heatmap_contrast <- function(data, gene_list, title, h = 4, w = 10, cluste
     
     # Heatmap(logFC_matrix, cluster_rows = FALSE, cluster_columns = FALSE)
     
+    max_abs <- max(abs(logFC_matrix), na.rm = TRUE)
+
+    my.col <- circlize::colorRamp2(
+        breaks = c(-1*max_abs,0,max_abs),
+        colors = c(APOE_carrier_colors[["E2+"]], "white", APOE_carrier_colors[["E4+"]])
+    )
+
     pdf(here(plot_dir, sprintf("DGE_%s_logFC_heatmap_%s.pdf", datatype, title)), height = h, width = w)
     print(Heatmap(logFC_matrix,
+                  col = my.col,
                   name = "log(FC)",
                   cluster_rows = FALSE,
                   cluster_columns = cluster_col,
