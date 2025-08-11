d8518527399e9911b95f8bc429a54c8a219d2062
lahuuki <lahuuki@gmail.com>
2025-08-11 14:36:56 -0400

    Add mode to sh script

--- code/18_LDSC/01_prep_marker_bed.sh
+++ code/18_LDSC/01_prep_marker_bed.sh
@@ -36,7 +36,7 @@ module load conda_R/4.5
 module list
 
 ## Edit with your job command
-Rscript 01_prep_marker_bed.R --datatype ${datatype}
+Rscript 01_prep_marker_bed.R --datatype ${datatype} --mode specificity
 
 echo "**** Job ends ****"
 date
