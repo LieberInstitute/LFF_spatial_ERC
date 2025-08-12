59ce0e311494da1332011d4362a23bc1a8619eda
lahuuki <lahuuki@gmail.com>
2025-08-11 22:12:28 -0400

    fix output, add job loop

--- code/18_LDSC/04_ldsc_results.R
+++ code/18_LDSC/04_ldsc_results.R
@@ -18,8 +18,9 @@ scec <- matrix(
 opt <- getopt(scec)
 
 
-opt$datatype <- "sn_broad"
-opt$mode <- "specificity"
+# opt$datatype <- "sn_broad"
+# opt$datatype <- "Visium"
+# opt$mode <- "specificity"
 
 data_dir <- here("processed-data", "18_LDSC", "04_ldsc_results")
 if (!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
@@ -52,11 +53,12 @@ ldsc_results |> group_by(cluster) |> slice_min(FDR) |>
 
 ## save data
 
-write.csv(ldsc_results, file = here(data_dir, sprintf("LDSC_results-%s_%s.scv", opt$datatype, opt$mode)))
+write.csv(ldsc_results, file = here(data_dir, sprintf("LDSC_results-%s_%s.scv", opt$datatype, opt$mode)),
+          row.names = FALSE)
 
 # slurmjobs::job_loop(loops = list(datatype = c("sn_broad","sn_fine","Visium")),
 #                     create_shell = TRUE,
-#                     name = "01_prep_marker_bed",
+#                     name = "04_ldsc_results",
 #                     create_script = FALSE)
 
 
