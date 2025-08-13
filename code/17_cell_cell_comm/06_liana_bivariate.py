4aac96616e0bb26f4deb03402867219db9e0e9f5
Nick-Eagles <nick.eagles@libd.org>
2025-08-13 10:59:40 -0400

    Make more progress, subsetting to just one sample and preparing to calculate connectivity

--- code/17_cell_cell_comm/06_liana_bivariate.py
+++ code/17_cell_cell_comm/06_liana_bivariate.py
@@ -6,9 +6,23 @@ from matplotlib import pyplot as plt
 from mudata import MuData
 from pyhere import here
 import os
+import json
 
 ad_sp_path = here('processed-data', '17_cell_cell_comm', 'spe.h5ad')
+sample_info_path = here(
+    'processed-data', '00_project_prep', '02_get_online_metadata',
+    'metadata_visium_plan.csv'
+)
+scale_path = here(
+    'processed-data', '01_spaceranger', '{}', 'outs', 'spatial',
+    'scalefactors_json.json'
+)
 plot_dir = here('plots', '17_cell_cell_comm', 'liana')
+spot_diameter_um = 65
+inter_spot_dist_um = 100
+
+sample_info = pd.read_csv(sample_info_path)
+sample_info['sample_id'] = sample_info['Visium Slide #'] + '_' + sample_info['Visium_subslide']
 
 ad_sp = sc.read_h5ad(ad_sp_path)
 
@@ -16,11 +30,22 @@ ad_sp = sc.read_h5ad(ad_sp_path)
 ad_sp.var.index = ad_sp.var['gene_name'].astype(str)
 ad_sp.var.index.name = None
 
-plot, _ = li.ut.query_bandwidth(
-    coordinates=ad_sp.obsm['spatial'], start=0, end=500, interval_n=20
-)
+#   Subset to just this one sample
+donor_id = ad_sp.obs['sample_id'].cat.categories[0]
+sample_id = sample_info.loc[sample_info['BrNum'] == donor_id, 'sample_id'].values[0]
+ad_sp_small = ad_sp[ad_sp.obs['sample_id'] == donor_id, :]
+scale_path = str(scale_path).format(sample_id)
+
+with open(scale_path, 'r') as f:
+    scale_factors = json.load(f)
+
+#   Calculate the bandwidth (radius from the center of a spot), in number of
+#   pixels, that just barely encompasses the 6 spot neighbors
+good_bandwidth = scale_factors['spot_diameter_fullres'] * (0.5 + inter_spot_dist_um / spot_diameter_um)
+
+plot, _ = li.ut.query_bandwidth(coordinates=ad_sp_small.obsm['spatial'], start=0, end=500, interval_n=20)
 plot.show()
-plt.savefig(os.path.join(plot_dir, 'neighbors_vs_radius.pdf'))
+plt.savefig(os.path.join(plot_dir, 'aaa.pdf'))
 plt.close('all')
 
 lr_df = li.mt.bivariate(
