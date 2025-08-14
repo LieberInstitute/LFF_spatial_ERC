3c29f0fc8f75a6791c6114bfd153142df716966d
Nick-Eagles <nick.eagles@libd.org>
2025-08-14 16:10:38 -0400

    Rewrite as an array job to handle broad/fine snRNA-seq and Visium data

--- code/17_cell_cell_comm/04_liana_mofa_run.py
+++ code/17_cell_cell_comm/04_liana_mofa_run.py
@@ -1,5 +1,5 @@
-#   Run LIANA by sample on the fine snRNA-seq data, one of two steps largely
-#   based on:
+#   Run LIANA by sample on the given data: either broad/fine snRNA-seq data or
+#   Visiums data. This is one of two steps largely based on:
 #   https://liana-py.readthedocs.io/en/latest/notebooks/mofatalk.html
 
 import scanpy as sc
@@ -8,25 +8,44 @@ from pyhere import here
 import session_info
 import os
 
-ad_in_path = here('processed-data', '17_cell_cell_comm', 'sce.h5ad')
-ad_out_path = here(
-    'processed-data', '17_cell_cell_comm', 'liana', 'ad_sc_ranked.h5ad'
-)
+task_id = int(os.getenv('SLURM_ARRAY_TASK_ID') - 1)
+
+if task_id == 1:
+    ad_in_path = here('processed-data', '17_cell_cell_comm', 'sce.h5ad')
+    ad_out_path = here(
+        'processed-data', '17_cell_cell_comm', 'liana', 'ranked_adatas',
+        'ad_sc_broad.h5ad'
+    )
+    group_var = 'cell_type_broad'
+elif task_id == 2:
+    ad_in_path = here('processed-data', '17_cell_cell_comm', 'sce.h5ad')
+    ad_out_path = here(
+        'processed-data', '17_cell_cell_comm', 'liana', 'ranked_adatas',
+        'ad_sc_fine.h5ad'
+    )
+    group_var = 'cell_type_anno'
+else:
+    ad_in_path = here('processed-data', '17_cell_cell_comm', 'spe.h5ad')
+    ad_out_path = here(
+        'processed-data', '17_cell_cell_comm', 'liana', 'ranked_adatas',
+        'ad_sc_visium.h5ad'
+    )
+    group_var = 'SpD'
 
 os.makedirs(ad_out_path.parent, exist_ok=True)
 num_cores = int(os.getenv('SLURM_CPUS_ON_NODE'))
 
 #   Read in and drop the incorrectly labeled donor
-ad_sc = sc.read_h5ad(ad_in_path)
-ad_sc = ad_sc[ad_sc.obs['sample_id'] != 'Br1289', :]
+adata = sc.read_h5ad(ad_in_path)
+adata = adata[adata.obs['sample_id'] != 'Br1289', :]
 
 #   Must use gene symbols for LIANA+
-ad_sc.var.index = ad_sc.var['gene_name'].astype(str)
-ad_sc.var.index.name = None
+adata.var.index = adata.var['gene_name'].astype(str)
+adata.var.index.name = None
 
 li.mt.rank_aggregate.by_sample(
-    ad_sc,
-    groupby='cell_type_anno',
+    adata,
+    groupby=group_var,
     resource_name='consensus',
     sample_key='sample_id',
     expr_prop = 0.1,
@@ -37,6 +56,6 @@ li.mt.rank_aggregate.by_sample(
     n_jobs=num_cores
 )
 
-sc.write(ad_out_path, ad_sc)
+sc.write(ad_out_path, adata)
 
 session_info.show()
