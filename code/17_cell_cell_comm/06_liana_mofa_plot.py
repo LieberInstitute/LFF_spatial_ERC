98341a84101242d7988d5ab48916e684d27c11ad
Nick-Eagles <nick.eagles@libd.org>
2025-08-15 11:50:13 -0400

    Also check correlation against tau pathology, and regenerate heatmaps

--- code/17_cell_cell_comm/06_liana_mofa_plot.py
+++ code/17_cell_cell_comm/06_liana_mofa_plot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import os
 import mudata
 import session_info
+import pandas as pd
 
 task_id = int(os.getenv('SLURM_ARRAY_TASK_ID')) - 1
 data_description = ['broad', 'fine', 'visium'][task_id]
@@ -13,11 +14,19 @@ mdata_in_path = here(
     'processed-data', '17_cell_cell_comm', 'liana', 'mofa_results',
     f'mdata_{data_description}.h5mu'
 )
+path_path = here(
+    "processed-data", "00_project_prep", "05_pathology", "sample_taupathy.csv"
+)
 plot_dir = here('plots', '17_cell_cell_comm', 'liana')
 
 os.makedirs(plot_dir, exist_ok=True)
 
+#   Read in results as an AnnData and append some pathology covariates
+path_df = pd.read_csv(path_path, index_col='BrNum')
 mdata = mudata.read(mdata_in_path)
+mdata.obs['taupathy'] = pd.Series(
+    ['t+' if x else 't-' for x in path_df['taupathy']], index = path_df.index
+)
 adata = sc.AnnData(obs = mdata.obs, obsm = mdata.obsm)
 
 dc.tl.rankby_obsm(adata, key='X_mofa', uns_key='rank_obsm')
--- code/17_cell_cell_comm/06_liana_mofa_plot.sh
+++ code/17_cell_cell_comm/06_liana_mofa_plot.sh
@@ -6,7 +6,7 @@
 #SBATCH -t 1-00:00:00
 #SBATCH -o ../../processed-data/17_cell_cell_comm/logs/06_liana_mofa_plot_%a.txt
 #SBATCH -e ../../processed-data/17_cell_cell_comm/logs/06_liana_mofa_plot_%a.txt
-#SBATCH --array=2
+#SBATCH --array=1-3%3
 
 set -e
 
