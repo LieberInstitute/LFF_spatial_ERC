253fd0ce1c2d7d5582fe3d17d1e27a7acc3f33f8
lahuuki <lahuuki@gmail.com>
2025-08-17 20:25:39 -0400

    move logFC_heatmap code to function file

--- code/13_compile_DGE/logFC_heatmap.R
+++ code/13_compile_DGE/logFC_heatmap.R
@@ -1,3 +1,54 @@
+logFC_Heatmap <- function(data, gene_list, title, h = 4, w = 10, cluster_col = FALSE){
+    
+    logFC_matrix <- data |>
+        filter(gene_name %in% gene_list) |>
+        select(cluster, gene_name, vlmf_logFC) |>
+        pivot_wider(names_from = gene_name, values_from = vlmf_logFC) |>
+        column_to_rownames("cluster") |>
+        as.matrix()
+    
+    pval_matrix <- data |>
+        filter(gene_name %in% gene_list) |>
+        mutate(signif = case_when(vlmf_adj.P.Val < 0.001 ~ "***",
+                                  vlmf_adj.P.Val < 0.01 ~ "**",
+                                  vlmf_adj.P.Val < 0.05 ~ "*",
+                                  TRUE ~ "")
+        ) |>
+        select(cluster, gene_name, signif) |>
+        pivot_wider(names_from = gene_name, values_from = signif) |>
+        column_to_rownames("cluster") |>
+        as.matrix()
+    
+    pval_matrix[is.na(pval_matrix)] <- ""
+    
+    ## reorder clusters & rows
+    gene_order <- order(colMeans(logFC_matrix, na.rm = TRUE))
+    
+    if(all(rownames(logFC_matrix) %in% cluster_levels)){
+        cluster_order <- cluster_levels[cluster_levels %in% rownames(logFC_matrix)]
+    } else {
+        cluster_order <- order(rownames(logFC_matrix))
+    }
+    
+    logFC_matrix <- logFC_matrix[cluster_order, gene_order]
+    pval_matrix <- pval_matrix[cluster_order, gene_order]
+    
+    # Heatmap(logFC_matrix, cluster_rows = FALSE, cluster_columns = FALSE)
+    
+    pdf(here(plot_dir, sprintf("DGE_%s_logFC_heatmap_%s.pdf", datatype, title)), height = h, width = w)
+    print(Heatmap(logFC_matrix,
+                  name = "log(FC)",
+                  cluster_rows = FALSE,
+                  cluster_columns = cluster_col,
+                  cell_fun = function(j, i, x, y, width, height, fill) {
+                      grid.text(pval_matrix[i, j], x, y, gp = gpar(fontsize = 10))
+                  }))
+    dev.off()
+    
+}
+
+
+
 logFC_Heatmap_contrast <- function(dge_data, gene_list, title, h = 4, w = 10, cluster_col = FALSE){
     
     dge_data_filter <- dge_data |>
