b6a0e52176be2faa61a5858668bcfcdc01dc29d4
Nick-Eagles <nick.eagles@libd.org>
2025-08-18 16:23:22 -0400

    Generate boxplots for each metric: raw scores, permutation-based p-values, and categorical scores

--- code/17_cell_cell_comm/08_liana_bivariate_plot.py
+++ code/17_cell_cell_comm/08_liana_bivariate_plot.py
@@ -1,6 +1,7 @@
 #   Summarize statistics for ligand-receptor pairs across donors and plot
 #   interesting pairs (based on high cosine similarity or high Moran's R)
 
+import numpy as np
 import pandas as pd
 import scanpy as sc
 from pyhere import here
@@ -18,6 +19,66 @@ out_path = here(
     'processed-data', '17_cell_cell_comm', 'liana', 'bivariate_stats.csv.gz'
 )
 min_num_donors = 20
+
+################################################################################
+#   Functions
+################################################################################
+
+#   Save boxplots of each metric by spatial domain for each LR pair
+def metric_boxplots(in_files, top_pairs, metric_name):
+    #   For each top pair, collect the value of the given metric at each spot
+    #   in each donor
+    pair_df_list = []
+    for f in in_files:
+        adata = sc.read(f)
+        present_pairs = [x for x in top_pairs if x in adata.var.index]
+        if len(present_pairs) > 0:
+            #   Grab an array of the metric values
+            if metric_name == 'score':
+                metric_arr = adata[:, present_pairs].X.toarray()
+            elif metric_name == 'p_value':
+                metric_arr = -1 * np.log10(
+                    adata[:, present_pairs].layers['pvals'].toarray()
+                )
+            else:
+                metric_arr = adata[:, present_pairs].layers['cats'].toarray()
+            
+            pair_df = pd.DataFrame(
+                metric_arr, columns = present_pairs,
+                index = adata.obs.index
+            )
+            pair_df[['SpD', 'sample_id']] = adata.obs[['SpD', 'sample_id']]
+            pair_df_list.append(pair_df)
+    
+    pair_df = (
+        pd.concat(pair_df_list, axis = 0)
+            .groupby(['SpD', 'sample_id'], as_index = False)
+            .agg({x: 'mean' for x in top_pairs})
+            .melt(
+                id_vars = ['SpD', 'sample_id'], value_vars = top_pairs,
+                var_name = 'pair_id', value_name=metric_name
+            )
+    )
+    
+    #   Boxplot showing metrics by spatial domain for each LR pair
+    p = (
+        ggplot(pair_df, aes(x = 'SpD', y = metric_name, color = 'SpD')) +
+            geom_boxplot(outlier_shape = None) +
+            geom_jitter(width = 0.1) +
+            facet_wrap('~pair_id', ncol = 4) +
+            theme_bw(base_size=15) +
+            theme(axis_text_x = element_text(angle=90, vjust=0.5, hjust=1))
+    )
+    p.save(
+        filename=os.path.join(
+            plot_dir, f'top_pairs_by_domain_{metric_name}.pdf')
+        ,
+        width=12, height=7
+    )
+
+################################################################################
+#   Main
+################################################################################
 
 #   Read input files as they exist (for one donor, no file was generated)
 in_files = [
@@ -89,41 +150,7 @@ sc.pl.spatial(
 plt.savefig(os.path.join(plot_dir, 'bivariate_cats.pdf'))
 plt.close('all')
 
-#   Grab scores for the top pairs at all spots in all samples
-pair_df_list = []
-for f in in_files:
-    adata = sc.read(f)
-    present_pairs = [x for x in top_pairs if x in adata.var.index]
-    if len(present_pairs) > 0:
-        pair_df = pd.DataFrame(
-            adata[:, present_pairs].X.toarray(), columns = present_pairs,
-            index = adata.obs.index
-        )
-        pair_df[['SpD', 'sample_id']] = adata.obs[['SpD', 'sample_id']]
-        pair_df_list.append(pair_df)
-
-pair_df = (
-    pd.concat(pair_df_list, axis = 0)
-        .groupby(['SpD', 'sample_id'], as_index = False)
-        .agg({x: 'mean' for x in top_pairs})
-        .melt(
-            id_vars = ['SpD', 'sample_id'], value_vars = top_pairs,
-            var_name = 'pair_id', value_name='score'
-        )
-)
-
-#   Boxplot showing metrics by spatial domain for each LR pair
-p = (
-    ggplot(pair_df, aes(x = 'SpD', y = 'score', color = 'SpD')) +
-        geom_boxplot(outlier_shape = None) +
-        geom_jitter(width = 0.1) +
-        facet_wrap('~pair_id', ncol = 4) +
-        theme_bw(base_size=15) +
-        theme(axis_text_x = element_text(angle=90, vjust=0.5, hjust=1))
-)
-p.save(
-    filename=os.path.join(plot_dir, 'top_pairs_by_domain.pdf'),
-    width=12, height=7
-)
+for metric_name in ['score', 'p_value', 'category_score']:
+    metric_boxplots(in_files, top_pairs, metric_name)
 
 session_info.show()
