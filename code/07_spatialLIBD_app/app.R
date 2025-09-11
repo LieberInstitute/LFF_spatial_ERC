823e4fbe024394afeba8f314135239707810a01f
lahuuki <lahuuki@gmail.com>
2025-09-11 10:54:18 -0400

    Update spatialLIBD app

--- code/07_spatialLIBD_app/app.R
+++ code/07_spatialLIBD_app/app.R
@@ -12,28 +12,30 @@ options(repos = BiocManager::repositories())
 ## main spe object
 spe <- readRDS("spe_ERC_app.rds")
 
-## k09 pseudobulk +modeling data
-spe_pb_k09 <- readRDS("spe_pseudobulk_k09.rds")
-## define spatialLIBD column
-spe_pb_k09$spatialLIBD <- spe_pb_k09$BayesSpace_SVGm_k09 
+# Add gene search
+rowData(spe)$gene_search <- paste0(rowData(spe)$gene_name, "; ", rowData(spe)$gene_id)
 
-modeling_results_k09 <- readRDS("modeling_results_k09.rds")
-sig_genes_k09 <- readRDS("sig_genes_k09.rds")
+## k09 pseudobulk +modeling data
+spe_pb <- readRDS("spe_pseudobulk-SpD.rds")
+
+## define spatialLIBD column
+spe_pb$spatialLIBD <- spe_pb$SpD
+
+# here::here("processed-data","05_spe_correct_cluster","20_model_pseudobulk_anno","spe_pseudobulk-SpD.rds")
+
+modeling_results <- readRDS("modeling-results-SpD.rds")
+sig_genes <- readRDS("sig_genes_SpD.rds")
 
 ## Quickly explore the data
 vars <- colnames(colData(spe))
 colnames(colData(spe)) <- vars <- gsub("X10x", "10x", vars)
 
-# ## Colors 
-# colors_BayesSpace <- Polychrome::palette36.colors(28)
-# names(colors_BayesSpace) <- c(1:28)
-# m <- match(as.character(spe$BayesSpace_harmony_09), names(colors_BayesSpace))
-# stopifnot(all(!is.na(m)))
-# spe$BayesSpace_colors <- spe$BayesSpace_harmony_09_colors <- colors_BayesSpace[m]
+## Colors 
+# metadata(spe)$SpD_colors
 
 spe_discrete_vars = c(
+    "SpD",
     "ManualAnnotation",
-    "in_tissue",
     vars[grep("^10x_", vars)],
     vars[grep("^scran_", vars)],
     "edge_spot",
@@ -47,14 +49,13 @@ spe_discrete_vars = c(
     sprintf("BayesSpace_Markers_k%02d", c(2,11))
 )
 
-
 spe_discrete_vars <- spe_discrete_vars[spe_discrete_vars %in% vars]
 
 spatialLIBD::run_app(
     spe = spe,
-    sce_layer = spe_pb_k09,
-    modeling_results = modeling_results_k09,
-    sig_genes = sig_genes_k09,
+    sce_layer = spe_pb,
+    modeling_results = modeling_results,
+    sig_genes = sig_genes,
     title = "LFF ERC, Visium, Sp09",
     spe_discrete_vars = spe_discrete_vars,
     spe_continuous_vars = c(
@@ -64,6 +65,6 @@ spatialLIBD::run_app(
         "expr_chrM_ratio",
         "edge_distance"
     ),
-    default_cluster = "BayesSpace_SVGm_k09",
+    default_cluster = "SpD",
     docs_path = "www"
 )
