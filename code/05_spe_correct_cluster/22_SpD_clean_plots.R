bb1cdb67e0b1c870230ffbb426e0c723c5614852
lahuuki <lahuuki@gmail.com>
2025-11-14 17:05:43 +0000

    rm prop from SRT sample summary

--- code/05_spe_correct_cluster/22_SpD_clean_plots.R
+++ code/05_spe_correct_cluster/22_SpD_clean_plots.R
@@ -54,7 +54,6 @@ write.csv(SpD_summary, file = here(data_dir, "ERC_Visium_summary_SpD.csv"))
 sample_summary <- pd |>
     group_by(sample_id, round, VNum, Visium_slide)  |>
     summarise(n_spots = n(),
-              prop = n_spots/ncol(spe),
               median_sum_umi = median(sum_umi),
               median_sum_gene = median(sum_gene),
               median_expr_chrM_ratio = median(expr_chrM_ratio))
