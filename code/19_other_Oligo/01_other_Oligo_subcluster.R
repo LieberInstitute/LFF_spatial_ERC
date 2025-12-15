f92fc82c6765ca3edd4e33961362c25b1097f4df
lahuuki <lahuuki@gmail.com>
2025-12-15 14:20:20 -0500

    Start subcluster code for other datasets - copy code from https://github.com/LieberInstitute/LFF_spatial_ERC/blob/899f604e503e1c62c4a7043bda1274e77c92b9b7/code/04_snRNA-seq/25_sn_cluster_subtype.R#L52C1-L131C1

---
+++ code/19_other_Oligo/01_other_Oligo_subcluster.R
@@ -0,0 +1,122 @@
+## Louise Huuki-Myers, Dec 2025
+## Subcluster Oligos in other datasets, check for Oligo 3
+
+#### Set up ####
+library("tidyverse")
+library("here")
+library("sessioninfo")
+library("spatialLIBD")
+
+data_dir <- here("processed-data", "19_other_Oligo", "01_other_Oligo_subcluster")
+if (!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
+
+plot_dir <- here("plots", "13_compile_DGE", "01_other_Oligo_subcluster")
+if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
+
+dataset <- "spatialDLPFC"
+
+#### Get Oligo data ####
+
+if(dataset == "spatialDLPFC"){
+    
+    ## get DLPFC snRNA-seq data
+    sce_path_zip <- fetch_data("spatialDLPFC_snRNAseq")
+    sce_path <- unzip(sce_path_zip, exdir = tempdir())
+    sce <- HDF5Array::loadHDF5SummarizedExperiment(
+        file.path(tempdir(), "sce_DLPFC_annotated")
+    )
+    
+    table(sce$cellType_hc)
+    table(sce$cellType_layer == "Oligo")
+    
+    sce <- sce[,!is.na(sce$cellType_layer)]
+    
+    ## subset to Oligos
+    sce <- sce[,sce$cellType_layer == "Oligo"]
+    
+    sce$cellType_hc <- droplevels(sce$cellType_hc)
+    table(sce$cellType_hc)
+}
+
+#### GLM PCA ####
+set.seed(425)
+
+harmony_file <- here(data_dir, sprintf("subcluster_HARMONY_pca_%s.rdata", cell_type))
+
+if(file.exists(harmony_file)){
+    message(Sys.time(), " - Load saved HARMONY PCs")
+    load(harmony_file)
+    reducedDim(sce, "HARMONY") <- subtype_HARMONY
+} else {
+    
+    message(Sys.time(), " - running Deviance Feat. Selection")
+    sce <- scry::devianceFeatureSelection(sce,
+                                          assay = "counts", fam = "binomial", sorted = F,
+                                          batch = as.factor(sce$seq_round)
+    )
+    
+    ## plot biononial deviance distribution
+    pdf(here(plot_dir, sprintf("sn_reprocess_binomial_deviance_%s.pdf", cell_type)))
+    plot(sort(rowData(sce)$binomial_deviance, decreasing = T),
+         type = "l", xlab = "ranked genes",
+         ylab = "binomial deviance", main = "Feature Selection with Deviance"
+    )
+    abline(v = 2000, lty = 2, col = "red")
+    abline(v = 5000, lty = 2, col = "blue")
+    dev.off()
+    
+    message(Sys.time(), " - running nullResiduals")
+    sce <- nullResiduals(sce,
+                         assay = "counts", 
+                         fam = "binomial", # default params
+                         type = "deviance"
+    )
+    
+    
+    hdgs <- rownames(sce)[order(rowData(sce)$binomial_deviance, decreasing = T)][1:5000]
+    
+    message(Sys.time(), " - running PCA")
+    sce <- runPCA(sce,
+                  exprs_values = "binomial_deviance_residuals",
+                  subset_row = hdgs,
+                  ncomponents = 100,
+                  name = "GLMPCA_approx",
+                  BSPARAM = BiocSingular::IrlbaParam()
+    )
+    
+    #### Batch Correction ####
+    message(Sys.time(), " - HARMONY Correction")
+    
+    ## Run harmony
+    ## needs PCA
+    reducedDim(sce, "PCA") <- reducedDim(sce, "GLMPCA_approx")
+    
+    message("running Harmony - ", Sys.time())
+    sce <- harmony::RunHarmony(sce, group.by.vars = "sample_id", verbose = TRUE)
+    
+    ## Remove redundant PCA
+    reducedDim(sce, "PCA") <- NULL
+    
+    ## save HARMONY dims
+    subtype_HARMONY <- reducedDim(sce, "HARMONY")
+    save(subtype_HARMONY, file = harmony_file)
+}
+
+#### SNN + Walktrap cluster ####
+
+## Build SNN graph
+k = opt$k
+message(Sys.time(), " - running buildSNNGraph: k =", k)
+snn.gr <- buildSNNGraph(sce, k = k, use.dimred = "HARMONY")
+
+## Run walk trap clustering
+message(Sys.time(), " - running walktrap")
+clusters <- igraph::cluster_walktrap(snn.gr)$membership
+table(clusters)
+
+## save data
+message(Sys.time() , " - saving data")
+save(clusters, file = here(data_dir, sprintf("walktrap_snn_k%02d_subclusters_%s.Rdata", k, cell_type)))
+
+
+
