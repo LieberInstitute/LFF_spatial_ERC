529a7ce00d87c4e7335b3e23146201a403a5aac9
lahuuki <lahuuki@gmail.com>
2026-01-15 15:49:29 -0500

    Explore NMF correlations

---
+++ code/20_NMF/02_NMF_var_correlation.R
@@ -0,0 +1,129 @@
+## Louise Huuki-Myers, Jan 2026
+## Correlate NMF factors with variables
+
+#### Set up ####
+
+library("SingleCellExperiment")
+library("tidyverse")
+library("sessioninfo")
+library("here")
+library("singlet")
+library(ComplexHeatmap)
+library(spatialLIBD)
+
+data_dir <- here("processed-data", "20_NMF", "02_NMF_var_correlation")
+if (!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
+
+plot_dir <- here("plots", "20_NMF", "02_NMF_var_correlation")
+if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
+
+#### load data ####
+message(Sys.time(), " - Load HDF5 sce")
+sce <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "sce_objects", "sce_ERC_subcluster"))
+
+## NMF data
+nmf <- readRDS(here("processed-data", "20_NMF", "01_runNMF", "ERC_sn_nmf.RDS"))
+
+class(nmf)
+
+#### Explore nmf ####
+## 75 factors
+dim(nmf@h)
+
+
+#### Technical Vars ####
+# data<-as.data.frame(sce$BrNum)
+# colnames(data)<-'brain'
+# onehot_brain <- reshape2::dcast(data = data, rownames(data) ~ brain, length)
+# 
+# head(onehot_brain)
+# dim(onehot_brain)
+
+tech_var_mod <- model.matrix(~Sex + Age + Rin + exp_round + seq_round + sum + detected + subsets_Mito_percent, colData(sce))
+dim(tech_var_mod)
+head(tech_var_mod)
+
+tech_var_mod <- as.data.frame(tech_var_mod)
+tech_var_mod[,"(Intercept)"] <- NULL
+
+colnames(tech_var_mod) <- gsub("BrNumBr", "Br", colnames(tech_var_mod))
+colnames(tech_var_mod) <- gsub("roundround", "round", colnames(tech_var_mod))
+
+nmf_tech_cor <- cor(t(nmf@h), tech_var_mod)
+
+head(nmf_tech_cor)
+
+## heatmap of technical variables
+pdf(here(plot_dir, "nmf_tech_var_cor_heatmap.pdf"), height = 11)
+Heatmap(nmf_tech_cor)
+dev.off()
+
+nmf_anno_tech_var <- annotate_registered_clusters(
+    cor_stats_layer = nmf_tech_cor,
+    confidence_threshold = 0.5,
+    cutoff_merge_ratio = 0.1
+)
+
+
+nmf_anno_tech_var |> filter(layer_confidence == "good")
+nmf_anno_tech_var |> filter(cluster == "nmf21")
+
+#### Demographic Vars ####
+
+demo_var_mod <- model.matrix(~BrNum, colData(sce))
+head(demo_var_mod)
+
+demo_var_mod <- as.data.frame(demo_var_mod)
+demo_var_mod[,"(Intercept)"] <- NULL
+
+colnames(demo_var_mod) <- gsub("BrNumBr", "Br", colnames(demo_var_mod))
+
+nmf_demo_cor <- cor(t(nmf@h), demo_var_mod)
+
+head(nmf_demo_cor)
+
+## heatmap of demonical variables
+pdf(here(plot_dir, "nmf_demo_var_cor_heatmap.pdf"), height = 11)
+Heatmap(nmf_demo_cor)
+dev.off()
+
+nmf_anno_demo_var <- annotate_registered_clusters(
+    cor_stats_layer = nmf_demo_cor,
+    confidence_threshold = 0.5,
+    cutoff_merge_ratio = 0.1
+)
+
+nmf_anno_demo_var |> filter(layer_confidence == "good")
+
+#### Cell type ####
+
+cellType_var_mod <- model.matrix(~cell_type_anno, colData(sce))
+head(cellType_var_mod)
+
+cellType_var_mod <- as.data.frame(cellType_var_mod)
+cellType_var_mod[,"(Intercept)"] <- NULL
+
+colnames(cellType_var_mod) <- gsub("cell_type_anno", "", colnames(cellType_var_mod))
+
+nmf_cellType_cor <- cor(t(nmf@h), cellType_var_mod)
+
+head(nmf_cellType_cor)
+
+## heatmap of cellType variables
+
+cell_type_colors <- metadata(sce)$cell_type_colors$anno
+
+pdf(here(plot_dir, "nmf_cellType_var_cor_heatmap.pdf"), height = 11)
+Heatmap(nmf_cellType_cor)
+dev.off()
+
+nmf_anno_cellType_var <- annotate_registered_clusters(
+    cor_stats_layer = nmf_cellType_cor,
+    confidence_threshold = 0.35,
+    cutoff_merge_ratio = 0.1
+)
+
+nmf_anno_cellType_var |> filter(layer_confidence == "good")
+nmf_anno_cellType_var |> filter(grepl("Oligo.3", layer_label))
+# cluster layer_confidence     layer_label
+# 1   nmf21             good Oligo.3/Oligo.4
