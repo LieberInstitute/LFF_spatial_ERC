db0af28844c3562b501390d9f19faaaecc109eae
lahuuki <lahuuki@gmail.com>
2026-01-22 08:50:05 -0500

    Explore Oligo subcluster proportions in differnt brain regions

--- code/19_other_Oligo/03_other_Oligo_explore.R
+++ code/19_other_Oligo/03_other_Oligo_explore.R
@@ -108,3 +108,63 @@ layer_stat_cor_plot(cor_layer_spatialHPC,
 )
 dev.off()
 
+#### Explore proportions ####
+
+cluster_tab_fn <- list(spatialdACC = "walktrap_snn_k20_subclusters_spatialdACC_wOPC.Rdata",
+                       spatialDLPFC = "walktrap_snn_k10_subclusters_spatialDLPFC_wOPC.Rdata",
+                       spatialHPC = "walktrap_snn_k10_subclusters_spatialHPC.Rdata")
+
+cluster_tab <- map_dfr(cluster_tab_fn, ~get(load(here("processed-data", "19_other_Oligo", "01_other_Oligo_subcluster", .x)))) |> 
+    mutate(cluster_anno2 = gsub("OPC.[0-9]","OPC", cluster_anno))
+
+## get Oligo annotations
+cor_layer_anno_fn <- list(spatialdACC = "other_OligoOPC_cor_layer_anno_spatialdACC_wOPC_k20.Rds",
+                          spatialDLPFC = "other_OligoOPC_cor_layer_anno_spatialDLPFC_wOPC_k10.Rds",
+                          spatialHPC = "other_Oligo_cor_layer_anno_spatialHPC_k10.Rds")
+
+cor_layer_anno <- map_dfr(cor_layer_anno_fn, ~pluck(readRDS(here("processed-data", "19_other_Oligo", "02_other_Oligo_model", .x)), 'anno')) |>
+    dplyr::rename(cluster_anno2 = cluster, anno_confidence = layer_confidence, ERC_Oligo = layer_label)
+
+cluster_sum <- cluster_tab  |>
+    dplyr::count(cluster_anno2) |>
+    left_join(cor_layer_anno) 
+
+erc_oligo_summary <- read.csv(here("processed-data", "04_snRNA-seq", "33_sn_subcluster_summary", "ERC_sn_subcluster_summary_cell_type.csv")) |>
+    filter(cell_type_broad == "Oligo") |>
+    select(oligo_anno = cell_type_anno, n = n_nuclei) |>
+    mutate(prop = n/sum(n), 
+           dataset= "erc", 
+           ERC_Oligo = oligo_anno)
+
+cluster_summary <- cluster_sum |>
+    filter(!grepl("OPC", cluster_anno2)) |>
+    separate(cluster_anno2, sep = "_", into = c("dataset", "oligo_anno"), remove = FALSE) |>
+    group_by(dataset) |>
+    mutate(prop = n/sum(n)) |>
+    bind_rows(erc_oligo_summary)
+
+erc_cluster_summary <- cluster_summary |> 
+    ungroup() |>
+    group_by(dataset, ERC_Oligo) |>
+    summarise(n = sum(n),
+              prop = sum(prop),
+              clus = paste(c(cluster_anno2), collapse = ", "))
+
+erc_cluster_summary |> print(n = 26)
+
+write_csv(erc_cluster_summary, here(data_dir, "other_Oligo_erc_annotation_summary.csv"))
+
+(erc_cluster_prop_wide <- erc_cluster_summary |> 
+    select(-n, -clus) |> 
+    pivot_wider(names_from = dataset, values_from = "prop")|>
+    arrange(ERC_Oligo))
+
+write_csv(erc_cluster_prop_wide, here(data_dir, "other_Oligo_erc_prop_wide.csv"))
+
+
+erc_cluster_summary |> select(-prop) |> pivot_wider(names_from = dataset, values_from = "n")
+
+erc_cluster_summary |> print(n = 22)
+
+
+                   
