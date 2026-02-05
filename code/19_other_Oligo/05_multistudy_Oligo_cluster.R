19a44898dfc71f809107139922fc02ff491039aa
lahuuki <lahuuki@gmail.com>
2026-02-05 13:01:09 -0500

    Run clustering loop k=10,20,30

--- code/19_other_Oligo/05_multistudy_Oligo_cluster.R
+++ code/19_other_Oligo/05_multistudy_Oligo_cluster.R
@@ -8,6 +8,7 @@ library("HDF5Array")
 library("tidyverse")
 library("here")
 library("sessioninfo")
+library("getopt")
 
 data_dir <- here("processed-data", "19_other_Oligo", "05_multistudy_Oligo_cluster")
 if (!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
@@ -15,26 +16,36 @@ if (!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
 plot_dir <- here("plots", "19_other_Oligo", "05_multistudy_Oligo_cluster")
 if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
+
+# Import command-line parameters
+scec <- matrix(
+    c("k", "k", "i", "integer", "k metric for cluster"),
+    ncol = 5, byrow = TRUE
+)
+opt <- getopt(scec)
+
+messsage("Run clustering with k=", opt$k)
+
 #### Load multi-study oligo SCE ####
-message(Sys.time(), " - Load HDF5 sce")
-sce <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "19_other_Oligo", "04_multistudy_Oligo_build", "sce_multistudy_Oligo"))
+# message(Sys.time(), " - Load HDF5 sce") ## loads in 3s
+# sce <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "19_other_Oligo", "04_multistudy_Oligo_build", "sce_multistudy_Oligo"))
 
-
+message(Sys.time(), " - Load Rds sce") ## 
+sce <- readRDS(here("processed-data", "19_other_Oligo", "04_multistudy_Oligo_build", "sce_multistudy_Oligo.Rds"))
 
 #### SNN + Walktrap cluster ####
-
-k = 50
+set.seed(2526)
 
 ## Build SNN graph
-message(Sys.time(), " - running buildSNNGraph: k =", k)
-snn.gr <- buildSNNGraph(sce, k = k, use.dimred = "HARMONY")
+message(Sys.time(), " - running buildSNNGraph: k =", opt$k)
+snn.gr <- buildSNNGraph(sce, k = opt$k, use.dimred = "HARMONY")
 
 ## Run walk trap clustering
 message(Sys.time(), " - running walktrap")
 clusters <- igraph::cluster_walktrap(snn.gr)$membership
 table(clusters)
 
-saveRDS(clusters, file = here(data_dir, "Multistudy_Oligo_cluster.Rds"))
+saveRDS(clusters, file = here(data_dir, sprintf("Multistudy_Oligo_cluster_k%i.Rds", opt$k)))
 
 # clusters <- readRDS(file = here("processed-data", "19_other_Oligo", "04_multistudy_Oligo_subcluster", "Multistudy_Oligo_cluster.Rds"))
 
@@ -45,19 +56,31 @@ table(clusters)
 
 message(Sys.time(), " - done walktrap")
 
+#### kmeans clustering #### 
+# k = 9
+# km <- kmeans(
+#     reducedDim(sce, "HARMONY"),
+#     centers = 
+# )
+# table(km$cluster, sce$cell_type_broad)
+# clusters <- km$cluster
+
+#### Annotate clusters ####
 cluster_anno <- as.data.frame(table(clusters, sce$cell_type_broad)) |>
     pivot_wider(names_from = "Var2", values_from = "Freq") |>
-    mutate(oligo_ratio = Oligo/OPC,
-           cell_type = ifelse(oligo_ratio >1, "Oligo", "OPC"),
+    mutate(oligo_prop = Oligo/(Oligo+OPC),
+           cell_type = case_when(oligo_prop > 0.8 ~ "Oligo", 
+                                 oligo_prop < 0.2 ~ "OPC",
+                                 TRUE ~ "Mix"),
            n = Oligo + OPC) |>
     dplyr::rename(cluster = clusters) |>
     group_by(cell_type) |>
     arrange(cell_type, -n) |>
     mutate(cluster_anno = paste0("multi_", cell_type,".", row_number()))
 
-write.csv(cluster_anno, file = here(data_dir, "Multistudy_Oligo_cluster_annotation.csv"))
+write.csv(cluster_anno, file = here(data_dir, sprintf("Multistudy_Oligo_cluster_annotation_k%i.csv", opt$k)))
 
-cluster_tab <- data.frame(key = sce$key, 
+cluster_tab <- data.frame(key = colnames(sce), 
                           cluster = clusters, 
                           cluster_anno = cluster_anno$cluster_anno[match(clusters, cluster_anno$cluster)])
 
@@ -65,20 +88,15 @@ head(cluster_tab)
 
 table(cluster_tab$cluster_anno)
 
-## save cluster_tab data
+#### save cluster_tab data ####
 message(Sys.time() , " - saving data")
-save(cluster_tab, file = here(data_dir, sprintf("walktrap_snn_k%02d_subclusters_Multistudy_Oligo.Rdata", k)))
+write_rds(cluster_tab, file = here(data_dir, sprintf("walktrap_snn_k%02d_subclusters_Multistudy_Oligo.Rds", opt$k)))
 
 ## Add to sce data & create color pal
 sce$Oligo_anno <- cluster_tab$cluster_anno
-other_oligo_colors <- create_cell_colors(cell_types = sort(unique(sce$Oligo_anno)), palette_name = "gg")
-
-
-#### Save data ####
+other_oligo_colors <- DeconvoBuddies::create_cell_colors(cell_types = sort(unique(sce$Oligo_anno)), palette_name = "gg")
 
 sce$metadata$colors <- other_oligo_colors
-
-saveHDF5SummarizedExperiment(sce, dir = here(data_dir, "sce_multistudy_Oligo"), replace=TRUE)
 
 #### Quality checks ####
 pd <- as.data.frame(colData(sce))
@@ -98,14 +116,11 @@ qc_violin_plot_all <- pd |>
     facet_grid(metric~., scales = "free") +
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) 
 
-ggsave(qc_violin_plot_all, filename = here(plot_dir, sprintf("other_Oligo_%s_k%i_QCmetricViolin.png", "Multistudy_Oligo", k)))
+ggsave(qc_violin_plot_all, filename = here(plot_dir, sprintf("Multistudy_Oligo_k%i_QCmetricViolin.png", opt$k)))
 
 
 #### Reduced dim plots ####
-
-message(Sys.time(), " - running TSNE")
-sce <- runTSNE(sce, dimred = "HARMONY")
-
+message(Sys.time(), " - Plot TSNE")
 source(here("code", "utils", "my_plot_reduced_dim.R"))
 
 tsne_plot <- my_plot_reduced_dim(sce,
@@ -114,9 +129,23 @@ tsne_plot <- my_plot_reduced_dim(sce,
                                  my_var = "Oligo_anno",
                                  var_type = "cat",
                                  save_plot = TRUE,
-                                 suffix = sprintf("mulristudy_Oligo_k%i", k),
+                                 suffix = sprintf("multistudy_Oligo_k%i", opt$k),
                                  facet = FALSE,
                                  plot_dir_rd = plot_dir,
                                  verbose = TRUE, 
                                  add_label = TRUE,
                                  color_pal = other_oligo_colors)
+
+# slurmjobs::job_loop(loops = list(k = c("10", "20", "30")),
+#                                  name = "05_multistudy_Oligo_cluster",
+#                                  create_shell = TRUE,
+#                                  create_script = FALSE)
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
+
+
