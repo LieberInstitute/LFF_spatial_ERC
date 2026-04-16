72eaec510006ed0ebb6ed1aa6171f413930e6be7
lahuuki <lahuuki@gmail.com>
2026-04-16 16:49:43 -0400

    Fix sh script

--- code/21_Xenium/07_xenium_build_spe.sh
+++ code/21_Xenium/07_xenium_build_spe.sh
@@ -21,13 +21,13 @@ echo "Node name: ${HOSTNAME}"
 echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
 ## Load the R module
-module load conda_R/4.4
+module load conda_R/4.5
 
 ## List current modules for reproducibility
 module list
 
 ## Edit with your job command
-Rscript 07_xenium_build_spe
+Rscript 07_xenium_build_spe.R
 
 echo "**** Job ends ****"
 date
