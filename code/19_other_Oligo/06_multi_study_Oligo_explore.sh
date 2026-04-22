82939ee5057b3f6df1ec5da61feeba1ea06f1eaa
lahuuki <lahuuki@gmail.com>
2026-04-22 13:35:52 -0400

    fix sh command

--- code/19_other_Oligo/06_multi_study_Oligo_explore.sh
+++ code/19_other_Oligo/06_multi_study_Oligo_explore.sh
@@ -36,7 +36,7 @@ module load conda_R/4.5
 module list
 
 ## Edit with your job command
-Rscript 06_multistudy_Oligo_explore.R --k ${k}
+Rscript 06_multi_study_Oligo_explore.R --k ${k}
 
 echo "**** Job ends ****"
 date
