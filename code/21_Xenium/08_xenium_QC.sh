f2aaefb5acb0b3dd9823b432d8fd85ab9dbecabc
lahuuki <lahuuki@gmail.com>
2026-04-27 16:53:45 -0400

    fix R version and mem

--- code/21_Xenium/08_xenium_QC.sh
+++ code/21_Xenium/08_xenium_QC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p shared
-#SBATCH --mem=100G
+#SBATCH --mem=20G
 #SBATCH --job-name=08_xenium_QC
 #SBATCH -c 1
 #SBATCH -t 1-00:00:00
@@ -21,7 +21,7 @@ echo "Node name: ${HOSTNAME}"
 echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
 ## Load the R module
-module load conda_R/4.4
+module load conda_R/4.5
 
 ## List current modules for reproducibility
 module list
