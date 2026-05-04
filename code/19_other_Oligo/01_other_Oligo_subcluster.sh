cf431aff1fe52fd8bc8b02547678de801bdefefa
lahuuki <lahuuki@gmail.com>
2026-05-04 12:31:03 -0400

    try dlpfc k20

--- code/19_other_Oligo/01_other_Oligo_subcluster.sh
+++ code/19_other_Oligo/01_other_Oligo_subcluster.sh
@@ -27,7 +27,7 @@ module load conda_R/4.5
 module list
 
 ## Edit with your job command
-Rscript 01_other_Oligo_subcluster.R --dataset spatialDLPFC --k 30 --opc TRUE -ds_short dlpfc
+Rscript 01_other_Oligo_subcluster.R --dataset spatialDLPFC --k 20 --opc TRUE -ds_short dlpfc
 
 echo "**** Job ends ****"
 date
