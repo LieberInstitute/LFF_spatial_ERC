76be7c750b2f30cba46e143d36fcb7a9d7eedd9a
lahuuki <lahuuki@gmail.com>
2026-05-08 16:00:58 -0400

    Use BiocParallel, add sh file

--- code/21_Xenium/09_xenium_label_transfer_singleR.R
+++ code/21_Xenium/09_xenium_label_transfer_singleR.R
@@ -7,6 +7,8 @@ library("qs2")
 library("here")
 library("sessioninfo")
 library("getopt")
+library("SingleR")
+library("BiocParallel")
 
 # Import command-line parameters
 scec <- matrix(
@@ -22,6 +24,12 @@ if(!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
 # plot_dir <- here("plots", "21_Xenium", "08_xenium_label_transfer_RCTD")
 # if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
+## SET UP MULTICORE PARAM
+ncores <- 4  # match Sys.getenv('SLURM_CPUS_ON_NODE')
+bp <- MulticoreParam(workers = ncores) #or bp <- MulticoreParam(4)
+
+set.seed(202605)
+
 #### load data ####
 message(Sys.time(), "- Load data")
 spe <- qs_read(here("processed-data", "21_Xenium", "08_xenium_QC_normalize","spe_xenium_QC.qs2"))
@@ -29,5 +37,25 @@ spe <- qs_read(here("processed-data", "21_Xenium", "08_xenium_QC_normalize","spe
 message(Sys.time(), " - Load HDF5 sce")
 sce <- HDF5Array::loadHDF5SummarizedExperiment(here("processed-data", "sce_objects", "sce_ERC_subcluster"))
 
-spe
-# dim: 366 467516 
+rownames(sce) <- rowData(sce)$gene_name
+
+#### Run SingleR ####
+message(Sys.time(), "- Run SingleR")
+pred <- SingleR(test = spe[,1:1000], 
+                ref = sce, 
+                labels = sce[[opt$cell_type_col]], 
+                assay.type.test="logcounts",
+                BSPARAM = BiocSingular::IrlbaParam(),
+                BPPARAM = bp)
+
+message(Sys.time(), " - Save qs2")
+qs2::qs_save(pred, here(data_dir, "SingleR_results_xenium.qs2"))
+
+# slurmjobs::job_single('09_xenium_label_transfer_singleR', create_shell = TRUE, memory = '200G', command = "Rscript 09_xenium_label_transfer_singleR.R --cell_type_col cell_type_anno")
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
