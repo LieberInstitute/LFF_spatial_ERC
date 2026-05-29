e7d7d74014dac907b972dcd7b44496916d4edc77
Nick-Eagles <nick.eagles@libd.org>
2026-05-29 15:16:18 -0400

    Add a pair of scripts to decide the bandwidth for the inflow analysis

---
+++ code/24_xenium_liana/02_bandwidth.py
@@ -0,0 +1,57 @@
+import scanpy as sc
+import liana as li
+import plotnine as p9
+import os
+from pyhere import here
+import session_info
+
+ad_path = here(
+    'processed-data', '24_xenium_liana', '01_to_python',
+    'cell_rna_converted_gobject.h5ad'
+)
+plot_dir = here('plots', '24_xenium_liana', '02_bandwidth')
+
+os.makedirs(plot_dir, exist_ok=True)
+
+#   Grab just the first sample. Spatial coordinates are in microns for Xenium,
+#   so units that apply to one sample should generally apply decently to the
+#   full dataset
+adata = sc.read_h5ad(ad_path)
+adata = adata[
+    adata.obs['sample_id'] == adata.obs['sample_id'].unique()[0], :
+]
+
+plot, df = li.ut.query_bandwidth(
+    coordinates=adata.obsm["spatial"],
+    start=10, end=50,           
+    interval_n=40    
+)
+
+#   Show that a bandwidth of 32 microns yields about 5 neighbors
+bandwidth = 32
+plot = (
+    plot + 
+        p9.geom_vline(xintercept=bandwidth, linetype="dashed", color="red") + 
+        p9.scale_y_continuous(
+            breaks=range(
+                int(df.neighbours.min()), int(df.neighbours.max()) + 1
+            )
+        )
+)
+plot.save(filename=str(plot_dir / "bandwidth_neighbors.pdf"), verbose=False)
+
+#   Compute neighbors (just for visualization purposes)
+li.ut.spatial_neighbors(
+    adata=adata, bandwidth=bandwidth, spatial_key="spatial"
+)
+
+#   Plot what the spatial kernel looks like on the example sample
+p = (
+    li.pl.connectivity(
+        adata, idx=5500, size=0.01, figure_size=(6, 5), spatial_key="spatial"
+    ) +
+    p9.coord_fixed()
+)
+p.save(filename=str(plot_dir / "spatial_kernel.pdf"), verbose=False)
+
+session_info.show()
---
+++ code/24_xenium_liana/02_bandwidth.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH -p katun
+#SBATCH --mem=10G
+#SBATCH --job-name=02_bandwidth
+#SBATCH -c 1
+#SBATCH -t 1-00:00:00
+#SBATCH -o ../../processed-data/24_xenium_liana/logs/02_bandwidth.txt
+#SBATCH -e ../../processed-data/24_xenium_liana/logs/02_bandwidth.txt
+
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load liana_plus/1.7.1
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+python 02_bandwidth.py
+
+echo "**** Job ends ****"
+date
+
+## This script was made using slurmjobs version 1.4.0
+## available from http://research.libd.org/slurmjobs/
