726712c7d12b2f1b24ddfb8a1fb9620b07666639
Nick-Eagles <nick.eagles@libd.org>
2026-06-01 15:02:52 -0400

    Produce spatial plots

--- code/24_xenium_liana/06_spatial_plots.sh
+++ code/24_xenium_liana/06_spatial_plots.sh
@@ -6,7 +6,6 @@
 #SBATCH -t 1-00:00:00
 #SBATCH -o ../../processed-data/24_xenium_liana/logs/06_spatial_plots.txt
 #SBATCH -e ../../processed-data/24_xenium_liana/logs/06_spatial_plots.txt
-#SBATCH --mail-type=ALL
 
 set -e
 
@@ -20,8 +19,7 @@ echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${HOSTNAME}"
 echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
-## Load the R module
-module load conda_R/4.5
+module load liana_plus/1.7.1
 
 ## List current modules for reproducibility
 module list
