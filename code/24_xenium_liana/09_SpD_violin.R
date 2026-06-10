2dbc013d4da12563f8d50e4f9ecb89544203127d
Nick-Eagles <nick.eagles@libd.org>
2026-06-10 15:30:19 -0400

    Fix a bug in quantile filtering; use boxplots which are visually more reasonable for this distribution, and create a version filtered to just Oligo.3 receiver cells

--- code/24_xenium_liana/09_SpD_violin.R
+++ code/24_xenium_liana/09_SpD_violin.R
@@ -15,24 +15,44 @@ plot_dir = here('plots', '24_xenium_liana', '09_SpD_violin')
 
 dir.create(plot_dir, showWarnings = FALSE)
 
+clean_boxplot = function(plot_df, plot_path) {
+    # Compute whisker bounds per domain to set y-axis limits
+    whisker_bounds = plot_df |>
+        group_by(SpX) |>
+        summarise(
+            lower = quantile(inflow_score, 0.25) - 1.5 * IQR(inflow_score),
+            upper = quantile(inflow_score, 0.75) + 1.5 * IQR(inflow_score),
+            .groups = 'drop'
+        )
+
+    y_max = max(whisker_bounds$upper)
+
+    p = ggplot(plot_df, aes(x = SpX, y = inflow_score)) +
+        geom_boxplot(outlier.shape = NA) +
+        coord_cartesian(ylim = c(0, y_max)) +
+        theme_bw(base_size = 20) +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+        labs(x = 'Spatial Domain', y = 'Inflow Score')
+    pdf(plot_path)
+    print(p)
+    dev.off()
+}
+
 spe = qs_read(spe_path)
 
 inflow_df = read_csv(inflow_path, show_col_types = FALSE)
 stopifnot(all(inflow_df$cell_id %in% colnames(spe)))
 
-p = tibble(cell_id = colnames(spe), SpX = spe$SpX) |>
-    inner_join(inflow_df, by = 'cell_id') |>
-    group_by(SpX) |>
-    filter(inflow_score > quantile(inflow_score, 0.99)) |>
-    ungroup() |>
-    ggplot(aes(x = SpX, y = inflow_score)) +
-        geom_violin(fill = 'steelblue') +
-        expand_limits(y = 0) +
-        theme_bw(base_size = 20) +
-        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-        labs(x = 'Spatial Domain', y = 'Inflow Score')
-pdf(file.path(plot_dir, 'SpD_violin.pdf'))
-print(p)
-dev.off()
+plot_df = tibble(
+        cell_id = colnames(spe), SpX = spe$SpX, cell_type = spe$cell_type_anno
+    ) |>
+    inner_join(inflow_df, by = 'cell_id')
+
+clean_boxplot(plot_df, file.path(plot_dir, 'SpD_boxplot_all_cell_types.pdf'))
+clean_boxplot(
+    plot_df |>
+        filter(cell_type == 'Astro.3'),
+    file.path(plot_dir, 'SpD_boxplot_astro_3.pdf')
+)
 
 session_info()
