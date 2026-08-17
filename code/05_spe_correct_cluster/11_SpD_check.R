644576b49975d3b43a0dfb717293ba79bb24a0a6
lahuuki <lahuuki@gmail.com>
2026-08-17 12:08:48 -0400

    rm old comments, checkup to k12

--- code/05_spe_correct_cluster/11_SpD_check.R
+++ code/05_spe_correct_cluster/11_SpD_check.R
@@ -1,5 +1,5 @@
 ## September 2024, Louise Huuki-Myers
-## Check quality metrics and marker genes expression of SpatialDomains
+## Check quality metrics and marker genes expression of Spatial Domains
 
 library("spatialLIBD")
 library("scater")
@@ -44,18 +44,18 @@ table(spe$BayesSpace_SVGm_k02)
 
 #### Plot clusters in reduced dims ####
 
-walk(c("UMAP", "TSNE", "UMAP.HARMONY", "TSNE.HARMONY"), function(dim){
-my_plot_reduced_dim(spe, 
-                    prefix = "spe", 
-                    var_type = "cat", 
-                    dimred = dim, 
-                    my_var = "BayesSpace_SVGm_k02", 
-                    color_pal = SpD_colors$k02)
-    })
+# walk(c("UMAP", "TSNE", "UMAP.HARMONY", "TSNE.HARMONY"), function(dim){
+# my_plot_reduced_dim(spe, 
+#                     prefix = "spe", 
+#                     var_type = "cat", 
+#                     dimred = dim, 
+#                     my_var = "BayesSpace_SVGm_k02", 
+#                     color_pal = SpD_colors$k02)
+#     })
 
 walk(c("UMAP", "TSNE", "UMAP.HARMONY", "TSNE.HARMONY"), function(dim){
 
-    walk(c(2:9), function(k){
+    walk(c(2:12), function(k){
         
         SpD_colors_k <- SpD_colors[[sprintf("k%02d", k)]]
         
@@ -75,7 +75,7 @@ walk(c("UMAP", "TSNE", "UMAP.HARMONY", "TSNE.HARMONY"), function(dim){
 
 message(Sys.time(), "- Scran Outlier Violin Plots")
 
-map(c(2,9), function(k){
+map(c(2,12), function(k){
     
     SpD_colors_k <- SpD_colors[[sprintf("k%02d", k)]]
     my_var = sprintf("BayesSpace_SVGm_k%02d", k)
@@ -101,7 +101,7 @@ map(c(2,9), function(k){
 
 pd <- as.data.frame(colData(spe))
 
-pd_qc_long <- map(c(k02 = 2, k09 = 9), function(k){
+pd_qc_long <- map(c(k02 = 2, k09 = 9, k11 = 11), function(k){
     
     SpD_colors_k <- SpD_colors[[sprintf("k%02d", k)]]
     my_var = sprintf("BayesSpace_SVGm_k%02d", k)
@@ -134,8 +134,11 @@ pd_qc_long <- map(c(k02 = 2, k09 = 9), function(k){
 
 rownames(spe) <- rowData(spe)$gene_name
 
+spe_pb_k11 <- readRDS(here("processed-data", "05_spe_correct_cluster", "08_model_pseudobulk", "BayesSpace_SVGm","spe_pseudobulk-BayesSpace_SVGm_k11.rds"))
 spe_pb_k09 <- readRDS(here("processed-data", "05_spe_correct_cluster", "08_model_pseudobulk", "BayesSpace_SVGm","spe_pseudobulk-BayesSpace_SVGm_k09.rds"))
 spe_pb_k02 <- readRDS(here("processed-data", "05_spe_correct_cluster", "08_model_pseudobulk", "BayesSpace_SVGm","spe_pseudobulk-BayesSpace_SVGm_k02.rds"))
+
+modeling_results_k11 <- readRDS(here("processed-data", "05_spe_correct_cluster", "08_model_pseudobulk", "BayesSpace_SVGm","modeling_results-BayesSpace_SVGm_k11.rds"))
 modeling_results_k09 <- readRDS(here("processed-data", "05_spe_correct_cluster", "08_model_pseudobulk", "BayesSpace_SVGm","modeling_results-BayesSpace_SVGm_k09.rds"))
 modeling_results_k09 <- readRDS(here("processed-data", "05_spe_correct_cluster", "08_model_pseudobulk", "BayesSpace_SVGm","modeling_results-BayesSpace_SVGm_k02.rds"))
 
@@ -145,17 +148,6 @@ modeling_results_k09$enrichment |>
     arrange(fdr_Sp09D07) |>
     head(10)
 
-#                     gene t_stat_Sp09D07 p_value_Sp09D07  fdr_Sp09D07 logFC_Sp09D07
-# ENSG00000197971      MBP       5.439940    1.161905e-07 1.391170e-07     1.5887006 * Oligo
-# ENSG00000168314     MOBP       5.361880    1.723183e-07 2.049026e-07     1.6362505 * Oligo
-# ENSG00000064787    BCAS1       5.139315    5.176398e-07 6.039571e-07     1.3500216
-# ENSG00000136541     ERMN       4.772775    2.928887e-06 3.334874e-06     1.4077814
-# ENSG00000160781    PAQR6       4.699450    4.093380e-06 4.637675e-06     0.9957585
-# ENSG00000078804 TP53INP2       4.491110    1.036391e-05 1.159142e-05     0.9421917
-# ENSG00000108387  SEPTIN4       4.427812    1.365368e-05 1.523207e-05     0.9601562
-# ENSG00000197430   OPALIN       4.300728    2.352742e-05 2.610959e-05     1.2275028 * Oligodendrocytic Myelin Paranodal And Inner Loop Protein
-# ENSG00000123560     PLP1       4.090538    5.629261e-05 6.195332e-05     1.2083869 * Oligo
-# ENSG00000064393    HIPK2       4.047131    6.711421e-05 7.364323e-05     0.8292675
 
 spe_pb_k09$spatialLIBD <- spe_pb_k09$BayesSpace_SVGm_k09
 
@@ -194,41 +186,10 @@ modeling_results_k02$enrichment |>
     dplyr::arrange(fdr_Sp02D02) |>
     head()
 
-# t_stat_Sp02D01 t_stat_Sp02D02 p_value_Sp02D01 p_value_Sp02D02  fdr_Sp02D01  fdr_Sp02D02 logFC_Sp02D01
-# ENSG00000197971      -4.027751       4.027751    0.0001531692    0.0001531692 0.0001829491 0.0001829491    -1.3134630
-# ENSG00000168314      -3.778068       3.778068    0.0003515309    0.0003515309 0.0004022662 0.0004022662    -1.2530633
-# ENSG00000160781      -2.632721       2.632721    0.0106279208    0.0106279208 0.0110006968 0.0110006968    -0.7610147
-# ENSG00000131095      -2.618882       2.618882    0.0110239323    0.0110239323 0.0113993786 0.0113993786    -0.7789849
-# ENSG00000123560      -2.607316       2.607316    0.0113651575    0.0113651575 0.0117448760 0.0117448760    -0.8642089
-# ENSG00000108387      -2.409959       2.409959    0.0188687516    0.0188687516 0.0194049051 0.0194049051    -0.6677911
-# logFC_Sp02D02         ensembl    gene
-# ENSG00000197971     1.3134630 ENSG00000197971     MBP
-# ENSG00000168314     1.2530633 ENSG00000168314    MOBP
-# ENSG00000160781     0.7610147 ENSG00000160781   PAQR6
-# ENSG00000131095     0.7789849 ENSG00000131095    GFAP
-# ENSG00000123560     0.8642089 ENSG00000123560    PLP1
-# ENSG00000108387     0.6677911 ENSG00000108387 SEPTIN4
-
 modeling_results_k02$enrichment |>     
     dplyr::filter(logFC_Sp02D01 > 0) |>
     dplyr::arrange(fdr_Sp02D01) |>
     head()
-
-# t_stat_Sp02D01 t_stat_Sp02D02 p_value_Sp02D01 p_value_Sp02D02  fdr_Sp02D01  fdr_Sp02D02 logFC_Sp02D01
-# ENSG00000137413       33.68944      -33.68944    3.605340e-42    3.605340e-42 4.170658e-38 4.170658e-38      6.071708
-# ENSG00000123975       31.16121      -31.16121    3.833033e-40    3.833033e-40 2.217026e-36 2.217026e-36      6.055455
-# ENSG00000177054       22.67885      -22.67885    4.081454e-32    4.081454e-32 1.573808e-28 1.573808e-28      6.171540
-# ENSG00000119328       22.43205      -22.43205    7.570381e-32    7.570381e-32 2.189354e-28 2.189354e-28      6.366399
-# ENSG00000255366       20.98213      -20.98213    3.185335e-30    3.185335e-30 6.870747e-27 6.870747e-27      5.900472
-# ENSG00000183060       20.93975      -20.93975    3.563666e-30    3.563666e-30 6.870747e-27 6.870747e-27      5.810567
-# logFC_Sp02D02         ensembl       gene
-# ENSG00000137413     -6.071708 ENSG00000137413       TAF8
-# ENSG00000123975     -6.055455 ENSG00000123975       CKS2
-# ENSG00000177054     -6.171540 ENSG00000177054    ZDHHC13
-# ENSG00000119328     -6.366399 ENSG00000119328    ABITRAM
-# ENSG00000255366     -5.900472 ENSG00000255366 AC120036.4
-# ENSG00000183060     -5.810567 ENSG00000183060     LYSMD4
-
 
 # write_csv(modeling_results_k02$enrichment, file = here("processed-data", "05_spe_correct_cluster", "08_model_pseudobulk", "modeling_results-BayesSpace_SVGm_k02.csv"))
 
