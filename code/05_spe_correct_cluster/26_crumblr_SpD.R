9ee62ffb21dea06e6788e541f21f1b8867c25a45
lahuuki <lahuuki@gmail.com>
2026-08-27 19:20:41 -0400

    Update crumblr w/ vSpD

--- code/05_spe_correct_cluster/26_crumblr_SpD.R
+++ code/05_spe_correct_cluster/26_crumblr_SpD.R
@@ -21,30 +21,25 @@ if(!dir.exists(data_dir)) dir.create(data_dir)
 load(here("processed-data", "SpD_colors.Rdata"), verbose = TRUE)
 load(here("processed-data", "project_colors.Rdata"), verbose = TRUE)
 
+SpD_levels <- names(SpD_colors)
+
 #### Load data ####
 load(here("processed-data", "05_spe_correct_cluster", "19_SpD_update_spe", "SpD_proportions.Rdata"), verbose = TRUE)
 # SpD_proportions
 
 SpD_proportions <- SpD_proportions |> filter(sample_id != "Br1289")
 
-SpD_df <- SpD_proportions |> 
-    mutate(SpD_syn = gsub("~", "_", SpD)) |>
-    ungroup() |>
-    select(SpD, SpD_syn) |>
-    unique()
-
 ## calc error formula for standard error of propotion SE = sqrt(p*(1-p)/n)
 error <- SpD_proportions |>
     mutate(error = sqrt((prop * (1-prop))/sum(n)),
            anno = paste0(n,"/", sum(n))) |>
-    select(sample_id, SpD, error, anno)
+    select(sample_id, vSpD, error, anno)
 
 ## create cell count matrix
 cell_counts <- SpD_proportions |> 
-    mutate(SpD_syn = gsub("~", "_", SpD)) |>
     ungroup() |>
-    select(sample_id, SpD_syn, n) |>
-    pivot_wider(names_from = SpD_syn, values_from = n)  |>
+    select(sample_id, vSpD, n) |>
+    pivot_wider(names_from = vSpD, values_from = n)  |>
     column_to_rownames("sample_id")
 
 ## replace NA w/ 0
@@ -71,10 +66,9 @@ cobj <- crumblr(cell_counts)
 
 clr_prop_long <- cobj$E |>
     as.data.frame() |>
-    rownames_to_column("SpD_syn") |>
-    pivot_longer(!SpD_syn, names_to = "sample_id", values_to = "CLR") |>
+    rownames_to_column("vSpD") |>
+    pivot_longer(!vSpD, names_to = "sample_id", values_to = "CLR") |>
     left_join(erc_info |> rownames_to_column("sample_id")) |>
-    left_join(SpD_df) |>
     left_join(error)
 
 # Partition variance into components for sample
@@ -83,7 +77,7 @@ vp <- fitExtractVarPartModel(cobj, form, erc_info)
 
 # Plot variance fractions
 fig.vp <- plotPercentBars(vp)
-fig.vp
+# fig.vp
 
 ggsave(fig.vp + 
            theme_bw() + 
@@ -110,7 +104,7 @@ cobj_pca <- ggplot(df_pca, aes(PC1, PC2, color = Visium_slide, shape = APOE)) +
     geom_point(size = 3) +
     theme_classic() +
     theme(aspect.ratio = 1) +
-    scale_color_discrete(name = "Subject") +
+    # scale_color_discrete(name = "Subject") +
     xlab("PC1") +
     ylab("PC2")
 
@@ -152,14 +146,17 @@ ggsave(clr_boxplot_Visium_slide, filename = here(plot_dir, "clr_boxplot_Visium_s
 
 
 #### Hierarchical clustering ####
-spe_pb <- readRDS(here("processed-data", "05_spe_correct_cluster", "25_SpD_pseudobulk", "spe_pseudobulk_only-SpD_syn.rds"))
+spe_pb <- readRDS(here("processed-data", "05_spe_correct_cluster", "25_SpD_pseudobulk", "spe_pseudobulk_only-vSpD.rds"))
 dim(spe_pb)
 
 dist.clusCollapsed <- dist(t(logcounts(spe_pb)))
 tree.clusCollapsed <- hclust(dist.clusCollapsed, "ward.D2")
 
 dend <- as.dendrogram(tree.clusCollapsed, hang = 0.2)
-# plot(dend, main = "hierarchical cluster dend", horiz = TRUE)
+
+pdf(here(plot_dir, "hierarchical_cluster_dendrogram_vSpD.pdf"))
+plot(dend, main = "hierarchical cluster dend", horiz = TRUE)
+dev.off()
 
 # Use variancePartition workflow to analyze each SpD
 
@@ -169,16 +166,18 @@ fit <- eBayes(fit)
 
 # Test APOE carrier
 (diff_prop_APOE_carrier <- topTable(fit, coef = "APOE_carrierE4+", number = Inf))
-#                     logFC     AveExpr           t    P.Value adj.P.Val         B
-# WM_Sp09D06    -0.49369667 -0.06336945 -1.88330104 0.07319348 0.6587413 -4.276222
-# L2.3_Sp09D01   0.29782763 -0.21215444  1.36020054 0.18779244 0.7776110 -4.487527
-# Inhib_Sp09D09  0.11282175 -0.51510636  0.97529404 0.34020990 0.7776110 -4.627923
-# L6_Sp09D04    -0.10189724  0.55858775 -0.87316693 0.39217475 0.7776110 -4.645685
-# L5_Sp09D03    -0.13401893 -0.16236857 -0.68279468 0.50199373 0.7776110 -4.759007
-# WM.uf_Sp09D07  0.15135687 -0.06844375  0.49499131 0.62560326 0.7776110 -4.733619
-# LD_Sp09D02     0.04024377  0.85387084  0.43449588 0.66823749 0.7776110 -4.723201
-# L1_Sp09D05    -0.09734721  0.28070405 -0.40258468 0.69120979 0.7776110 -4.702966
-# Vasc_Sp09D08  -0.01867111 -0.67172007 -0.07730314 0.93909390 0.9390939 -4.762624
+#              logFC    AveExpr           t    P.Value adj.P.Val         B
+# vWMd   -0.69198449 -0.7925698 -2.30943789 0.03000760 0.3300836 -3.524001
+# vL2     0.48733930  0.2590005  1.79309029 0.08580174 0.3959066 -4.118591
+# vL5b    0.47879560  0.2865521  1.67086840 0.10797452 0.3959066 -4.247660
+# vInhib  0.20547960 -1.2511731  1.32306758 0.19850529 0.5458896 -4.527167
+# vWMpv  -0.32388097 -0.1606711 -1.05357860 0.30275696 0.6280980 -4.757622
+# vL6    -0.17906856  0.5290294 -0.96856016 0.34259889 0.6280980 -4.813384
+# vL5a   -0.17744437  0.1263453 -0.74014624 0.46652027 0.6674069 -4.978527
+# vL1    -0.21874928  0.6487558 -0.70882259 0.48538681 0.6674069 -4.834797
+# vLD     0.10152290  0.7206322  0.58551407 0.56376658 0.6779896 -4.931778
+# vWMuf   0.17037805  0.1973571  0.50773999 0.61635418 0.6779896 -4.993951
+# vVasc  -0.01696857 -0.5632584 -0.05991562 0.95272795 0.9527280 -5.026387
 
 write.csv(diff_prop_APOE_carrier, file = here(data_dir, "SpD_diff_prop_APOE_carrier.csv"))
 
@@ -190,10 +189,12 @@ head(cleanY_cobj)
 
 clean_clr_prop_long <- cleanY_cobj |>
     as.data.frame() |>
-    rownames_to_column("SpD_syn") |>
-    pivot_longer(!SpD_syn, names_to = "sample_id", values_to = "CLR_cleanY")
+    rownames_to_column("vSpD") |>
+    pivot_longer(!vSpD, names_to = "sample_id", values_to = "CLR_cleanY")
 
-clr_prop_long <- clr_prop_long |> left_join(clean_clr_prop_long)
+clr_prop_long <- clr_prop_long |> 
+    left_join(clean_clr_prop_long) |>
+    mutate(vSpD = factor(vSpD, levels = SpD_levels))
 
 ## clr plot of top results
 clr_cleanY_boxplot_APOE_carrier  <- clr_prop_long |>
@@ -201,7 +202,7 @@ clr_cleanY_boxplot_APOE_carrier  <- clr_prop_long |>
     geom_boxplot(outlier.shape = NA) +
     # geom_jitter(aes(color = error), width = .1) +
     geom_jitter(width = .1) +
-    facet_wrap(~SpD, scales = "free_y", nrow = 1) +
+    facet_wrap(~vSpD, scales = "free_y", nrow = 1) +
     scale_fill_manual(values = APOE_carrier_colors) +
     theme_bw() +
     theme(legend.position = "None")
@@ -244,16 +245,18 @@ ggsave(forest_plot, filename = here(plot_dir, "crumblr_SpD_APOE_carrier_forest.p
 (slides <- colnames(fit$design)[grep("Visium_slide", colnames(fit$design))])
 
 topTable(fit, coef = slides, number = Inf)
-#               Visium_slideV13Y24.343 Visium_slideV13Y24.344     AveExpr         F    P.Value adj.P.Val
-# L3_Sp09D02               -0.09108499             -0.1492597  0.85933332 3.2954274 0.01356117 0.1220505
-# WM_Sp09D06                0.28476468             -0.2938779 -0.06954456 1.3910685 0.25485856 0.5457378
-# WM.uf_Sp09D07             0.33007275              0.2088800 -0.08853475 1.3733007 0.26193527 0.5457378
-# L6_Sp09D04               -0.04727934              0.0838746  0.57643431 1.2683854 0.30746514 0.5457378
-# L2.3_Sp09D01              0.05055732             -0.5125162 -0.19707896 1.1430306 0.37079949 0.5457378
-# Vasc_Sp09D08              0.59171416              0.6716892 -0.70635115 1.0920655 0.39946856 0.5457378
-# L5_Sp09D03                0.15539167             -0.2040875 -0.14699867 1.0362420 0.43283429 0.5457378
-# L1_Sp09D05                0.48705349              0.4129016  0.27080407 0.9548491 0.48510030 0.5457378
-# LD_Sp09D09               -0.10479198             -0.1859432 -0.49806362 0.8272432 0.57497078 0.5749708
+# Visium_slideV13Y24.342 Visium_slideV13Y24.343 Visium_slideV13Y24.344    AveExpr         F    P.Value adj.P.Val
+# vWMd              -0.80351260             0.57154893           -0.898452477 -0.7925698 2.7067623 0.03274812 0.3602293
+# vLD                0.09468673            -0.33180266           -0.140153068  0.7206322 1.7128547 0.15432382 0.6206414
+# vL2                0.56641285             0.22150738           -0.444922193  0.2590005 1.3405776 0.27571861 0.6206414
+# vL1                0.37281046             0.75339193            0.657143517  0.6487558 1.2944525 0.29580661 0.6206414
+# vL6                0.27671656             0.09665595            0.255609798  0.5290294 1.2376391 0.32232400 0.6206414
+# vWMuf             -0.47356866             0.24001734            0.136346822  0.1973571 1.2048821 0.33853165 0.6206414
+# vL5a               0.62905803             0.84072543            0.326752297  0.1263453 0.9079456 0.51718765 0.7418922
+# vL5b               0.23809129            -0.24496856           -0.289252154  0.2865521 0.8669803 0.54612801 0.7418922
+# vVasc              0.68481767             0.66452535            0.850362758 -0.5632584 0.7844065 0.60700271 0.7418922
+# vWMpv             -0.42367786             0.52231617            0.009814914 -0.1606711 0.5770769 0.76733878 0.7877749
+# vInhib             0.12542752            -0.09287964           -0.068536604 -1.2511731 0.5502414 0.78777489 0.7877749
 
 # res <- treeTest(fit, cobj, tree.clusCollapsed, coef = slides, method = "FE")
 # res <- treeTest(fit, cobj, tree.clusCollapsed, coef = slides, method = "FE.empirical")
@@ -264,19 +267,19 @@ res <- treeTest(fit, cobj, tree.clusCollapsed, coef = slides, method = "sidak")
 res <- treeTest(fit, cobj, tree.clusCollapsed, coef = slides, method = "fisher")
 
 # Plot hierarchy and testing results
-plotTreeTest(res)
-plotTreeTestBeta(res) 
-
-plotForest(res)
-           
-combined_fig <- fig.vp +
-    theme(legend.position = "left") |
-    plotTreeTestBeta(res) +
-    theme(legend.position = "bottom", legend.box = "vertical") |
-    plotForest(res, hide = FALSE) 
-
-ggsave(combined_fig, filename = here(plot_dir, "crumblr_SpD_combined_APOE_carrier.png"))
-
+# plotTreeTest(res)
+# plotTreeTestBeta(res) 
+# 
+# plotForest(res)
+#            
+# combined_fig <- fig.vp +
+#     theme(legend.position = "left") |
+#     plotTreeTestBeta(res) +
+#     theme(legend.position = "bottom", legend.box = "vertical") |
+#     plotForest(res, hide = FALSE) 
+# 
+# ggsave(combined_fig, filename = here(plot_dir, "crumblr_SpD_combined_APOE_carrier.png"))
+# 
 
 #### fit APOE_carrier ####
 fit <- dream(cobj, ~ APOE + Age + Visium_slide, erc_info)
@@ -286,29 +289,33 @@ fit <- eBayes(fit)
 (geno <- colnames(fit$design)[grep("APOE", colnames(fit$design))])
 
 topTable(fit, coef = geno, number = Inf)
-#                 APOEE2.E3   APOEE3.E4   APOEE4.E4     AveExpr         F    P.Value  adj.P.Val
-# WM.uf_Sp09D07 -1.45822296 -0.71161195 -0.57578954 -0.08853475 4.7981628 0.01019429 0.07254207
-# L2.3_Sp09D01   0.97780063  1.05490717  0.91721907 -0.19707896 4.2715653 0.01612046 0.07254207
-# L1_Sp09D05    -0.98712303 -0.68638232 -0.69236950  0.27080407 3.5695900 0.03053114 0.09159341
-# WM_Sp09D06    -0.72657490 -0.91152411 -0.83819807 -0.06954456 2.6446709 0.07451405 0.14996941
-# LD_Sp09D09     0.43800262  0.49694333  0.40256131 -0.49806362 2.5329896 0.08331634 0.14996941
-# L5_Sp09D03     0.62567182  0.37905216  0.27819314 -0.14699867 1.6860349 0.19918238 0.29877357
-# L3_Sp09D02     0.22188381  0.24068605  0.11585516  0.85933332 1.0709642 0.38178055 0.49086070
-# Vasc_Sp09D08  -0.13047354  0.03866813 -0.34979559 -0.70635115 0.6093153 0.61607566 0.69308512
-# L6_Sp09D04     0.03480808 -0.11499476  0.05244457  0.57643431 0.3705864 0.77498382 0.77498382
+#         APOEE2.E3    APOEE3.E4   APOEE4.E4    AveExpr         F    P.Value adj.P.Val
+# vWMuf  -1.4792312 -0.678682931 -0.43884724  0.1973571 4.4893064 0.01282457 0.1272176
+# vL1    -1.2232885 -1.011312475 -0.74114774  0.6487558 3.4198735 0.03431942 0.1272176
+# vL2     1.0675820  1.323825639  1.02873329  0.2590005 3.4084638 0.03469570 0.1272176
+# vWMd   -0.6416058 -1.024912066 -1.02029635 -0.7925698 2.3714389 0.09689224 0.2551571
+# vL5a    0.7958566  0.302081456  0.47584396  0.1263453 2.1513275 0.12155455 0.2551571
+# vWMpv  -0.9179539 -0.849297602 -0.77556945 -0.1606711 2.0211745 0.13917659 0.2551571
+# vL5b    0.7563362  1.087187775  0.68640148  0.2865521 1.7372074 0.18755243 0.2947252
+# vLD     0.1136103  0.421578891 -0.03805594  0.7206322 1.4608008 0.25149517 0.3458059
+# vInhib  0.3620315  0.464401527  0.36140096 -1.2511731 1.1559260 0.34805307 0.4253982
+# vL6    -0.1240307 -0.351651950 -0.20753701  0.5290294 0.5464017 0.65556579 0.7211224
+# vVasc  -0.1677176  0.003933727 -0.33546314 -0.5632584 0.3436182 0.79402577 0.7940258
 
 
 topTable(fit, coef = "APOEE2/E3", number = Inf)
-#                     logFC     AveExpr          t     P.Value  adj.P.Val          B
-# WM.uf_Sp09D07 -1.45822296 -0.08853475 -3.7192489 0.001198532 0.01078679 -0.9108791
-# L1_Sp09D05    -0.98712303  0.27080407 -3.1978520 0.004165307 0.01435555 -1.9918766
-# L2.3_Sp09D01   0.97780063 -0.19707896  3.1387595 0.004785185 0.01435555 -2.3535928
-# LD_Sp09D09     0.43800262 -0.49806362  2.3705441 0.026979388 0.06070362 -3.7208538
-# L5_Sp09D03     0.62567182 -0.14699867  2.2111866 0.037757567 0.06785159 -4.2650056
-# WM_Sp09D06    -0.72657490 -0.06954456 -2.1235449 0.045234391 0.06785159 -4.3347318
-# L3_Sp09D02     0.22188381  0.85933332  1.5814708 0.128090230 0.16468744 -5.0093801
-# Vasc_Sp09D08  -0.13047354 -0.70635115 -0.3812269 0.706704178 0.79504220 -6.3409796
-# L6_Sp09D04     0.03480808  0.57643431  0.1756788 0.862158770 0.86215877 -6.1948897
+#             logFC    AveExpr          t     P.Value  adj.P.Val         B
+# vWMuf  -1.4792312  0.1973571 -3.4975452 0.001954218 0.02149640 -1.332302
+# vL1    -1.2232885  0.6487558 -3.1430477 0.004582582 0.02520420 -2.040101
+# vL2     1.0675820  0.2590005  2.6258585 0.015157620 0.05557794 -3.258967
+# vL5a    0.7958566  0.1263453  2.3184468 0.029734716 0.07059277 -3.871608
+# vWMpv  -0.9179539 -0.1606711 -2.2826970 0.032087625 0.07059277 -3.963263
+# vL5b    0.7563362  0.2865521  1.6991708 0.102868408 0.18859208 -4.933679
+# vWMd   -0.6416058 -0.7925698 -1.5233722 0.141392049 0.20699618 -5.250391
+# vInhib  0.3620315 -1.2511731  1.4875615 0.150542675 0.20699618 -5.042012
+# vLD     0.1136103  0.7206322  0.4664093 0.645340335 0.67946447 -6.016474
+# vL6    -0.1240307  0.5290294 -0.4465423 0.659410553 0.67946447 -6.175231
+# vVasc  -0.1677176 -0.5632584 -0.4185361 0.679464469 0.67946447 -6.085853
 
 # Perform multivariate test across the hierarchy - APOE geno
 res <- treeTest(fit, cobj, tree.clusCollapsed, coef = "APOEE2/E3")
