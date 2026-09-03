2ed5019388b6da8be26d5c86bedd111b2fbca9d0
lahuuki <lahuuki@gmail.com>
2026-09-02 20:46:13 -0400

    Run Age interaction

--- code/12_voomLmFit/03_Clusterwise_voomLmFit_interaction_Visium.sh
+++ code/12_voomLmFit/03_Clusterwise_voomLmFit_interaction_Visium.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=03_Clusterwise_voomLmFit_interaction_Visium
 #SBATCH -c 1
 #SBATCH -t 1-00:00:00
-#SBATCH -o logs/03_Clusterwise_voomLmFit_interaction_Visium_Anc.txt
-#SBATCH -e logs/03_Clusterwise_voomLmFit_interaction_Visium_Anc.txt
+#SBATCH -o logs/03_Clusterwise_voomLmFit_interaction_Visium_Age.txt
+#SBATCH -e logs/03_Clusterwise_voomLmFit_interaction_Visium_Age.txt
 #SBATCH --mail-type=ALL
 
 set -e
@@ -27,7 +27,7 @@ module load conda_R/4.5
 module list
 
 ## Edit with your job command
-Rscript 03_Clusterwise_voomLmFit_interaction.R --datatype Visium --interaction Anc
+Rscript 03_Clusterwise_voomLmFit_interaction.R --datatype Visium --interaction Age
 
 echo "**** Job ends ****"
 date
