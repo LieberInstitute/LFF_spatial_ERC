deed8726a1920c3267c2b43a937db9fa8c32ddf3
lahuuki <lahuuki@gmail.com>
2026-09-23 11:12:26 -0400

    Update sh and logs

--- code/04_snRNA-seq/29_sn_subcluster_model_pseudobulk.sh
+++ code/04_snRNA-seq/29_sn_subcluster_model_pseudobulk.sh
@@ -27,7 +27,7 @@ module load conda_R/4.5
 module list
 
 ## Edit with your job command
-Rscript 29_sn_subcluster_model_pseudobulk.R --cluster 'cell_type_broad'
+Rscript 29_sn_subcluster_model_pseudobulk.R --cluster 'cell_type_anno'
 
 echo "**** Job ends ****"
 date
